dc70731a4aac5fea5ff31317a1638fefdddf8573
Jörn Friedrich Dreyer <jfd@butonic.de>
2012-07-10 11:59:52 +0200

    add optimize code

---
+++ apps/search_lucene/ajax/lucene.php
@@ -0,0 +1,38 @@
+<?php
+ 
+
+header('Content-type: text/html; charset=UTF-8') ;
+
+OCP\JSON::checkLoggedIn();
+OCP\JSON::checkAppEnabled('search_lucene');
+
+function handleReIndex() {
+    /*
+  OCP\DB::beginTransaction();
+  set_time_limit(0);
+  OC_Gallery_Album::cleanup();
+  $eventSource = new OC_EventSource();
+  OC_Gallery_Scanner::scan($eventSource);
+  $eventSource->close();
+  OCP\DB::commit();
+     * 
+     */
+    echo 'bla';
+}
+
+function handleOptimize() {
+    OC_Search_Lucene::optimizeIndex();
+}
+
+if ($_GET['operation']) {
+  switch($_GET['operation']) {
+  case 'reindex':
+    handleReIndex();
+    break;
+  case 'optimize':
+    handleOptimize();
+    break;
+  default:
+    OCP\JSON::error(array('cause' => 'Unknown operation'));
+  }
+}
--- apps/search_lucene/lib/lucene.php
+++ apps/search_lucene/lib/lucene.php
@@ -40,6 +40,30 @@ class OC_Search_Lucene extends OC_Search_Provider {
         return $index;
     }
 
+    /**
+     * optimizes the lucene index
+     * 
+     * 
+     * @author Jörn Dreyer <jfd@butonic.de>
+     * 
+     * @param Zend_Search_Lucene_Interface $index an optional index
+     * 
+     * @return void
+     */
+    static public function optimizeIndex(Zend_Search_Lucene_Interface $index = null) {  
+                
+        if ($index === null) {
+           $index = self::openOrCreate();
+        }
+        
+        
+        OC_Log::write('search_lucene',
+                      'oprimizing index ' . $path,
+                      OC_Log::DEBUG);
+        
+        $index->optimize();
+                
+    }
     
     /**
      * upates a file in the lucene index
@@ -127,10 +151,13 @@ class OC_Search_Lucene extends OC_Search_Provider {
     public function search($query){
         $results=array();
         if ( $query !== null ) {
+            // * query * kills performance for bigger indexes
+            // query * works ok
+            // query is still best
             //$query = '*' . $query . '*'; //FIXME emulates the old search but breaks all the nice lucene search query options
             try {
                 $index = self::openOrCreate(); 
-                //Zend_Search_Lucene_Search_Query_Wildcard::setMinPrefixLength(0); //default is 3, 0 needed to keep current search behaviour
+                Zend_Search_Lucene_Search_Query_Wildcard::setMinPrefixLength(0); //default is 3, 0 needed to keep current search behaviour
 
                 $hits = $index->find($query);
                 
