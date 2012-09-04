9313e0d8021db4efa80390c240fb1782adc4da90
Jörn Friedrich Dreyer <jfd@butonic.de>
2012-09-05 01:46:17 +0200

    disable queries starting with a wildcard

--- apps/search_lucene/lib/lucene.php
+++ apps/search_lucene/lib/lucene.php
@@ -157,7 +157,7 @@ class OC_Search_Lucene extends OC_Search_Provider {
             //$query = '*' . $query . '*'; //FIXME emulates the old search but breaks all the nice lucene search query options
             try {
                 $index = self::openOrCreate(); 
-                Zend_Search_Lucene_Search_Query_Wildcard::setMinPrefixLength(0); //default is 3, 0 needed to keep current search behaviour
+                //Zend_Search_Lucene_Search_Query_Wildcard::setMinPrefixLength(0); //default is 3, 0 needed to keep current search behaviour
 
                 $hits = $index->find($query);
                 
