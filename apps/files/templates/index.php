aa75f3a75000f8f36b216f92ce951317af39a6f8
Moritz Schwoerer <mr.mosch@gmail.com>
2012-11-19 23:20:28 +0100

    added dynamic css solution. had to set the table layout to fixed. moved the id attribute from tbody to table and changed css to that.

--- apps/files/templates/index.php
+++ apps/files/templates/index.php
@@ -8,12 +8,14 @@
 				<ul class="popup popupTop">
 					<li style="background-image:url('<?php echo OCP\mimetype_icon('text/plain') ?>')" data-type='file'><p><?php echo $l->t('Text file');?></p></li>
 					<li style="background-image:url('<?php echo OCP\mimetype_icon('dir') ?>')" data-type='folder'><p><?php echo $l->t('Folder');?></p></li>
-					<li style="background-image:url('<?php echo OCP\image_path('core','actions/public.png') ?>')" data-type='web'><p><?php echo $l->t('From url');?></p></li>
+					<li style="background-image:url('<?php echo OCP\image_path('core', 'actions/public.png') ?>')" data-type='web'><p><?php echo $l->t('From link');?></p></li>
 				</ul>
 			</div>
 			<div class="file_upload_wrapper svg">
 				<form data-upload-id='1' id="data-upload-form" class="file_upload_form" action="<?php echo OCP\Util::linkTo('files', 'ajax/upload.php'); ?>" method="post" enctype="multipart/form-data" target="file_upload_target_1">
 					<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $_['uploadMaxFilesize'] ?>" id="max_upload">
+					<!-- Send the requesttoken, this is needed for older IE versions because they don't send the CSRF token via HTTP header in this case -->
+					<input type="hidden" name="requesttoken" value="<?php echo $_['requesttoken'] ?>" id="requesttoken">
 					<input type="hidden" class="max_human_file_size" value="(max <?php echo $_['uploadMaxHumanFilesize']; ?>)">
 					<input type="hidden" name="dir" value="<?php echo $_['dir'] ?>" id="dir">
 					<input class="file_upload_start" type="file" name='files[]'/>
@@ -40,10 +42,10 @@
 	<div id="emptyfolder"><?php echo $l->t('Nothing in here. Upload something!')?></div>
 <?php endif; ?>
 
-<table>
+<table id="fileList">
 	<thead>
 		<tr>
-			<th id='headerName'>
+			<th id="headerName">
 				<input type="checkbox" id="select_all" />
 				<span class='name'><?php echo $l->t( 'Name' ); ?></span>
 				<span class='selectedActions'>
@@ -56,7 +58,7 @@
 			<th id="headerSize"><?php echo $l->t( 'Size' ); ?></th>
 			<th id="headerDate">
 				<span id="modified"><?php echo $l->t( 'Modified' ); ?></span>
-				<?php if ($_['permissions'] & OCP\Share::PERMISSION_DELETE): ?>
+				<?php if ($_['permissions'] & OCP\PERMISSION_DELETE): ?>
 <!-- 					NOTE: Temporary fix to allow unsharing of files in root of Shared folder -->
 					<?php if ($_['dir'] == '/Shared'): ?>
 						<span class="selectedActions"><a href="" class="delete"><?php echo $l->t('Unshare')?> <img class="svg" alt="<?php echo $l->t('Unshare')?>" src="<?php echo OCP\image_path("core", "actions/delete.svg"); ?>" /></a></span>
@@ -67,7 +69,7 @@
 			</th>
 		</tr>
 	</thead>
-	<tbody id="fileList">
+	<tbody>
 		<?php echo($_['fileList']); ?>
 	</tbody>
 </table>
