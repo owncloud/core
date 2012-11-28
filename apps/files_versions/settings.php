be38a2730a870a24a5b55e6d5bd58bc903442d08
Björn Schießle <schiessle@owncloud.com>
2012-11-28 16:48:52 +0100

    finalize settings to expire old versions

--- apps/files_versions/settings.php
+++ apps/files_versions/settings.php
@@ -4,12 +4,10 @@ OCP\User::checkAdminUser();
 
 //OCP\Util::addscript( 'files_versions', 'versions' );
 OCP\Util::addscript('files_versions', 'settings-admin');
-
 $tmpl = new OCP\Template( 'files_versions', 'settings');
 $tmpl->assign( 'versioningEnabled', OCP\Config::getSystemValue('versions', 'true') );
 $tmpl->assign( 'versioningLimitType', OCP\Config::getAppValue('files_versions', 'limitType', 'number') );
 $tmpl->assign( 'versioningLimitSize', OCP\Config::getAppValue('files_versions', 'max_size', '10485760') );
 $tmpl->assign( 'versioningLimitNumber', OCP\Config::getAppValue('files_versions', 'max_number', '5') );
-
 
 return $tmpl->fetchPage();
