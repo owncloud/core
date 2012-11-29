6bc2ed190c536275c81b9afa570c2738f4d3eb7d
Björn Schießle <schiessle@owncloud.com>
2012-11-29 14:04:51 +0100

    check for correct limit type

--- apps/files_versions/templates/settings.php
+++ apps/files_versions/templates/settings.php
@@ -12,8 +12,8 @@ var version_limit_size =  "<?php echo $_['versioningLimitSize']; ?>";
 			<label for="versions"><?php echo $l->t('Enable Versioning'); ?></label> <br/>
 			
 			<?php echo $l->t('Limit'); ?>: <select name='versionLimitType' id='versionLimitType' <?php if (!$_['versioningEnabled']) echo ' disabled="disabled"'; ?> >
-					<option value='size' <?php if( $_['versioningLimitType'] == "max_size") echo 'selected="selected"' ?> ><?php echo $l->t('Maximal size of versions per file'); ?></option>
-					<option value='number' <?php if( $_['versioningLimitType'] == "max_number") echo 'selected="selected"' ?>><?php echo $l->t('Maximal number of versions per file'); ?></option>
+					<option value='size' <?php if( $_['versioningLimitType'] == "size") echo 'selected="selected"' ?> ><?php echo $l->t('Maximal size of versions per file'); ?></option>
+					<option value='number' <?php if( $_['versioningLimitType'] == "number") echo 'selected="selected"' ?>><?php echo $l->t('Maximal number of versions per file'); ?></option>
 				</select> <input name="maxVersions" id="maxVersions" style="width:90px;" value='<?php if ( $_['versioningLimitType'] == 'size') echo $_['versioningLimitSize']; else echo $_['versioningLimitNumber']; ?>' title="<?php echo $l->t( '0 is unlimited' ); ?>"<?php if (!$_['versioningEnabled']) echo ' disabled="disabled"'; ?> />
         </fieldset>
 </form>
