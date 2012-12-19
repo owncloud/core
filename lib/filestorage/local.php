298522510b0f27d7879f61a8af048b72b9d22bb4
Björn Schießle <schiessle@owncloud.com>
2012-12-19 14:19:34 +0100

    somehow the recursive switch was lost, fixed

--- lib/filestorage/local.php
+++ lib/filestorage/local.php
@@ -11,7 +11,7 @@ class OC_Filestorage_Local extends OC_Filestorage_Common{
 		}
 	}
 	public function mkdir($path) {
-		return @mkdir($this->datadir.$path, 0770);
+		return @mkdir($this->datadir.$path, 0770, true);
 	}
 	public function rmdir($path) {
 		return @rmdir($this->datadir.$path);
