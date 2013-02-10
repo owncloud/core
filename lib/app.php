1aec7626e2879c7fb15d97322f44acbe5a0bb907
Georg Ehrke <developer@georgehrke.com>
2013-02-10 18:40:11 +0100

    revert changes in comment

--- lib/app.php
+++ lib/app.php
@@ -690,7 +690,7 @@ class OC_App{
 						$app['name'] == $remote['name']
 						// To set duplicate detection to use OCS ID instead of string name,
 						// enable this code, remove the line of code above,
-						// and add <ocs_id>[ID]</ocs_id> to info.xml of each 3rd Party:
+						// and add <ocs_id>[ID]</ocs_id> to info.xml of each 3rd party app:
 						// OR $app['ocs_id'] == $remote['ocs_id']
 						) {
 						unset( $remoteApps[$key]);
