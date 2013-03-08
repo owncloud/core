faf34a78cefbba4aa1e45e3f6513edbeb576bfc3
Philipp Kapfer <philipp.kapfer@gmx.at>
2013-03-08 18:15:20 +0100

    Modified app to dynamically register backends instead of hard-coding them Moved dependency check to the individual backends instead of the config

--- apps/files_external/appinfo/app.php
+++ apps/files_external/appinfo/app.php
@@ -21,3 +21,79 @@ OCP\App::registerAdmin('files_external', 'settings');
 if (OCP\Config::getAppValue('files_external', 'allow_user_mounting', 'yes') == 'yes') {
 	OCP\App::registerPersonal('files_external', 'personal');
 }
+
+OC_Mount_Config::registerBackend('\OC\Files\Storage\Local', array(
+	'backend' => 'Local',
+	'configuration' => array(
+		'datadir' => 'Location')));
+
+OC_Mount_Config::registerBackend('\OC\Files\Storage\AmazonS3', array(
+	'backend' => 'Amazon S3',
+	'configuration' => array(
+		'key' => 'Key',
+		'secret' => '*Secret',
+		'bucket' => 'Bucket')));
+
+OC_Mount_Config::registerBackend('\OC\Files\Storage\Dropbox', array(
+	'backend' => 'Dropbox',
+	'configuration' => array(
+		'configured' => '#configured',
+		'app_key' => 'App key',
+		'app_secret' => 'App secret',
+		'token' => '#token',
+		'token_secret' => '#token_secret'),
+	'custom' => 'dropbox'));
+
+OC_Mount_Config::registerBackend('\OC\Files\Storage\FTP', array(
+	'backend' => 'FTP',
+	'configuration' => array(
+		'host' => 'URL',
+		'user' => 'Username',
+		'password' => '*Password',
+		'root' => '&Root',
+		'secure' => '!Secure ftps://'),
+	'has_dependencies' => true));
+
+OC_Mount_Config::registerBackend('\OC\Files\Storage\Google', array(
+	'backend' => 'Google Drive',
+	'configuration' => array(
+		'configured' => '#configured',
+		'token' => '#token',
+		'token_secret' => '#token secret'),
+	'custom' => 'google'));
+
+OC_Mount_Config::registerBackend('\OC\Files\Storage\SWIFT', array(
+	'backend' => 'OpenStack Swift',
+	'configuration' => array(
+		'host' => 'URL',
+		'user' => 'Username',
+		'token' => '*Token',
+		'root' => '&Root',
+		'secure' => '!Secure ftps://')));
+
+OC_Mount_Config::registerBackend('\OC\Files\Storage\SMB', array(
+	'backend' => 'SMB / CIFS',
+	'configuration' => array(
+		'host' => 'URL',
+		'user' => 'Username',
+		'password' => '*Password',
+		'share' => 'Share',
+		'root' => '&Root'),
+	'has_dependencies' => true));
+
+OC_Mount_Config::registerBackend('\OC\Files\Storage\DAV', array(
+	'backend' => 'ownCloud / WebDAV',
+	'configuration' => array(
+		'host' => 'URL',
+		'user' => 'Username',
+		'password' => '*Password',
+		'root' => '&Root',
+		'secure' => '!Secure https://')));
+
+OC_Mount_Config::registerBackend('\OC\Files\Storage\SFTP', array(
+	'backend' => 'SFTP',
+	'configuration' => array(
+		'host' => 'URL',
+		'user' => 'Username',
+		'password' => '*Password',
+		'root' => '&Root')));
