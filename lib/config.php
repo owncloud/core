03b72883365cce9d12536a909f30daa3cee67183
Brice Maron <brice@bmaron.net>
2013-03-30 22:36:55 +0100

    Change logQuery from define() to OC::Config

--- lib/config.php
+++ lib/config.php
@@ -169,9 +169,6 @@ class OC_Config{
 		if (defined('DEBUG') && DEBUG) {
 			$content .= "define('DEBUG',true);\n";
 		}
-		if (defined('LOG_QUERIES') && LOG_QUERIES) {
-			$content .= "define('LOG_QUERIES',true);\n";
-		}
 		$content .= "\$CONFIG = ";
 		$content .= var_export(self::$cache, true);
 		$content .= ";\n";
