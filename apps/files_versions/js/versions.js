7a137327be023a959ed05a6ca5ecaffd9fe13d03
raghunayyar <me@iraghu.com>
2013-04-08 18:22:31 +0530

    [Initial Commit] : If Share dropdown is open, version closes.

--- apps/files_versions/js/versions.js
+++ apps/files_versions/js/versions.js
@@ -23,8 +23,17 @@ $(document).ready(function(){
 							createVersionsDropdown(filename, file);
 						});
 					}
+					if (('#dropdown-share').length > 0){
+						$('#dropdown-version').hide();
+					}
 				} else {
 					createVersionsDropdown(filename, file);
+				}
+				// To check if drop down for the share is open for any other file
+				if (($('#dropdown-share').length > 0) && $('#dropdown-share').hasClass('drop-share')){
+					if ($('#dropdown-version').length > 0) {
+						$('#dropdown-share').hide();
+					}
 				}
 			}
 		);
