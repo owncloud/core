c016e91113bf2d0ade79533b6580d921baeae429
Roman Geber <rgeber@owncloudapps.com>
2013-05-20 14:03:00 +0200

    Fixed "You don't have write permissions" message in public view

--- apps/files/templates/index.php
+++ apps/files/templates/index.php
@@ -51,7 +51,7 @@
 			</div>
 		</div>
 		<div id="file_action_panel"></div>
-	<?php elseif(!$_['isPublic']):?>
+	<?php elseif( !$_['isPublic'] ):?>
 		<div class="actions"><input type="button" disabled value="<?php p($l->t('You don’t have write permissions here.'))?>"></div>
 		<input type="hidden" name="dir" value="<?php p($_['dir']) ?>" id="dir">
 	<?php endif;?>
