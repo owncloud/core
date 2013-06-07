47a4513a3ccc1a5927045f4cc1ba779013af64c7
raghunayyar <me@iraghu.com>
2013-06-07 20:36:52 +0530

    corrects path of jquery.inview to user settings.

--- settings/users.php
+++ settings/users.php
@@ -12,7 +12,7 @@ OC_App::loadApps();
 OC_Util::addScript( 'settings', 'users' );
 OC_Util::addScript( 'core', 'multiselect' );
 OC_Util::addScript( 'core', 'singleselect' );
-OC_Util::addScript('core', 'jquery.inview');
+OC_Util::addScript('core', 'vendor/jquery/jquery.inview');
 OC_Util::addStyle( 'settings', 'settings' );
 OC_App::setActiveNavigationEntry( 'core_users' );
 
