be6a48c652354bb6175d7522ffb22800c7c7ca09
Florian Scholz <work@bgstyle.de>
2013-06-11 14:59:39 +0200

    - added rule "no empty THIRD-PARTY-PATH"

--- lib/templatelayout.php
+++ lib/templatelayout.php
@@ -109,7 +109,7 @@ class OC_TemplateLayout extends OC_Template {
 		foreach($styles as $style) {
 			// is it in 3rdparty?
 			if(strpos($style, '3rdparty') === 0 &&
-				self::appendIfExist($files, OC::$THIRDPARTYROOT, OC::$THIRDPARTYWEBROOT, $style.'.css')) {
+				self::appendIfExist($files, OC::$THIRDPARTYROOT, OC::$THIRDPARTYWEBROOT, $style.'.css') && OC::$THIRDPARTYROOT!=='' && OC::$THIRDPARTYWEBROOT!=='') {
 
 			// or in the owncloud root?
 			}elseif(self::appendIfExist($files, OC::$SERVERROOT, OC::$WEBROOT, "$style$fext.css" )) {
@@ -163,7 +163,7 @@ class OC_TemplateLayout extends OC_Template {
 		foreach($scripts as $script) {
 			// Is it in 3rd party?
 			if(strpos($script, '3rdparty') === 0 &&
-				self::appendIfExist($files, OC::$THIRDPARTYROOT, OC::$THIRDPARTYWEBROOT, $script.'.js')) {
+				self::appendIfExist($files, OC::$THIRDPARTYROOT, OC::$THIRDPARTYWEBROOT, $script.'.js') && OC::$THIRDPARTYROOT!=='' && OC::$THIRDPARTYWEBROOT!=='') {
 
 			// Is it in apps and overwritten by the theme?
 			}elseif(self::appendIfExist($files, OC::$SERVERROOT, OC::$WEBROOT, "themes/$theme/apps/$script$fext.js" )) {
