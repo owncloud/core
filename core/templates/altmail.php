d88a5cbe8ca0e771744bc6b8b97add05febac8a3
Stephan Arts <sarts@rssd.esa.int>
2013-06-13 20:37:14 +0200

    Update e-mail alttext

--- core/templates/altmail.php
+++ core/templates/altmail.php
@@ -1,7 +1,15 @@
-ownCloud
-
-<?php p($l->t('User %s shared the file "%s" with you. It is available for download here: %s', array($_['user_displayname'], $_['filename'], $_['link']))); ?>
+<?php
+if ($_['type'] === 'folder')
+{
+	p($l->t('%s shared »%s« with you. Open it here: %s', array($_['user_displayname'], $_['filename'], $_['link'])));
+}
+else
+{
+	p($l->t('%s shared »%s« with you. Download it here: %s', array($_['user_displayname'], $_['filename'], $_['link'])));
+}
+?>
 
 --
 ownCloud
 Your Cloud, Your Data, Your Way!
+http://ownCloud.org
