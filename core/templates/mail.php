924c041b91eb018c0c9397f9ec1b89bbe039884d
Stephan Arts <sarts@rssd.esa.int>
2013-06-13 13:21:05 +0200

    Add missing "." after the closing anchor tag

--- core/templates/mail.php
+++ core/templates/mail.php
@@ -9,7 +9,7 @@
 <tr>
 <td bgcolor="#f8f8f8" width="20px">&nbsp;</td>
 <td bgcolor="#f8f8f8" style="font-weight:normal; font-size:0.8em; line-height:1.2em; font-family:verdana,'arial',sans;">
-<?php print_unescaped($l->t('User %s shared the file "%s" with you. It is available for download <a href="%s">here</a>', array($_['user_displayname'], $_['filename'], $_['link']))); ?>
+<?php print_unescaped($l->t('User %s shared the file "%s" with you. It is available for download <a href="%s">here</a>.', array($_['user_displayname'], $_['filename'], $_['link']))); ?>
 </td>
 </tr>
 <tr><td bgcolor="#f8f8f8" colspan="2">&nbsp;</td></tr>
