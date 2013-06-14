410c184b678aa9ff6a05a2ba3ed6d7acdcdefe21
Stephan Arts <stephan@xfce.org>
2013-06-14 16:31:05 +0200

    Minor fixes

--- core/templates/altmail.php
+++ core/templates/altmail.php
@@ -1,7 +1,7 @@
 <?php
-print_unescaped($l->t("Hey there,\n\njust letting you know that %s shared »%s« with you.\nView it here: %s.\n\nCheers!", array($_['user_displayname'], $_['filename'], $_['link'])));
+print_unescaped($l->t("Hey there,\n\njust letting you know that %s shared »%s« with you.\nView it: %s\n\nCheers!", array($_['user_displayname'], $_['filename'], $_['link'])));
 ?>
 
 <?php
-p($l->t("ownCloud – web services under your control\nhttp://ownCloud.org"));
+p($l->t("--\nownCloud – web services under your control\nhttp://ownCloud.org"));
 ?>
