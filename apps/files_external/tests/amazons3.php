9440b319d7a5efa1fd915e42123348aa54e7ca8d
Christian Berendt <berendt@b1-systems.de>
2013-06-24 08:40:22 +0200

    make tearDown() workable

--- apps/files_external/tests/amazons3.php
+++ apps/files_external/tests/amazons3.php
@@ -28,12 +28,12 @@ class AmazonS3 extends Storage {
 	private $id;
 
 	public function setUp() {
-		$id = uniqid();
+		$this->id = uniqid();
 		$this->config = include('files_external/tests/config.php');
 		if ( ! is_array($this->config) or ! isset($this->config['amazons3']) or ! $this->config['amazons3']['run']) {
 			$this->markTestSkipped('AmazonS3 backend not configured');
 		}
-		$this->config['amazons3']['bucket'] = $id; // Make sure we have a new empty bucket to work in
+		$this->config['amazons3']['bucket'] = $this->id; // Make sure we have a new empty bucket to work in
 		$this->instance = new \OC\Files\Storage\AmazonS3($this->config['amazons3']);
 	}
 
