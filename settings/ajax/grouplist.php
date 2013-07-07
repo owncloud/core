8238b00e25c69c196d9a198f36c797439aa92c5f
raghunayyar <me@iraghu.com>
2013-07-07 21:21:05 +0530

    Adds a groupid for hyperlinks

--- settings/ajax/grouplist.php
+++ settings/ajax/grouplist.php
@@ -62,7 +62,8 @@ else {
 
 // convert them to the needed format
 foreach( $groups as $gid ) {
-	$groupname[] = array( 
+	$groupname[] = array(
+		'groupid' => str_replace(' ','', $gid ),
 		'name' => $gid,
 		'useringroup' => OC_Group::usersInGroup($gid, '', $limit, $offset),
 		'isAdmin' => !OC_User::isAdminUser($gid)
