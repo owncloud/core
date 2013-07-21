9153770020833e303e84cc97af5d813d20f0e5ac
Pellaeon Lin <nfsmwlin@gmail.com>
2013-07-21 11:53:16 +0800

    Handle errormsg in register.php

--- core/registration/templates/register.php
+++ core/registration/templates/register.php
@@ -1,7 +1,23 @@
 <?php if ($_['entered']): ?>
-	<div class="success"><p>
-		blablabla
-	</p></div>
+	<?php if (empty($_['errormsg'])): ?>
+		<div class="success"><p>
+		<?php print_unescaped($l->t('Thank you for registering, you should receive verification link in a few minutes.')); ?>
+		</p></div>
+	<?php else: ?>
+		<form action="<?php print_unescaped(OC_Helper::linkToRoute('core_registration_send_email')) ?>" method="post">
+			<fieldset>
+				<div class="errors"><p>
+				<?php print_unescaped($_['errormsg']); ?>
+				</p></div>
+				<p class="infield">
+					<input type="text" name="email" id="email" placeholder="" value="" required autofocus />
+					<label for="email" class="infield"><?php print_unescaped($l->t( 'Email' )); ?></label>
+					<img class="svg" src="<?php print_unescaped(image_path('', 'actions/mail.svg')); ?>" alt=""/>
+				</p>
+				<input type="submit" id="submit" value="<?php print_unescaped($l->t('Request verification link')); ?>" />
+			</fieldset>
+		</form>
+	<?php endif; ?>
 <?php else: ?>
 	<form action="<?php print_unescaped(OC_Helper::linkToRoute('core_registration_send_email')) ?>" method="post">
 		<fieldset>
