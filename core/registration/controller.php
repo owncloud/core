f1377424decec23526c3e43a12d940a9d3971828
Pellaeon Lin <nfsmwlin@gmail.com>
2013-07-24 17:41:38 +0800

    Save pending registration to database, send verification email.

--- core/registration/controller.php
+++ core/registration/controller.php
@@ -14,20 +14,62 @@ class OC_Core_Registration_Controller {
 		// Check if user with this email already exists
 		// This function wll be implemented in the future with searchValue function in OC_Preference
 
+		$l = OC_L10N::get('core');
+
 		if ( !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
-			$l = OC_L10N::get('core');
 			self::displayRegisterPage($l->t('Email address you entered is not valid'), true);
 			return;
 		}
 
 		// Check domain (not implemented)
 
+		$token = self::savePendingRegistration($_POST['email']);
+		if ( $token === false ) {
+			self::displayRegisterPage($l->t('There is already a pending registration with this email'), true);
+			return;
+		} elseif ( strlen($token) === 64 ) {
+			$link = OC_Helper::linkToRoute('core_registration_register_form',
+				array('token' => $token));
+			$link = OC_Helper::makeURLAbsolute($link);
+			$from = OCP\Util::getDefaultEmailAddress('register');
+			$tmpl = new OC_Template('core/registration', 'email');
+			$tmpl->assign('link', $link, false);
+			$msg = $tmpl->fetchPage();
+			try {
+				OC_Mail::send($_POST['email'], 'ownCloud User', $l->t('Verify your ownCloud registration request'), $msg, $from, 'ownCloud');
+			} catch (Exception $e) {
+				OC_Template::printErrorPage( 'A problem occurs during sending the e-mail please contact your administrator.');
+			}
+			self::displayRegisterPage('', true);
+		}
 	}
 
 	public static function registerForm($args) {
 	}
 
 	public static function createAccount($args) {
+	}
+
+	/**
+	 * @brief Save a registration request to database
+	 * @param string $email Request from this email
+	 * @return false if a request with the email already exists, returns the generated token when success
+	 */
+	public static function savePendingRegistration($email) {
+		// Check if the email does exist
+		$query = OC_DB::prepare('SELECT `email` FROM `*PREFIX*pending_regist` WHERE `email` = ? ');
+		$values=$query->execute(array($email))->fetchAll();
+		$exists=(count($values)>0);
+
+		if ( $exists ) {
+			return false;
+		} else {
+			$query = OC_DB::prepare( 'INSERT INTO `*PREFIX*pending_regist`'
+				.' ( `email`, `token`, `requested` ) VALUES( ?, ?, ? )' );
+			$token = hash('sha256', OC_Util::generate_random_bytes(30).OC_Config::getValue('passwordsalt', ''));
+			$query->execute(array( $email, $token, time() ));
+			return $token;
+		}
 	}
 }
 
---
+++ core/registration/templates/email.php
@@ -0,0 +1,2 @@
+<?php
+echo str_replace('{link}', $_['link'], $l->t('To create a new account on ownCloud, just click the following link: {link}'));
