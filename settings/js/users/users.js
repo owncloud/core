01097e0977ccf3cf25e030a41ec6aca9064382d8
raghunayyar <me@iraghu.com>
2013-07-26 00:24:50 +0530

    Adds ngBlur and ngFocus directives.

--- settings/js/users/users.js
+++ settings/js/users/users.js
@@ -180,3 +180,27 @@ usersmanagement.directive('loading', function() {
 		}		
 	}
 });
+
+/* ngFocus and ngBlur Directives */
+
+usersmanagement.directive('ngFocus', ['$parse', function($parse) {
+    return function(scope, element, attr) {
+        var fn = $parse(attr['ngFocus']);
+        element.on('focus', function(event) {
+            scope.$apply(function() {
+                fn(scope, {$event:event});
+            });
+        });
+    };
+}]);
+
+usersmanagement.directive('ngBlur', ['$parse', function($parse) {
+    return function(scope, element, attr) {
+        var fn = $parse(attr['ngBlur']);
+        element.on('blur', function(event) {
+            scope.$apply(function() {
+                fn(scope, {$event:event});
+            });
+        });
+    };
+}]);
