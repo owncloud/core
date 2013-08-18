7b3fdea46e4e341825c2ba317c4c4e37ed5c78ab
raghunayyar <me@iraghu.com>
2013-08-18 20:02:26 +0530

    Implements Group Deletion ServerSide.

--- settings/ajax/removegroup.php
+++ settings/ajax/removegroup.php
@@ -5,10 +5,8 @@ OC_JSON::checkAdminUser();
 
 $l = OC_L10n::get('settings');
 
-$params = json_decode(file_get_contents('php://input'), true);
-
-$groupname = $params["groupname"];
-
+$groupname = $_GET["groupname"];
+var_dump($params);
 // Return Success story
 if( OC_Group::deleteGroup( $groupname )) {
 	OC_JSON::success(array("result" => array( "groupname" => $groupname )));
