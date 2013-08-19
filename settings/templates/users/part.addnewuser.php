b47a2b579ff07283164c83d1f1bc5150132edfa7
raghunayyar <me@iraghu.com>
2013-08-19 16:08:02 +0530

    Removes Checks for Users and Groups.

--- settings/templates/users/part.addnewuser.php
+++ settings/templates/users/part.addnewuser.php
@@ -25,9 +25,8 @@
 <div ng-controller="addUserController">
 	<fieldset>
 		<form id="newuser" autocomplete="off" name="createuser_form">
-			<input class="" id="newusername" type="text" name="loginnamefield" placeholder="<?php p($l->t('Username')) ?>" ng-minlength="3" required ng-model="newuser" title="Minimum 3 characters" original-title="Minimum 3 chracters"/>
-			<!--<span class="error" id="nologinname" ng-show="createuser_form.loginnamefield.$error.minlength">Minimum 3 characters</span>-->
-			<input id="newuserpassword" name="passwordfield" type="password" placeholder="<?php p($l->t('Password')) ?>" ng-model="password" title="Minimum 3 chracters" original-title="Minimum 3 chracters" required/>
+			<input class="" id="newusername" type="text" name="loginnamefield" placeholder="<?php p($l->t('Username')) ?>" ng-minlength="3" required ng-model="newuser" />
+			<input id="newuserpassword" name="passwordfield" type="password" placeholder="<?php p($l->t('Password')) ?>" ng-model="password" />
 			<select
 				chosen multiple id="newusergroups"
 				class="groupselect" title="<?php p($l->t('Groups'))?>"
@@ -35,10 +34,16 @@
 				data-placeholder="Select Group.."
 				no-result-text="No Such Group.."
 				ng-model="selectedgroup"
-				ng-options="pergroup.name for pergroup in allgroups.result">
+				ng-options="pergroup.name for pergroup in allgroups.result"
+				required>
 				<option value=""></option>
 			</select>
-			<button title="<?php p($l->t('Create'))?>" ng-click="saveuser()" ng-disabled="createuser_form.$invalid"><?php p($l->t('Add User'))?></button>
+			<button
+				title="<?php p($l->t('Create'))?>"
+				ng-click="saveuser()" 
+				ng-disabled="createuser_form.$invalid">
+					<?php p($l->t('Add User'))?>
+			</button>
 		</form>
 	</fieldset>
 </div>
