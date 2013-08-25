14228b81c980e8875ac0f4933b16b5345b6d58b4
raghunayyar <me@iraghu.com>
2013-08-25 15:38:41 +0530

    Fixes Display Name Updation Issue.

--- settings/js/users/public/app.js
+++ settings/js/users/public/app.js
@@ -452,7 +452,7 @@ usersmanagement.controller('userlistController',
 			$scope.allgroups = GroupService.getByGroupId().get();
 			
 			$scope.updateDisplayName = function(userid,displayname) {
-				UserService.updateName().save({ username : userid }, { displayName : displayname })
+				UserService.updateName(userid,displayname);
 			}
 			/* Deletes Users */
 			
