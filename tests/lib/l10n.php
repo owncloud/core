d224d31b42b74517bdd7e1a56f6bfae7c3b5315d
Pellaeon Lin <nfsmwlin@gmail.com>
2013-08-27 23:26:13 +0800

    Remove tests

--- tests/lib/l10n.php
+++ tests/lib/l10n.php
@@ -8,17 +8,6 @@
 
 class Test_L10n extends PHPUnit_Framework_TestCase {
 
-	public function testLanguageExists() {
-		$this->assertTrue(OC_L10N::languageExists('core', 'en')); // en should always be available
-		$this->assertTrue(OC_L10N::languageExists('files', 'zh_TW')); // /apps/files/l10n/zh_TW.php should exist for this to be true
-	}
-
-	public function testCheckPreferenceLanguage() {
-		$this->assertFalse(OC_L10N::checkPreferenceLanguage(array('en', 'fr')));
-		$this->assertFalse(OC_L10N::checkPreferenceLanguage());
-		// Currently we don't test when user is logged in
-	}
-
 	public function testGermanPluralTranslations() {
 		$l = new OC_L10N('test');
 		$transFile = OC::$SERVERROOT.'/tests/data/l10n/de.php';
