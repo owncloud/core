49390644b88ec2c635b6bacb1af774550ef5bdca
planetpaul <planetpaul@users.noreply.github.com>
2013-08-27 22:50:04 +0200

    Edited to use customizable email footers

--- core/templates/altmail.php
+++ core/templates/altmail.php
@@ -1,7 +1,4 @@
 <?php
 print_unescaped($l->t("Hey there,\n\njust letting you know that %s shared %s with you.\nView it: %s\n\nCheers!", array($_['user_displayname'], $_['filename'], $_['link'])));
 ?>
-
---
-<?php p($theme->getName() . ' - ' . $theme->getSlogan()); ?>
-<?php print_unescaped("\n".$theme->getBaseUrl());
+<?php p($theme->getEmailFooter()); ?>
