21db34305d5b464620a28c2aaaa5a6f6fb22f61e
raghunayyar <me@iraghu.com>
2013-08-29 14:46:59 +0530

    Fixes Multiselect Issue, Removes All dependancies. \o/

--- settings/js/users/app/directive.js
+++ settings/js/users/app/directive.js
@@ -69,47 +69,12 @@ usersmanagement.directive('ngBlur',
 
 usersmanagement.directive('multiselectDropdown', [function() {
 	return function(scope, element, attributes) {
-		element = $(element[0]);
 		
-        element.multiselect({
-            buttonClass : 'btn btn-small',
-            buttonContainer : '<div class="btn-group" />',
-            maxHeight : 200,
-            enableFiltering : true,
-            enableCaseInsensitiveFiltering: true,
-            buttonText : function(options) {
-                if (options.length == 0) {
-                    return element.data()['placeholder'] + ' <b class="caret"></b>';
-                } else if (options.length > 1) {
-                    return _.first(options).text 
-                    + ' + ' + (options.length - 1)
-                    + ' more selected <b class="caret"></b>';
-                } else {
-                    return _.first(options).text
-                    + ' <b class="caret"></b>';
-                }
-            },
-            // Replicate the native functionality on the elements so
-            // that angular can handle the changes for us.
-            onChange: function (optionElement, checked) {
-                optionElement.removeAttr('selected');
-                if (checked) {
-                    optionElement.attr('selected', 'selected');
-                }
-                element.change();
-            }
-            
-        });
-        // Watch for any changes to the length of our select element
-        scope.$watch(function () {
-            return element[0].length;
-        }, function () {
-            element.multiselect('rebuild');
-        });
-        
-        // Watch for any changes from outside the directive and refresh
-        scope.$watch(attributes.ngModel, function () {
-            element.multiselect('refresh');
+		/* Use as jQuery */
+		element = $(element[0]);
+        element.multiSelect({
+			title: 'More Groups..',
+			maxWIdth:'200px'
         });
 	}
 }]);
