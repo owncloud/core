784bd861494f6124fbf17c849ccdea6f21965154
raghunayyar <me@iraghu.com>
2013-09-06 19:13:09 +0530

    Add group on top, to maintain consistency with other apps.

--- settings/templates/users/main.php
+++ settings/templates/users/main.php
@@ -24,8 +24,8 @@
 
 <div id="user-settings" ng-app="usersmanagement">
 	<div id="app-navigation">
+        <?php print_unescaped($this->inc('users/part.addnewgroup')); ?>
 		<?php print_unescaped($this->inc('users/part.prioritygroups')); ?>
-		<?php print_unescaped($this->inc('users/part.addnewgroup')); ?>
 		<?php print_unescaped($this->inc('users/part.grouplist')); ?>
 		<?php print_unescaped($this->inc('users/part.usersettings')); ?>
 	</div>
