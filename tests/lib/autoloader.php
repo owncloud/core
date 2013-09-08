8155f3b0044c9ec38428a6294f224d63a0a3102c
Bart Visscher <bartv@thisnet.nl>
2013-09-08 22:51:11 +0200

    Fix unit tests for autoloader

--- tests/lib/autoloader.php
+++ tests/lib/autoloader.php
@@ -19,56 +19,57 @@ class AutoLoader extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testLeadingSlashOnClassName() {
-		$this->assertEquals(array('files/storage/local.php'), $this->loader->findClass('\OC\Files\Storage\Local'));
+		$this->assertEquals(\OC::$SERVERROOT.'/lib/files/storage/local.php', $this->loader->findClass('\OC\Files\Storage\Local'));
 	}
 
 	public function testNoLeadingSlashOnClassName() {
-		$this->assertEquals(array('files/storage/local.php'), $this->loader->findClass('OC\Files\Storage\Local'));
+		$this->assertEquals(\OC::$SERVERROOT.'/lib/files/storage/local.php', $this->loader->findClass('OC\Files\Storage\Local'));
 	}
 
 	public function testLegacyPath() {
-		$this->assertEquals(array('legacy/files.php', 'files.php'), $this->loader->findClass('OC_Files'));
+		$this->assertEquals(\OC::$SERVERROOT.'/lib/legacy/config.php', $this->loader->findClass('OC_Config'));
 	}
 
 	public function testClassPath() {
-		$this->loader->registerClass('Foo\Bar', 'foobar.php');
-		$this->assertEquals(array('foobar.php'), $this->loader->findClass('Foo\Bar'));
+		// can't find it without registerClass
+		$this->assertFalse($this->loader->findClass('Patchwork\Utf8'));
+		$this->loader->registerClass('Patchwork\Utf8', 'Patchwork/Utf8.php');
+		$this->assertEquals(\OC::$SERVERROOT.'/3rdparty/Patchwork/Utf8.php', $this->loader->findClass('Patchwork\Utf8'));
 	}
 
 	public function testPrefixNamespace() {
-		$this->loader->registerPrefix('Foo', 'foo');
-		$this->assertEquals(array('foo/Foo/Bar.php'), $this->loader->findClass('Foo\Bar'));
+		$this->assertFalse($this->loader->findClass('Patchwork\Utf8'));
+		$this->loader->registerPrefix('Patchwork', '3rdparty');
+		$this->assertEquals(\OC::$SERVERROOT.'/3rdparty/Patchwork/Utf8.php', $this->loader->findClass('Patchwork\Utf8'));
 	}
 
 	public function testPrefix() {
-		$this->loader->registerPrefix('Foo_', 'foo');
-		$this->assertEquals(array('foo/Foo/Bar.php'), $this->loader->findClass('Foo_Bar'));
+		$this->loader->registerPrefix('Sabre_', '3rdparty');
+		$this->assertEquals(\OC::$SERVERROOT.'/3rdparty/Sabre/DAV/Node.php', $this->loader->findClass('Sabre_DAV_Node'));
 	}
 
 	public function testLoadTestNamespace() {
-		$this->assertEquals(array('tests/lib/foo/bar.php'), $this->loader->findClass('Test\Foo\Bar'));
+		$this->assertEquals(__FILE__, $this->loader->findClass('Test\AutoLoader'));
 	}
 
 	public function testLoadTest() {
-		$this->assertEquals(array('tests/lib/foo/bar.php'), $this->loader->findClass('Test_Foo_Bar'));
+		$this->assertEquals(__FILE__, $this->loader->findClass('Test_Autoloader'));
 	}
 
 	public function testLoadCoreNamespace() {
-		$this->assertEquals(array('foo/bar.php'), $this->loader->findClass('OC\Foo\Bar'));
+		$this->assertEquals(\OC::$SERVERROOT.'/lib/files/storage/local.php', $this->loader->findClass('OC\Files\Storage\Local'));
 	}
 
 	public function testLoadCore() {
-		$this->assertEquals(array('legacy/foo/bar.php', 'foo/bar.php'), $this->loader->findClass('OC_Foo_Bar'));
+		$this->assertEquals(\OC::$SERVERROOT.'/lib/files/storage/local.php', $this->loader->findClass('OC_Files_Storage_Local'));
 	}
 
 	public function testLoadPublicNamespace() {
-		$this->assertEquals(array('public/foo/bar.php'), $this->loader->findClass('OCP\Foo\Bar'));
+		$this->assertEquals(\OC::$SERVERROOT.'/lib/public/db.php', $this->loader->findClass('OCP\DB'));
 	}
 
 	public function testLoadAppNamespace() {
-		$result = $this->loader->findClass('OCA\Files\Foobar');
-		$this->assertEquals(2, count($result));
-		$this->assertStringEndsWith('apps/files/foobar.php', $result[0]);
-		$this->assertStringEndsWith('apps/files/lib/foobar.php', $result[1]);
+		$this->assertStringEndsWith('apps/files/settings.php', $this->loader->findClass('OCA\Files\Settings'));
+		$this->assertStringEndsWith('apps/files/lib/helper.php', $this->loader->findClass('OCA\Files\Helper'));
 	}
 }
