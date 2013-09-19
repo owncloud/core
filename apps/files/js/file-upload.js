27d4c76efa218fe559405e95456ec89e9b22d79e
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-09-19 11:32:56 +0200

    fix double translation of error message

--- apps/files/js/file-upload.js
+++ apps/files/js/file-upload.js
@@ -307,7 +307,7 @@ $(document).ready(function() {
 					var file = result[0];
 				} else {
 					data.textStatus = 'servererror';
-					data.errorThrown = t('files', result.data.message);
+					data.errorThrown = result.data.message; // error message has been translated on server
 					var fu = $(this).data('blueimp-fileupload') || $(this).data('fileupload');
 					fu._trigger('fail', e, data);
 				}
