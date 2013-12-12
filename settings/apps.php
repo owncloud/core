a3727a2954bda2d483397c396a0d598270547881
raghunayyar <me@iraghu.com>
2013-12-13 03:17:50 +0530

    Initial Commit: Old Styles to New Directory

--- settings/apps.php
+++ settings/apps.php
@@ -25,7 +25,7 @@ OC_Util::checkAdminUser();
 OC_App::loadApps();
 
 // Load the files we need
-OC_Util::addStyle( "settings", "settings" );
+OC_Util::addStyle( "settings", "apps/apps" );
 OC_App::setActiveNavigationEntry( "core_apps" );
 
 $combinedApps = OC_App::listAllApps();
