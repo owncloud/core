e56142d22384dd5b3ce9bce8f5e4f49ce4b3d777
raghunayyar <me@iraghu.com>
2013-12-14 19:13:11 +0530

    CSS: Adds font-weight to active apps.

--- settings/templates/apps/part.applist.php
+++ settings/templates/apps/part.applist.php
@@ -10,12 +10,14 @@
 		<?php if ( isset( $app['ocs_id'] ) ) { print_unescaped("data-id-ocs=\"{".OC_Util::sanitizeHTML($app['ocs_id'])."}\""); } ?>
 			data-type="<?php p($app['internal'] ? 'internal' : 'external') ?>" data-installed="1">
 		<a class="app<?php if(!$app['internal']) p(' externalapp') ?>"
-			href="?appid=<?php p($app['id']) ?>"><?php p($app['name']) ?></a>
+			href="?appid=<?php p($app['id']) ?>"><?php p($app['name']); ?></a>
 		<?php  if(!$app['internal'])
 			print_unescaped('<span class="'.OC_Util::sanitizeHTML($app['internalclass']).' list">'.OC_Util::sanitizeHTML($app['internallabel']).'</span>') ?>
 	</li>
 	<?php endforeach;?>
 	<li>
-		<a class="app-external" target="_blank" href="http://apps.owncloud.com"><?php p($l->t('More Apps'));?> …</a>
+		<a class="app-external" target="_blank" href="http://apps.owncloud.com">
+			<?php p($l->t('More Apps'));?> …
+		</a>
 	</li>
 </ul>
