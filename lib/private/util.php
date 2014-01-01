32093bad1881ab1835152ad4ac2029c4e157ec46
thequestion <loic.rouch@gmail.com>
2014-01-01 23:20:57 +0100

    Update util.php
    Remove write perms on "files" user's directory.
    Effect: Remove "New" button and display message that user doesn't have rights to write in this folder.

--- lib/private/util.php
+++ lib/private/util.php
@@ -75,6 +75,11 @@ class OC_Util {
 				mkdir( $userDirectory, 0755, true );
 				OC_Util::copySkeleton($userDirectory);
 			}
+			if( OC_Preferences::getValue($user, 'files', 'quota') === 'disallow' ) {
+				chmod( $userDirectory, 0555 );
+			} else {
+				chmod( $userDirectory, 0755 );
+			}
 			//jail the user into his "home" directory
 			\OC\Files\Filesystem::init($user, $userDir);
 
