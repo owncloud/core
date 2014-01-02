b235258c0ead0a28bebc344f4028f8822f9d53a5
Adam Williamson <awilliam@redhat.com>
2014-01-01 18:00:41 -0800

    Use autoloader for Archive
    This makes life easier for downstreams who unbundle third party libraries. I've tested (in a very ugly and probably stupid way) that this will find Archive/Tar.php from OC's 3rdparty directory if it's there, or a system copy if OC's 3rdparty copy isn't there, on a test Fedora 20 instance.

--- lib/base.php
+++ lib/base.php
@@ -413,6 +413,7 @@ class OC {
 		self::$loader->registerPrefix('Sabre\\VObject', '3rdparty');
 		self::$loader->registerPrefix('Sabre_', '3rdparty');
 		self::$loader->registerPrefix('Patchwork', '3rdparty');
+		self::$loader->registerPrefix('Archive', '3rdparty');
 		spl_autoload_register(array(self::$loader, 'load'));
 
 		// set some stuff
