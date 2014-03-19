362a0d0e12f7fd3f121b67b4507be85475795f96
Oskar Hollmann <oskarhollmann@gmail.com>
2014-03-19 14:05:25 +0100

    Fix 'Upload button tooltip doesn't disappear' #7461  @jancborchardt please review my fix for the mentioned issue.

--- apps/files/js/file-upload.js
+++ apps/files/js/file-upload.js
@@ -361,6 +361,7 @@ $(document).ready(function() {
 			 */
 			done:function(e, data) {
 				OC.Upload.log('done', e, data);
+				$('.tipsy').remove(); // otherwise the 'Upload max.' tooltip remains visible
 				// handle different responses (json or body from iframe for ie)
 				var response;
 				if (typeof data.result === 'string') {
