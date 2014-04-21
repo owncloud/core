563168d84d49135346abe14e5b9f360c0a2ec105
Radist Morse <radist.morse@gmail.com>
2014-04-21 16:05:26 +0200

    second draft. already-existed shares now work too, 'localroot' stuff was moved to server-side, admin check

--- apps/files_external/js/local.js
+++ apps/files_external/js/local.js
@@ -1,70 +1,87 @@
-var localroot = '/home';
-
 $(document).ready(function() {
-	
-	/*$('#externalStorage tbody tr.\\\\OC\\\\Files\\\\Storage\\\\Local').each(function() {
-			var local_path = $(this).find('[data-parameter="datadir"]');
-			if (local_path.substring(0, localroot.length) == localroot) {
-				var config = $(this).find('td.configuration');
-				$(config).append('<span id="localroot"></span>');
-				$(config).append('<a class="button local-dir-up ui-state-disabled">&larr;</a>');
-				$(config).append('<a class="button local-dir-down ui-state-disabled">&rarr;</a>');
-				var dirs = local_path.substring(localroot.length).split(/\\|\//);
-				var cur_dir = localroot;
-				createDirSelector($(config).find('span').last(), cur_dir, false);
-				$.each(dirs, function (dir) {
-					if (dir.length > 0) {
-						var last_select = $(config).find('span').last().find('selector');
-					}
-				});
-			}
-			
-	});*/
+	//construct selectors for existing shares
+	$('#externalStorage tbody tr.\\\\OC\\\\Files\\\\Storage\\\\Local').each(function() {
+		var localPath = $(this).find('[data-parameter="datadir"]').val();
+		var config = $(this).find('td.configuration');
+		$(config).append('<span id="localroot"></span>');
+		$(config).append('<a class="button local-dir-up ui-state-disabled">&larr;</a>');
+		$(config).append('<a class="button local-dir-down ui-state-disabled">&rarr;</a>');
 
-	$('#externalStorage').on('change', '#selectBackend', function(event) {
+		$.post(OC.filePath('files_external', 'ajax', 'local.php'), { path: localPath, ascendpath: true }, function(result) {
+			if (result && result.status === 'success') {
+				var lastSpan = $(config).find('span').last();
+				var keys = Object.keys(result.data).sort();
+				var lastValue = result.data[keys[keys.length -1]];
+				for(var i = 0; i < keys.length; i++) {
+					var key = keys[i];
+					var mySelect = function(value) {
+						return function(selector) {
+							$(selector).val(value);
+							if (value === lastValue) {
+								$(selector).trigger('change');
+							}
+						}
+					}
+					createDirSelector(lastSpan, key, mySelect(result.data[key]));
+					$(lastSpan).append('<span id="'+key+'"></span>');
+					lastSpan = $(lastSpan).find('span').last();
+				}
+			} else {
+				OC.dialogs.alert(result.data.message, t('files_external', 'Error getting the predecessors for ' + local_path ));
+			}
+		});
+	});
+
+	//construct selectors for newly created shares
+	$('#externalStorage').on('change', '#selectBackend', function() {
 		var tr = $('#externalStorage tbody tr').eq(-2);
 		if ($(tr).find('.backend').data('class') === '\\OC\\Files\\Storage\\Local') {
 			var config = $(tr).find('td.configuration');
 			$(config).append('<span id="localroot"></span>');
 			$(config).append('<a class="button local-dir-up ui-state-disabled">&larr;</a>');
 			$(config).append('<a class="button local-dir-down ui-state-disabled">&rarr;</a>');
-			createDirSelector($(config).find('span'),localroot);
+			createDirSelector($(config).find('span'),'');
 		}
 	});
 
+	//ascend one level
 	$('#externalStorage').on('click', '.local-dir-up:not(.ui-state-disabled)', function(event) {
 		event.preventDefault();
+		$(this).addClass('ui-state-disabled');
 		var config = $(this).parent();
 		var lastSpan = $(config).find('span').last();
 		if ( $(lastSpan).attr('id') !== 'localroot') {
-			var parent = $(lastSpan).parent();
 			$(lastSpan).remove();
 		}
 		$(config).find('span').last().find('select').trigger('change');
 	});
 
+	//descend one level
 	$('#externalStorage').on('click', '.local-dir-down:not(.ui-state-disabled)', function(event) {
 		event.preventDefault();
+		$(this).addClass('ui-state-disabled');
 		var config = $(this).parent();
-                var downButton = $(config).find('.local-dir-down');
+		var downButton = $(config).find('.local-dir-down');
 		var lastSpan = $(config).find('span').last();
 		var lastSelect = $(lastSpan).find('select');
-		$.post(OC.filePath('files_external', 'ajax', 'local.php'), { path: $(lastSelect).find('option:selected').val(), isnotempty: true }, function(result) {
+		var currentPath = $(lastSelect).find('option:selected').val();
+		$.post(OC.filePath('files_external', 'ajax', 'local.php'), { path: currentPath, isnotempty: true }, function(result) {
 			if (result && result.status === 'success') {
 				if (result.data) {
 					$(downButton).removeClass('ui-state-disabled');
 					$(lastSpan).append('<span id="'+$(lastSelect).attr('id')+'"></span>');
-					createDirSelector($(lastSpan).find('span'),$(lastSelect).find('option:selected').val());
+					createDirSelector($(lastSpan).find('span'),currentPath);
 				} else {
 					$(downButton).addClass('ui-state-disabled');
 				}
 			} else {
-				OC.dialogs.alert(result.data.message, t('files_external', 'Error getting the local directory listing in' + $(lastSelect).find('option:selected').val() ));
+				OC.dialogs.alert(result.data.message, t('files_external', 'Error getting the local directory listing in' + currentPath ));
 			}
 		});
 	});
 
-	$('#externalStorage').on('change', '.local-file-chooser', function(event) {
+	//some selector was changed
+	$('#externalStorage').on('change', '.local-file-chooser', function() {
 		$(this).parent().find('span').remove();
 
 		var path = $(this).find('option:selected').val();
@@ -90,22 +107,28 @@ $(document).ready(function() {
 		});
 		if ($(this).parents('td.configuration').find('[data-parameter="datadir"]').val() !== path) {
 			$(this).parents('td.configuration').find('[data-parameter="datadir"]').val(path);
+			$(this).parents('td.configuration').find('[data-parameter="datadir"]').trigger('keyup');
 		}
 	});
 
-	function createDirSelector(dirSpan, path, trigger = true) {
+	//places a selector to $(dirSpan) with the subfolders of 'path'
+	//calls callback when (if) done if provided, or triggers the selector change otherwise
+	function createDirSelector(dirSpan, path, callback) {
 		$.post(OC.filePath('files_external', 'ajax', 'local.php'), { path: path }, function(result) {
 			if (result && result.status === 'success') {
-				$(dirSpan).append('<select id="'+path+'" class="local-file-chooser"></select>');
+				$(dirSpan).prepend('<select id="'+path+'" class="local-file-chooser"></select>');
+				var selector = $(dirSpan).find('select').first();
 				$.each(result.data, function (dir, path) {
-					$(dirSpan).find('select')
+					$(selector)
 					   .append($('<option></option>')
 					   .attr('value',path)
 					   .text(dir));
-					if (trigger) {
-						$(dirSpan).find('select').trigger('change');
-					}
 				});
+				if (typeof callback === 'function') {
+					callback(selector);
+				} else {
+					$(dirSpan).find('select').trigger('change');
+				}
 			} else {
 				OC.dialogs.alert(result.data.message, t('files_external', 'Error getting the local directory listing in ' + path ));
 			}
