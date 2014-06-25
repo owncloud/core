dbb9fbb66d41d8df2c57c6632fa80d84b9ad8ac6
Thomas Müller <thomas.mueller@tmit.eu>
2014-06-25 16:56:46 +0200

    fixing sql statement generation on index creation - keyword have to be escaped here as well

--- lib/private/db/dbal/platforms/dblibplatform.php
+++ lib/private/db/dbal/platforms/dblibplatform.php
@@ -2,6 +2,9 @@
 
 namespace OC\DB\DBAL\Platforms;
 
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Schema\Index;
+
 class DblibPlatform extends  \Realestate\MssqlBundle\Platforms\DblibPlatform
 {
 	/**
@@ -11,5 +14,59 @@ class DblibPlatform extends  \Realestate\MssqlBundle\Platforms\DblibPlatform
 	{
 		return 'VARCHAR(MAX)';
 	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getIndexFieldDeclarationListSQL(array $fields)
+	{
+		$ret = array();
+
+		foreach ($fields as $field => $definition) {
+			if (!is_array($definition)) {
+				$field = $definition;
+			}
+			$ret[] = $this->quoteIdentifier($field);
+		}
+
+		return implode(', ', $ret);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getCreateIndexSQL(Index $index, $table)
+	{
+		$constraint = AbstractPlatform::getCreateIndexSQL($index, $table);
+
+		if ($index->isUnique() && !$index->isPrimary()) {
+			$constraint = $this->_appendUniqueConstraintDefinition($constraint, $index);
+		}
+
+		return $constraint;
+	}
+
+	/**
+	 * Extend unique key constraint with required filters
+	 *
+	 * @param string $sql
+	 * @param Index $index
+	 *
+	 * @return string
+	 */
+	private function _appendUniqueConstraintDefinition($sql, Index $index)
+	{
+		$fields = array();
+		foreach ($index->getColumns() as $field => $definition) {
+			if (!is_array($definition)) {
+				$field = $definition;
+			}
+
+			$fields[] = $this->quoteIdentifier($field) . ' IS NOT NULL';
+		}
+
+		return $sql . ' WHERE ' . implode(' AND ', $fields);
+	}
+
 
 }
