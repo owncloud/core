2872127ea3b0892f021fa3b21bc03bdb359c91ab
SA <stephen@mthosting.net>
2014-12-15 08:48:49 -0500

    Prep for stfp custom port

--- apps/files_external/lib/sftp.php
+++ apps/files_external/lib/sftp.php
@@ -16,6 +16,7 @@ class SFTP extends \OC\Files\Storage\Common {
 	private $user;
 	private $password;
 	private $root;
+	private $port = 22;
 
 	/**
 	* @var \Net_SFTP
@@ -37,7 +38,10 @@ class SFTP extends \OC\Files\Storage\Common {
 		require_once 'Net/SFTP/Stream.php';
 
 		$this->host = $params['host'];
+		
 		$proto = strpos($this->host, '://');
+
+		
 		if ($proto != false) {
 			$this->host = substr($this->host, $proto+3);
 		}
@@ -67,7 +71,7 @@ class SFTP extends \OC\Files\Storage\Common {
 		}
 
 		$hostKeys = $this->readHostKeys();
-		$this->client = new \Net_SFTP($this->host);
+		$this->client = new \Net_SFTP($this->host,$this->port);
 
 		// The SSH Host Key MUST be verified before login().
 		$currentHostKey = $this->client->getServerPublicHostKey();
@@ -98,7 +102,7 @@ class SFTP extends \OC\Files\Storage\Common {
 	}
 
 	public function getId(){
-		return 'sftp::' . $this->user . '@' . $this->host . '/' . $this->root;
+		return 'sftp::' . $this->user . '@' . $this->host . ':' . $this->port . '/' . $this->root;
 	}
 
 	/**
@@ -316,7 +320,7 @@ class SFTP extends \OC\Files\Storage\Common {
 		// Do not pass the password here. We want to use the Net_SFTP object
 		// supplied via stream context or fail. We only supply username and
 		// hostname because this might show up in logs (they are not used).
-		$url = 'sftp://'.$this->user.'@'.$this->host.$this->root.$path;
+		$url = 'sftp://'.$this->user.'@'.$this->host.':'.$this->port.$this->root.$path;
 		return $url;
 	}
 }
