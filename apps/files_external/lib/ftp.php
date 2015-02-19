5c1d26e258d1b79c4c7f3d9ff403aed323c0b556
Robin Appelman <icewind@owncloud.com>
2015-02-19 15:58:35 +0100

    Switch ftp backend to flysystem

--- apps/files_external/lib/ftp.php
+++ apps/files_external/lib/ftp.php
@@ -28,20 +28,28 @@
  */
 namespace OC\Files\Storage;
 
-class FTP extends \OC\Files\Storage\StreamWrapper{
+use OCA\Files_External\Flysystem;
+use OCA\Files_External\FtpAdapter;
+use OCA\Files_External\PolyFill\CopyDirectory;
+
+class FTP extends Flysystem {
+	use CopyDirectory;
+
 	private $password;
 	private $user;
 	private $host;
 	private $secure;
-	private $root;
 
-	private static $tempFiles=array();
+	/**
+	 * @var \League\Flysystem\Adapter\Ftp
+	 */
+	private $adapter;
 
 	public function __construct($params) {
 		if (isset($params['host']) && isset($params['user']) && isset($params['password'])) {
-			$this->host=$params['host'];
-			$this->user=$params['user'];
-			$this->password=$params['password'];
+			$this->host = $params['host'];
+			$this->user = $params['user'];
+			$this->password = $params['password'];
 			if (isset($params['secure'])) {
 				if (is_string($params['secure'])) {
 					$this->secure = ($params['secure'] === 'true');
@@ -51,93 +59,28 @@ class FTP extends \OC\Files\Storage\StreamWrapper{
 			} else {
 				$this->secure = false;
 			}
-			$this->root=isset($params['root'])?$params['root']:'/';
-			if ( ! $this->root || $this->root[0]!='/') {
-				$this->root='/'.$this->root;
+			$this->root = isset($params['root']) ? $params['root'] : '/';
+			if (!$this->root || $this->root[0] != '/') {
+				$this->root = '/' . $this->root;
 			}
 			if (substr($this->root, -1) !== '/') {
 				$this->root .= '/';
 			}
+			$this->adapter = new FtpAdapter([
+				'host' => $this->host,
+				'username' => $this->user,
+				'password' => $this->password,
+				'ssl' => $this->secure
+			]);
+			$this->buildFlySystem($this->adapter);
 		} else {
 			throw new \Exception('Creating \OC\Files\Storage\FTP storage failed');
 		}
-		
+
 	}
 
-	public function getId(){
+	public function getId() {
 		return 'ftp::' . $this->user . '@' . $this->host . '/' . $this->root;
-	}
-
-	/**
-	 * construct the ftp url
-	 * @param string $path
-	 * @return string
-	 */
-	public function constructUrl($path) {
-		$url='ftp';
-		if ($this->secure) {
-			$url.='s';
-		}
-		$url.='://'.$this->user.':'.$this->password.'@'.$this->host.$this->root.$path;
-		return $url;
-	}
-
-	/**
-	 * Unlinks file or directory
-	 * @param string $path
-	 */
-	public function unlink($path) {
-		if ($this->is_dir($path)) {
-			return $this->rmdir($path);
-		}
-		else {
-			$url = $this->constructUrl($path);
-			$result = unlink($url);
-			clearstatcache(true, $url);
-			return $result;
-		}
-	}
-	public function fopen($path,$mode) {
-		switch($mode) {
-			case 'r':
-			case 'rb':
-			case 'w':
-			case 'wb':
-			case 'a':
-			case 'ab':
-				//these are supported by the wrapper
-				$context = stream_context_create(array('ftp' => array('overwrite' => true)));
-				return fopen($this->constructUrl($path), $mode, false, $context);
-			case 'r+':
-			case 'w+':
-			case 'wb+':
-			case 'a+':
-			case 'x':
-			case 'x+':
-			case 'c':
-			case 'c+':
-				//emulate these
-				if (strrpos($path, '.')!==false) {
-					$ext=substr($path, strrpos($path, '.'));
-				} else {
-					$ext='';
-				}
-				$tmpFile=\OCP\Files::tmpFile($ext);
-				\OC\Files\Stream\Close::registerCallback($tmpFile, array($this, 'writeBack'));
-				if ($this->file_exists($path)) {
-					$this->getFile($path, $tmpFile);
-				}
-				self::$tempFiles[$tmpFile]=$path;
-				return fopen('close://'.$tmpFile, $mode);
-		}
-		return false;
-	}
-
-	public function writeBack($tmpFile) {
-		if (isset(self::$tempFiles[$tmpFile])) {
-			$this->uploadFile($tmpFile, self::$tempFiles[$tmpFile]);
-			unlink($tmpFile);
-		}
 	}
 
 	/**
@@ -145,10 +88,17 @@ class FTP extends \OC\Files\Storage\StreamWrapper{
 	 */
 	public static function checkDependencies() {
 		if (function_exists('ftp_login')) {
-			return(true);
+			return (true);
 		} else {
 			return array('ftp');
 		}
 	}
 
+	public function disconnect() {
+		$this->adapter->disconnect();
+	}
+
+	public function __destruct() {
+		$this->disconnect();
+	}
 }
