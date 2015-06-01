2a3bf7669f369aea76abebef31af3fb547ba1fe6
Bjoern Schiessle <schiessle@owncloud.com>
2015-06-01 10:22:42 +0200

    add social media buttons to share your federated cloud id

---
+++ apps/files_sharing/templates/settings-personal.php
@@ -0,0 +1,54 @@
+<?php
+/** @var OC_L10N $l */
+/** @var array $_ */
+?>
+<div id="fileSharingSettings" class="section">
+	<h2><?php p($l->t('Federated Cloud'));?></h2>
+
+	<p>
+		Your Federated Cloud Id: <?php p($_['cloudId']); ?>
+	</p>
+
+	<p>
+
+	<h3>Share your Federated Cloud Id:</h3>
+
+	<p>
+
+	<a href='http://sharetodiaspora.github.io/?title=<?php p($_['message_without_URL']); ?>&url=<?php p($_['reference']); ?>' target='_blank'>
+		<img src='TODO: Add Diaspora Button' style="border: 0px solid;" alt='Share on Diaspora' />
+	</a>
+
+	<a href='https://twitter.com/intent/tweet?text=<?php p(urlencode($_['message_with_URL'])); ?>'>
+		<img src='TODO: Add Twitter Button' alt=' | Share on Twitter' />
+	</a>
+
+	<a href='https://www.facebook.com/sharer/sharer.php?u=<?php p($_['reference']); ?>'>
+		<img src='TODO: Add Facebook Button' alt=' | Share on Facebook' /></a>
+
+	<a href='https://plus.google.com/share?url=<?php p($_['reference']); ?>'>
+		<img src='TODO: Add Google+ Button' alt=' | Share on Google+' /></a>
+
+	</p>
+
+	<h3>Add your Federated Cloud Id to your homepage:</h3>
+
+	<p>
+
+	<a href="<?php p($_['reference']); ?>">
+		<img src='TODO: Add Example Banner/Button' alt='<?php p($_['message_with_URL']); ?>' />
+	</a>
+
+		<br />
+		HTML Code:
+		<br />
+
+	<xmp>
+		<a href="<?php p($_['reference']); ?>">
+			<img src='TODO: Add Banner/Button' alt='<?php p($_['message_with_URL']); ?>' />
+		</a>
+	</xmp>
+
+	</p>
+
+</div>
