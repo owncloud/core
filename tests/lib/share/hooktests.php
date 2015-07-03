bb6367a6d57d8e0148235a0f8994d4fadb020291
Bjoern Schiessle <schiessle@owncloud.com>
2015-07-03 17:04:05 +0200

    intproduce pre_addToGroup hook. we need to calculate the possible unique targets before the user was added to the group otherwise we will always detect a name collision

---
+++ tests/lib/share/hooktests.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * @author Björn Schießle <schiessle@owncloud.com>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+
+namespace OC\Tests\Share;
+
+
+use Test\TestCase;
+
+class HookTests extends TestCase {
+
+	protected function setUp() {
+		parent::setUp();
+	}
+
+	protected function tearDown() {
+		$query = \OC_DB::prepare('DELETE FROM `*PREFIX*share` WHERE `item_type` = ?');
+		$query->execute(array('test'));
+
+		parent::tearDown();
+	}
+
+	public function testPostAddToGroup() {
+
+		/** @var \OC\DB\Connection $connection */
+		$connection = \OC::$server->getDatabaseConnection();
+		$query = $connection->createQueryBuilder();
+		$expr = $query->expr();
+
+		$values = [
+			'group1' =>
+				[
+					[
+						'`item_type`' => $expr->literal('test'),
+						'`item_source`' => $expr->literal('42'),
+						'`item_target`' => $expr->literal('42'),
+						'`file_target`' => $expr->literal('test'),
+						'`share_type`' => $expr->literal('2'),
+						'`share_with`' => $expr->literal('group1'),
+						'`uid_owner`' => $expr->literal('owner'),
+						'`permissions`' => $expr->literal('0'),
+						'`stime`' => $expr->literal('676584'),
+						'`file_source`' => $expr->literal('42'),
+					],
+					[
+						'`item_type`' => $expr->literal('test'),
+						'`item_source`' => $expr->literal('42'),
+						'`item_target`' => $expr->literal('42 (2)'),
+						'`share_type`' => $expr->literal('2'),
+						'`share_with`' => $expr->literal('group1'),
+						'`uid_owner`' => $expr->literal('owner'),
+						'`permissions`' => $expr->literal('0'),
+						'`stime`' => $expr->literal('676584'),
+					]
+				],
+			'group2' =>
+				[
+					[
+						'`item_type`' => $expr->literal('test'),
+						'`item_source`' => $expr->literal('42'),
+						'`item_target`' => $expr->literal('42'),
+						'`share_type`' => $expr->literal('2'),
+						'`share_with`' => $expr->literal('group2'),
+						'`uid_owner`' => $expr->literal('owner'),
+						'`permissions`' => $expr->literal('0'),
+						'`stime`' => $expr->literal('676584'),
+					]
+				]
+		];
+
+		// add some dummy values to the private $updateTargets variable
+		$this->addDummyValuesToUpdateTargets($values);
+
+		// add unique targets for group1 to database
+		\OC\Share\Hooks::post_addToGroup(['gid' => 'group1']);
+
+
+		$query->select('`share_with`')->from('`*PREFIX*share`');
+		$result = $query->execute()->fetchAll();
+		$this->assertSame(2, count($result));
+		foreach ($result as $r) {
+			$this->assertSame('group1', $r['share_with']);
+		}
+	}
+
+	/**
+	 * the values which should be set for 'updateTargets'
+	 *
+	 * @param array $value
+	 */
+	private function addDummyValuesToUpdateTargets($value) {
+
+		$object = new \OC\Share\Hooks();
+		$methodName = 'updateTargets';
+
+
+		$reflection = new \ReflectionClass(get_class($object));
+		$property = $reflection->getProperty($methodName);
+		$property->setAccessible(true);
+		$property->setValue($object, $value);
+	}
+
+}
