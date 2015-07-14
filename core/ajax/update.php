a8d26448ca2dce665bb1eb317d5f35f82f29111d
Arnaud JAEGERS <a_jaegers@hotmail.com>
2015-07-14 09:50:16 +0200

    Non-blocking use of set_time_limit()
    Using 'set_time_limit()' on some hosting provider which have 'set_time_limit()' disabled will cause an incomplete update.
    A more elegant solution could be the use of "function_exists('set_time_limit')".

--- core/ajax/update.php
+++ core/ajax/update.php
@@ -25,7 +25,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
  */
-set_time_limit(0);
+@set_time_limit(0);
 require_once '../../lib/base.php';
 
 \OCP\JSON::callCheck();
