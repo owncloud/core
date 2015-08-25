c5a7459cd1489a71d08aee9257f18ba1d4b8aff2
Vincent Petry <pvince81@owncloud.com>
2015-08-25 11:54:40 +0200

    Grant requested preview size in public page

--- apps/files_sharing/js/public.js
+++ apps/files_sharing/js/public.js
@@ -159,8 +159,8 @@ OCA.Sharing.PublicApp = {
 
 			this.fileList.generatePreviewUrl = function (urlSpec) {
 				urlSpec.t = $('#dirToken').val();
-				urlSpec.y = Math.floor(36 * window.devicePixelRatio);
-				urlSpec.x = Math.floor(36 * window.devicePixelRatio);
+				urlSpec.y = Math.floor((urlSpec.y || 36) * window.devicePixelRatio);
+				urlSpec.x = Math.floor((urlSpec.x || 36) * window.devicePixelRatio);
 				return OC.generateUrl('/apps/files_sharing/ajax/publicpreview.php?') + $.param(urlSpec);
 			};
 
