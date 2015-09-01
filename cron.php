c920ab24d3058ab414c139f43b0aaaab44750a56
Robin McCorkell <rmccorkell@owncloud.com>
2015-09-01 16:46:35 +0100

    Simplify occ and cron.php checks to allow more usecases

--- cron.php
+++ cron.php
@@ -82,19 +82,9 @@ try {
 		set_time_limit(0);
 
 		// the cron job must be executed with the right user
-		if (!OC_Util::runningOnWindows())  {
-			if (!function_exists('posix_getuid')) {
-				echo "The posix extensions are required - see http://php.net/manual/en/book.posix.php" . PHP_EOL;
-				exit(0);
-			}
-			$user = posix_getpwuid(posix_getuid());
-			$configUser = posix_getpwuid(fileowner(OC::$SERVERROOT . '/config/config.php'));
-			if ($user['name'] !== $configUser['name']) {
-				echo "Console has to be executed with the same user as the web server is operated" . PHP_EOL;
-				echo "Current user: " . $user['name'] . PHP_EOL;
-				echo "Web server user: " . $configUser['name'] . PHP_EOL;
-				exit(0);
-			}
+		if (!is_writable(\OC::$configDir.'/config.php')) {
+			echo "Console has to be executed with a user that can write to config.php" . PHP_EOL;
+			exit(1);
 		}
 
 		$config = OC::$server->getConfig();
