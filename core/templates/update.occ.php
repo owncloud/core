8a3ba2c9da99eeb8b9ab0224497a321fe6774cc6
Robin McCorkell <rmccorkell@owncloud.com>
2015-09-03 09:01:11 +0100

    Improve warning text, mention config.php parameter

--- core/templates/update.occ.php
+++ core/templates/update.occ.php
@@ -3,8 +3,12 @@
 		<h2 class="title bold"><?php p($l->t('%s requires an update to version %s',
 			array($_['productName'], $_['version']))); ?></h2>
 		<div class="infogroup">
-			<?php p($l->t('Upgrading must be done with the command line tool:')) ?>
+			<?php p($l->t('A configuration option has disabled the web upgrader, you must use the command line tool to upgrade:')) ?>
 			<pre>./occ upgrade</pre>
+		</div>
+		<div class="infogroup">
+			<?php p($l->t('Alternatively, where this is not possible, set the following parameter in config.php:')) ?>
+			<pre><?php p('"upgrade.forceocc" => false') // easy handling of special characters ?></pre>
 		</div>
 	</div>
 </div>
