c29cd227258396bb4c3289a6360dd906f19183e2
Individual IT Services <info@individual-it.net>
2015-09-03 10:26:13 +0545

    reorganizing updates to avoid double work

--- lib/private/connector/sabre/file.php
+++ lib/private/connector/sabre/file.php
@@ -207,9 +207,6 @@ class File extends Node implements IFile {
 				throw new FileLocked($e->getMessage(), $e->getCode(), $e);
 			}
 
-			// since we skipped the view we need to scan and emit the hooks ourselves
-			$this->fileView->getUpdater()->update($this->path);
-
 			if ($view) {
 				if (!$exists) {
 					\OC_Hook::emit(\OC\Files\Filesystem::CLASSNAME, \OC\Files\Filesystem::signal_post_create, array(
@@ -379,15 +376,20 @@ class File extends Node implements IFile {
 					// assemble directly into the final file
 					$chunk_handler->file_assemble($targetPath);
 				}
-
+				
 				// allow sync clients to send the mtime along in a header
 				$request = \OC::$server->getRequest();
 				if (isset($request->server['HTTP_X_OC_MTIME'])) {
-					if ($this->fileView->touch($targetPath, $request->server['HTTP_X_OC_MTIME'])) {
+					if ($this->fileView->touch($this->path, $request->server['HTTP_X_OC_MTIME'], true)) {
 						header('X-OC-MTime: accepted');
 					}
-				}
-
+					//touch() has done already a updateMtime(), now we only need to run a updateFolderSize()
+					$this->fileView->getUpdater()->updateFolderSize($this->path);
+				} else { //no touch was done so we need to do a full update
+					// since we skipped the view we need to scan and emit the hooks ourselves
+					$this->fileView->getUpdater()->update($this->path);
+				}				
+		
 				$info = $this->fileView->getFileInfo($targetPath);
 				return $info->getEtag();
 			} catch (\Exception $e) {
