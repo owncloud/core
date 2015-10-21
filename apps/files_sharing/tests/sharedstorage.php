513bb8f0d2c892c59ddb5d89bc77d548624e0907
Robin Appelman <icewind@owncloud.com>
2015-10-21 16:04:01 +0200

    remove tests of removed logic

--- apps/files_sharing/tests/sharedstorage.php
+++ apps/files_sharing/tests/sharedstorage.php
@@ -58,51 +58,6 @@ class Test_Files_Sharing_Storage extends OCA\Files_sharing\Tests\TestCase {
 	}
 
 	/**
-	 * if the parent of the mount point is gone then the mount point should move up
-	 *
-	 * @medium
-	 */
-	function testParentOfMountPointIsGone() {
-
-		// share to user
-		$fileinfo = $this->view->getFileInfo($this->folder);
-		$result = \OCP\Share::shareItem('folder', $fileinfo['fileid'], \OCP\Share::SHARE_TYPE_USER,
-			self::TEST_FILES_SHARING_API_USER2, 31);
-
-		$this->assertTrue($result);
-
-		self::loginHelper(self::TEST_FILES_SHARING_API_USER2);
-		$user2View = new \OC\Files\View('/' . self::TEST_FILES_SHARING_API_USER2 . '/files');
-		$this->assertTrue($user2View->file_exists($this->folder));
-
-		// create a local folder
-		$result = $user2View->mkdir('localfolder');
-		$this->assertTrue($result);
-
-		// move mount point to local folder
-		$result = $user2View->rename($this->folder, '/localfolder/' . $this->folder);
-		$this->assertTrue($result);
-
-		// mount point in the root folder should no longer exist
-		$this->assertFalse($user2View->is_dir($this->folder));
-
-		// delete the local folder
-		/** @var \OC\Files\Storage\Storage $storage */
-		list($storage, $internalPath)  = \OC\Files\Filesystem::resolvePath('/' . self::TEST_FILES_SHARING_API_USER2 . '/files/localfolder');
-		$storage->rmdir($internalPath);
-
-		//enforce reload of the mount points
-		self::loginHelper(self::TEST_FILES_SHARING_API_USER2);
-
-		//mount point should be back at the root
-		$this->assertTrue($user2View->is_dir($this->folder));
-
-		//cleanup
-		self::loginHelper(self::TEST_FILES_SHARING_API_USER1);
-		$this->view->unlink($this->folder);
-	}
-
-	/**
 	 * @medium
 	 */
 	function testRenamePartFile() {
