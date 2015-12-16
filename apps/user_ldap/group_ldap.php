0e628a610fab9d25a6b93dacd7db6cd65730d411
alexweirig <alex.weirig@technolink.lu>
2015-12-16 08:32:46 +0100

    Update group_ldap.php

--- apps/user_ldap/group_ldap.php
+++ apps/user_ldap/group_ldap.php
@@ -12,7 +12,7 @@
  * @author Thomas Müller <thomas.mueller@tmit.eu>
  * @author Vincent Petry <pvince81@owncloud.com>
  * @author Richard Bentley <rbentley@e2advance.com>
- 
+ *
  * @copyright Copyright (c) 2015, ownCloud, Inc.
  * @license AGPL-3.0
  *
