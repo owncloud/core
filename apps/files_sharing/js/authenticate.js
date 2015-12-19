3a608d64f32794600148100ecaf84529962300bc
Gustavo Murayama <g_murayama@hotmail.com>
2015-12-18 22:44:35 -0200

    Public link share disable password button when no password entered

---
+++ apps/files_sharing/js/authenticate.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#password").keyup(function() {
+		var n = $("#password").val().length;
+		
+		if (n > 0) {
+			$("#submit").prop("disabled", false);
+		} else {
+			$("#submit").prop("disabled", true);
+		}
+	});
+}); 
