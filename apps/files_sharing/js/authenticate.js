7d92ae52e77e906bcf69604b31fe9a0056b31c50
Roeland Jago Douma <rullzer@owncloud.com>
2016-01-12 11:28:36 +0100

    Catch more input events

--- apps/files_sharing/js/authenticate.js
+++ apps/files_sharing/js/authenticate.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	$("#password").keyup(function() {
-		var n = $("#password").val().length;
-		
+	$('#password').on('keyup input change', function() {
+		var n = $('#password').val().length;
+
 		if (n > 0) {
-			$("#submit").prop("disabled", false);
+			$('#submit').prop('disabled', false);
 		} else {
-			$("#submit").prop("disabled", true);
+			$('#submit').prop('disabled', true);
 		}
 	});
-}); 
+});
