f06b65c08ae3e26335b459503749e82440efc6ea
Manish Bisht <manish.bisht490@gmail.com>
2016-03-14 21:30:59 +0530

    Update oci.php

--- lib/private/setup/oci.php
+++ lib/private/setup/oci.php
@@ -51,8 +51,11 @@ class OCI extends AbstractDatabase {
 		if(empty($config['dbuser'])) {
 			$errors[] = $this->trans->t("%s enter the database username.", array($this->dbprettyname));
 		}
-		if(empty($config['dbname'])) {
+		else if(empty($config['dbname'])) {
 			$errors[] = $this->trans->t("%s enter the database name.", array($this->dbprettyname));
+		}
+		else if(empty($config['dbuser']&&empty($config['dbname'])) {
+			$errors[] = $this->trans->t("%s enter the database username and name.", array($this->dbprettyname));
 		}
 		return $errors;
 	}
