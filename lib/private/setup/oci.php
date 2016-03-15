99bd4f148e680092ebba1081fb2741ba96d9e7b2
Manish Bisht <manish.bisht490@gmail.com>
2016-03-15 15:57:59 +0530

    Error Fixed

--- lib/private/setup/oci.php
+++ lib/private/setup/oci.php
@@ -50,14 +50,14 @@ class OCI extends AbstractDatabase {
 
 	public function validate($config) {
 		$errors = array();
-		if(empty($config['dbuser'])) {
+		if(empty($config['dbuser'])&&empty($config['dbname'])) {
+			$errors[] = $this->trans->t("%s enter the database username and name.", array($this->dbprettyname));
+		}
+		else if(empty($config['dbuser'])) {
 			$errors[] = $this->trans->t("%s enter the database username.", array($this->dbprettyname));
 		}
 		else if(empty($config['dbname'])) {
 			$errors[] = $this->trans->t("%s enter the database name.", array($this->dbprettyname));
-		}
-		else if(empty($config['dbuser']&&empty($config['dbname'])) {
-			$errors[] = $this->trans->t("%s enter the database username and name.", array($this->dbprettyname));
 		}
 		return $errors;
 	}
