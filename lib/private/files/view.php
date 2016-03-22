e74c848f04e160799f8fbdf5b23dab44ff7278f4
karakayasemi <karakayasemi@itu.edu.tr>
2016-03-22 19:29:25 +0200

    pull request to fix #23360
    fromTmpFile function, usual mkdir call is only working for file's parent directory. Does not care upper parent folders. I added a recursive function that creates parent non-existing folders with usual mkdir.

--- lib/private/files/view.php
+++ lib/private/files/view.php
@@ -942,7 +942,10 @@ class View {
 
 			// Create the directories if any
 			if (!$this->file_exists($filePath)) {
-				$this->mkdir($filePath);
+				$result = $this->createParentDirectories($filePath);
+				if($result === false) {
+					return false;
+				}
 			}
 
 			$source = fopen($tmpFile, 'r');
@@ -2050,5 +2053,23 @@ class View {
 			}
 		}
 		return [$uid, $filename];
+	}
+	
+	/**
+	 * Creates parent non-existing folders
+	 * @param $filePath
+	 * @return bool
+	 */
+	public function createParentDirectories($filePath)
+	{
+		$parentDirectory = dirname($filePath);
+		while(!$this->file_exists($parentDirectory)) {
+			$result=$this->createParentDirectories($parentDirectory);
+			if($result == false) {
+				return false;
+			}
+		}
+		$this->mkdir($filePath);
+		return true;
 	}
 }
