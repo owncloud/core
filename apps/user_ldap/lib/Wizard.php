630ddc0398f329db59c6f86bea94f8368d89e192
davicente <dvicente@owncloud.com>
2016-05-31 15:29:22 +0100

    value var can be an array

--- apps/user_ldap/lib/Wizard.php
+++ apps/user_ldap/lib/Wizard.php
@@ -1109,7 +1109,7 @@ class Wizard extends LDAPUtility {
 		$this->checkAgentRequirements();
 		foreach($reqs as $option) {
 			$value = $this->configuration->$option;
-			if((!is_string($value)) || ($value === '')) {
+			if(empty($value)) {
 				return false;
 			}
 		}
