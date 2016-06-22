2e5670067771072d3fc919ed76c08eb0161540a1
Georg Ehrke <developer@georgehrke.com>
2016-06-22 11:42:59 +0200

    [stable9] Contacts API: replace raw image data with url

--- apps/dav/appinfo/application.php
+++ apps/dav/appinfo/application.php
@@ -137,7 +137,8 @@ class Application extends App {
 	public function setupContactsProvider(IManager $contactsManager, $userID) {
 		/** @var ContactsManager $cm */
 		$cm = $this->getContainer()->query('ContactsManager');
-		$cm->setupContactsProvider($contactsManager, $userID);
+		$urlGenerator = $this->getContainer()->getServer()->getURLGenerator();
+		$cm->setupContactsProvider($contactsManager, $userID, $urlGenerator);
 	}
 
 	public function registerHooks() {
