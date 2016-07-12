8f2e249a9a733991866dcbef1403f3aceb68e817
Thomas Citharel <tcit@tcit.fr>
2016-07-12 16:35:27 +0200

    Fix some tests

--- apps/dav/tests/unit/Connector/Sabre/FilesPluginTest.php
+++ apps/dav/tests/unit/Connector/Sabre/FilesPluginTest.php
@@ -446,7 +446,7 @@ class FilesPluginTest extends TestCase {
 		$node = $this->getMockBuilder('\OCA\DAV\Connector\Sabre\Node')
 			->disableOriginalConstructor()
 			->getMock();
-		$node->expects($this->once())
+		$node->expects($this->at(0))
 			->method('getFileInfo')
 			->willReturn($fileInfoFolderATestTXT);
 
@@ -520,7 +520,7 @@ class FilesPluginTest extends TestCase {
 				->getMock();
 
 		$request
-			->expects($this->once())
+			->expects($this->at(1))
 			->method('getPath')
 			->will($this->returnValue('test/somefile.xml'));
 
