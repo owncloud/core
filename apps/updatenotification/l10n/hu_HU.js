ef22b9a21e8447f9bafac38cfeb2cd7786c66e0b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-18 01:54:58 -0400

    [tx-robot] updated from transifex

--- apps/updatenotification/l10n/hu_HU.js
+++ apps/updatenotification/l10n/hu_HU.js
@@ -12,6 +12,8 @@ OC.L10N.register(
     "Your version is up to date." : "Verzió frissítve.",
     "Checked on %s" : "Ellenőrizve: %s",
     "Update channel:" : "Frissítési csatorna:",
-    "You can always update to a newer version / experimental channel. But you can never downgrade to a more stable channel." : "Mindig frissíthetsz az újabb verzióra vagy kísérleti csatornára, de visszafelé sosem frissíthetsz egy jóval stabilabb verzióra."
+    "You can always update to a newer version / experimental channel. But you can never downgrade to a more stable channel." : "Mindig frissíthetsz az újabb verzióra vagy kísérleti csatornára, de visszafelé sosem frissíthetsz egy jóval stabilabb verzióra.",
+    "Notify members of the following groups about available updates:" : "A tagjai a következő csoportnak kapjanak értesítést az elérhető frissítésekről:",
+    "Only notification for app updates are available, because the selected update channel for ownCloud itself does not allow notifications." : "Csak az alkalmazás frissítés éretsítések érhetőek el, mert a kiválasztott frissítési csatorna az ownCloudnak nem engedélyezi az értesítéseket."
 },
 "nplurals=2; plural=(n != 1);");
