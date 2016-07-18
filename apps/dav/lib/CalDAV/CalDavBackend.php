41b9ac97189955bde806d36ca924d03b6df97859
Thomas Citharel <tcit@tcit.fr>
2016-07-18 15:38:16 +0200

    Fix wrong way to get publish status

--- apps/dav/lib/CalDAV/CalDavBackend.php
+++ apps/dav/lib/CalDAV/CalDavBackend.php
@@ -1510,8 +1510,7 @@ class CalDavBackend extends AbstractBackend implements SyncSupport, Subscription
 
 		$row = $result->fetch();
 		$result->closeCursor();
-
-		return $row[0] > 0;
+		return reset($row) > 0;
 	}
 
 	/**
