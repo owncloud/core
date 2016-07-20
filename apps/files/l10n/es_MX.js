3c5fc714f3dd06ae89ecf6be4db2a26b15a9678c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-20 01:55:53 -0400

    [tx-robot] updated from transifex

--- apps/files/l10n/es_MX.js
+++ apps/files/l10n/es_MX.js
@@ -58,6 +58,7 @@ OC.L10N.register(
     "Save" : "Guardar",
     "Settings" : "Ajustes",
     "WebDAV" : "WebDAV",
+    "Select all" : "Seleccionar todo",
     "Upload too large" : "Subida demasido grande",
     "The files you are trying to upload exceed the maximum size for file uploads on this server." : "Los archivos que estás intentando subir sobrepasan el tamaño máximo permitido en este servidor.",
     "Text file" : "Archivo de texto"
