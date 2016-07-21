32429fe029d81a3c9226685528f0f3cff4dfe43e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-21 19:54:57 -0400

    [tx-robot] updated from transifex

--- apps/updatenotification/l10n/ast.js
+++ apps/updatenotification/l10n/ast.js
@@ -1,6 +1,8 @@
 OC.L10N.register(
     "updatenotification",
     {
-    "Updater" : "Anovador"
+    "Updater" : "Anovador",
+    "Update channel:" : "Anovar canal:",
+    "You can always update to a newer version / experimental channel. But you can never downgrade to a more stable channel." : "Siempres vas poder anovar a la versión más recién o a un canal esperimental, pero xamás vas poder volver a una canal más estable."
 },
 "nplurals=2; plural=(n != 1);");
