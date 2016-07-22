a30e8e6dee8d7bd62d8fb0db1a020367ef8a557b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-22 19:57:28 -0400

    [tx-robot] updated from transifex

--- apps/comments/l10n/hy.js
+++ apps/comments/l10n/hy.js
@@ -1,7 +1,13 @@
 OC.L10N.register(
     "comments",
     {
+    "Delete comment" : "Ջնջել մեկնաբանությունը",
     "Cancel" : "Չեղարկել",
-    "Save" : "Պահպանել"
+    "Edit comment" : "Խմբագրել մեկնաբանությունը",
+    "Comments" : "Մեկնաբանություններ",
+    "Save" : "Պահպանել",
+    "Comment" : "Մեկնաբանել",
+    "You commented" : "Դու մեկնաբանեցիր",
+    "%1$s commented" : "%1$s մեկնաբանեց"
 },
 "nplurals=2; plural=(n != 1);");
---
+++ apps/encryption/l10n/hy.js
@@ -0,0 +1,7 @@
+OC.L10N.register(
+    "encryption",
+    {
+    "Cheers!" : "Չը՛խկ",
+    "Enabled" : "Միացված"
+},
+"nplurals=2; plural=(n != 1);");
