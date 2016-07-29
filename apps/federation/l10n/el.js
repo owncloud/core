1b838ff1a6ce108f3b6af74e806947c697a17713
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-29 19:55:27 -0400

    [tx-robot] updated from transifex

--- apps/federation/l10n/el.js
+++ apps/federation/l10n/el.js
@@ -5,6 +5,8 @@ OC.L10N.register(
     "Server is already in the list of trusted servers." : "Ο διακομιστής περιλαμβάνεται ήδη στην λίστα των έμπιστων ownCloud",
     "No ownCloud server found" : "Δεν βρέθηκε διακομιστής ownCloud",
     "Could not add server" : "Αδυναμία προσθήκης διακομιστή",
+    "Trusted ownCloud Servers" : "Έμπιστοι διακομιστές ownCloud",
+    "+ Add ownCloud server" : "+ Προσθήκη διακομιστή ownCloud",
     "ownCloud Server" : "Διακομιστής ownCloud"
 },
 "nplurals=2; plural=(n != 1);");
