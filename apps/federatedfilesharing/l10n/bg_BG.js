5f629730bcaf27b7df424a9bb325d5a133b3c86a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-08-20 01:55:52 +0200

    [tx-robot] updated from transifex

--- apps/federatedfilesharing/l10n/bg_BG.js
+++ apps/federatedfilesharing/l10n/bg_BG.js
@@ -3,8 +3,14 @@ OC.L10N.register(
     {
     "Sharing %s failed, because this item is already shared with %s" : "Неуспешно споделяне на %s, защото това съдържание е вече споделено с %s.",
     "Sharing %s failed, could not find %s, maybe the server is currently unreachable." : "Неуспешно споделяне на на %s, не може бъде намерено %s. Може би сървъра в момента е недостъпен.",
+    "Accept" : "Приемам",
+    "Decline" : "Отказвам",
     "Open documentation" : "Отвори документацията",
     "Allow users on this server to send shares to other servers" : "Позволи на потребители от този сървър да споделят папки  с други сървъри",
-    "Allow users on this server to receive shares from other servers" : "Позволи на потребители на този сървър да получават споделени папки от други сървъри"
+    "Allow users on this server to receive shares from other servers" : "Позволи на потребители на този сървър да получават споделени папки от други сървъри",
+    "Share it:" : "Сподели:",
+    "Add to your website" : "Добавете към вашия сайт",
+    "Share with me via ownCloud" : "Сподели с мен чрез ownCloud",
+    "HTML Code:" : "HTML код:"
 },
 "nplurals=2; plural=(n != 1);");
