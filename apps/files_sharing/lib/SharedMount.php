2f88c624cb947c43fd3edf3d945d443f4548283c
Vincent Petry <pvince81@owncloud.com>
2016-08-30 09:52:54 +0200

    Added closeCursor in SharedMount numeric id

--- apps/files_sharing/lib/SharedMount.php
+++ apps/files_sharing/lib/SharedMount.php
@@ -243,6 +243,7 @@ class SharedMount extends MountPoint implements MoveableMount {
 
 		$result = $query->execute();
 		$row = $result->fetch();
+		$result->closeCursor();
 		if ($row) {
 			return $row['storage'];
 		}
