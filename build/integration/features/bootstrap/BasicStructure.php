5732b572cc7ce7846c2afc8971f1941671d661d6
Sergio Bertolín <sbertolin@solidgear.es>
2016-08-30 10:53:19 +0000

    Creating the folder when the tests run

--- build/integration/features/bootstrap/BasicStructure.php
+++ build/integration/features/bootstrap/BasicStructure.php
@@ -315,7 +315,7 @@ trait BasicStructure {
 	 * @BeforeScenario @local_storage
 	 */
 	public static function removeFilesFromLocalStorageBefore(){
-		$dir = "./local_storage/";
+		$dir = "./work/local_storage/";
 		$di = new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
 		$ri = new RecursiveIteratorIterator($di, RecursiveIteratorIterator::CHILD_FIRST);
 		foreach ( $ri as $file ) {
@@ -327,7 +327,7 @@ trait BasicStructure {
 	 * @AfterScenario @local_storage
 	 */
 	public static function removeFilesFromLocalStorageAfter(){
-		$dir = "./local_storage/";
+		$dir = "./work/local_storage/";
 		$di = new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
 		$ri = new RecursiveIteratorIterator($di, RecursiveIteratorIterator::CHILD_FIRST);
 		foreach ( $ri as $file ) {
