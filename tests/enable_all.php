b92e849091074fcd6462e91dec02f24a877e498b
Thomas Müller <DeepDiver1975@users.noreply.github.com>
2016-09-12 06:49:22 +0200

    Clean up code coverage and some more user_ldap left over (#26091)

--- tests/enable_all.php
+++ tests/enable_all.php
@@ -19,7 +19,6 @@ function enableApp($app) {
 enableApp('files_sharing');
 enableApp('files_trashbin');
 enableApp('encryption');
-enableApp('user_ldap');
 enableApp('files_versions');
 enableApp('provisioning_api');
 enableApp('federation');
