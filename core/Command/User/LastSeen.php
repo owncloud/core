0f664d73b9a86bb1204c51c3aeb84d73b727697e
Jörn Friedrich Dreyer <jfd@butonic.de>
2016-09-22 14:09:53 +0200

    let occ user:lastseen report latest logins

--- core/Command/User/LastSeen.php
+++ core/Command/User/LastSeen.php
@@ -24,51 +24,141 @@
 
 namespace OC\Core\Command\User;
 
+use OC\Core\Command\Base;
+use OCP\IDBConnection;
 use OCP\IUserManager;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 
-class LastSeen extends Command {
+class LastSeen extends Base  {
 	/** @var IUserManager */
 	protected $userManager;
+
+	/** @var IDBConnection */
+	protected $connection;
 
 	/**
 	 * @param IUserManager $userManager
 	 */
-	public function __construct(IUserManager $userManager) {
+	public function __construct(IUserManager $userManager, IDBConnection $connection) {
 		$this->userManager = $userManager;
+		$this->connection = $connection;
 		parent::__construct();
 	}
 
 	protected function configure() {
+		parent::configure();
 		$this
 			->setName('user:lastseen')
-			->setDescription('shows when the user was logged in last time')
+			->setDescription('shows when users were logged in last time')
 			->addArgument(
 				'uid',
-				InputArgument::REQUIRED,
+				InputArgument::OPTIONAL,
 				'the username'
+			)
+			->addOption(
+				'limit',
+				10,
+				InputOption::VALUE_OPTIONAL,
+				'limit to n users'
 			);
 	}
 
-	protected function execute(InputInterface $input, OutputInterface $output) {
-		$user = $this->userManager->get($input->getArgument('uid'));
-		if(is_null($user)) {
-			$output->writeln('<error>User does not exist</error>');
-			return;
+	protected function getLastLogins($userId = null, $limit = 10) {
+		$queryBuilder = $this->connection->getQueryBuilder();
+		$queryBuilder->select(['userid', 'configvalue'])
+			->from('preferences')
+			->where($queryBuilder->expr()->eq(
+				'appid', $queryBuilder->createNamedParameter('login'))
+			)
+			->andWhere($queryBuilder->expr()->eq(
+				'configkey', $queryBuilder->createNamedParameter('lastLogin'))
+			)
+			->andWhere($queryBuilder->expr()->isNotNull('configvalue')
+			)
+			->orderBy('configvalue', 'DESC');
+
+		if ($userId) {
+			$queryBuilder->andWhere($queryBuilder->expr()->eq(
+				'userid', $queryBuilder->createNamedParameter($userId))
+			);
 		}
 
-		$lastLogin = $user->getLastLogin();
-		if($lastLogin === 0) {
-			$output->writeln('User ' . $user->getUID() .
-				' has never logged in, yet.');
-		} else {
+		if (!empty($limit)) {
+			$queryBuilder->setMaxResults($limit);
+		}
+
+		$query = $queryBuilder->execute();
+		$result = [];
+
+		while ($row = $query->fetch()) {
+			$result[] = ['userid' => $row['userid'], 'lastLogin' => $row['configvalue']];
+		}
+
+		return $result;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$uid = $input->getArgument('uid');
+		$users = $this->getLastLogins($uid, $input->getOption('limit'));
+		$outputFormat = $input->getOption('output');
+		if (empty($users)) {
+			if ($uid) {
+				$user = $this->userManager->get($uid);
+				if ($user) {
+					if ($outputFormat === self::OUTPUT_FORMAT_PLAIN ) {
+						$output->writeln('User '.$uid.' has never logged in, yet.');
+						return;
+					} else {
+						$users = [
+							[
+								'userid' => $uid,
+								'displayname' => $user->getDisplayName(),
+								'email' => $user->getEMailAddress(),
+								'lastLogin' => null
+							]
+						];
+					}
+				} else if ($outputFormat === self::OUTPUT_FORMAT_PLAIN ) {
+					$output->writeln('<error>User $uid does not exist</error>');
+					return;
+				}
+			}
+		}
+		foreach ($users as $key => $data) {
 			$date = new \DateTime();
-			$date->setTimestamp($lastLogin);
-			$output->writeln($user->getUID() .
-				'`s last login: ' . $date->format('d.m.Y H:i'));
+			$date->setTimestamp($data['lastLogin']);
+			$user = $this->userManager->get($data['userid']);
+				switch ($outputFormat) {
+					case self::OUTPUT_FORMAT_JSON:
+					case self::OUTPUT_FORMAT_JSON_PRETTY:
+						if ($users[$key]['lastLogin'] !== null) {
+							$users[$key]['lastLogin'] = $date->format('Y-m-d\TH:i:s');
+						}
+						// make the json output useful by adding more info
+						if ($user) {
+							$users[$key]['displayname'] = $user->getDisplayName();
+							$users[$key]['email'] = $user->getEMailAddress();
+						} else {
+							// user no longer exists
+							$users[$key]['displayname'] = null;
+							$users[$key]['email'] = null;
+						}
+						ksort($users[$key]);
+						break;
+					default:
+						$output->writeln(
+							$data['userid'] .'`s last login: ' . $date->format('d.m.Y H:i') );
+						break;
+				}
+		}
+		switch ($outputFormat) {
+			case self::OUTPUT_FORMAT_JSON:
+			case self::OUTPUT_FORMAT_JSON_PRETTY:
+				$this->writeArrayInOutputFormat($input, $output, $users);
+				break;
 		}
 	}
 }
--- tests/Core/Command/User/LastSeenTest.php
+++ tests/Core/Command/User/LastSeenTest.php
@@ -25,9 +25,19 @@ namespace Tests\Core\Command\User;
 use OC\Core\Command\User\LastSeen;
 use Test\TestCase;
 
+
+/**
+ * Class LastSeenTest
+ *
+ * @group DB
+ *
+ * @package Test
+ */
 class LastSeenTest extends TestCase {
 	/** @var \PHPUnit_Framework_MockObject_MockObject */
 	protected $userManager;
+	/** @var \OCP\IDBConnection */
+	protected $connection;
 	/** @var \PHPUnit_Framework_MockObject_MockObject */
 	protected $consoleInput;
 	/** @var \PHPUnit_Framework_MockObject_MockObject */
@@ -39,44 +49,118 @@ class LastSeenTest extends TestCase {
 	protected function setUp() {
 		parent::setUp();
 
-		$userManager = $this->userManager = $this->getMockBuilder('OCP\IUserManager')
+		/** @var \OCP\IUserManager $userManager */
+		$this->userManager = $this->getMockBuilder('OCP\IUserManager')
 			->disableOriginalConstructor()
 			->getMock();
+
+		$this->connection = \OC::$server->getDatabaseConnection();
+		$sql = $this->connection->getQueryBuilder();
+		$sql->select('*')
+			->from('preferences');
+		$result = $sql->execute();
+		$this->originalConfig = $result->fetchAll();
+		$result->closeCursor();
+
+		$sql = $this->connection->getQueryBuilder();
+		$sql->delete('preferences');
+		$sql->execute();
+
+		$sql = $this->connection->getQueryBuilder();
+		$sql->insert('preferences')
+			->values([
+				'userid' => $sql->createParameter('userid'),
+				'appid' => $sql->createParameter('appid'),
+				'configkey' => $sql->createParameter('configkey'),
+				'configvalue' => $sql->createParameter('configvalue'),
+			]);
+
+		$sql->setParameters([
+			'userid' => 'user',
+			'appid' => 'login',
+			'configkey' => 'lastLogin',
+			'configvalue' => null,
+		])->execute();
+		$sql->setParameters([
+			'userid' => 'user1',
+			'appid' => 'login',
+			'configkey' => 'lastLogin',
+			'configvalue' => '1474453520',
+		])->execute();
+		$sql->setParameters([
+			'userid' => 'user2',
+			'appid' => 'login',
+			'configkey' => 'lastLogin',
+			'configvalue' => '1473067798',
+		])->execute();
+
 		$this->consoleInput = $this->createMock('Symfony\Component\Console\Input\InputInterface');
 		$this->consoleOutput = $this->createMock('Symfony\Component\Console\Output\OutputInterface');
 
-		/** @var \OCP\IUserManager $userManager */
-		$this->command = new LastSeen($userManager);
+		$this->command = new LastSeen($this->userManager, $this->connection);
 	}
 
-	public function validUserLastSeen() {
+	public function tearDown() {
+		$sql = $this->connection->getQueryBuilder();
+		$sql->delete('preferences');
+		$sql->execute();
+
+		$sql = $this->connection->getQueryBuilder();
+		$sql->insert('preferences')
+			->values([
+				'userid' => $sql->createParameter('userid'),
+				'appid' => $sql->createParameter('appid'),
+				'configkey' => $sql->createParameter('configkey'),
+				'configvalue' => $sql->createParameter('configvalue'),
+			]);
+
+		foreach ($this->originalConfig as $configs) {
+			$sql->setParameter('userid', $configs['userid'])
+				->setParameter('appid', $configs['appid'])
+				->setParameter('configkey', $configs['configkey'])
+				->setParameter('configvalue', $configs['configvalue']);
+			$sql->execute();
+		}
+
+		parent::tearDown();
+	}
+
+
+	public function validUserLastSeenPlain() {
 		return [
-			[0, 'never logged in'],
-			[time(), 'last login'],
+			['user', 'never logged in'],
+			['user1', '21.09.2016 10:25'],
+			['user2', '05.09.2016 09:29'],
 		];
 	}
 
 	/**
-	 * @dataProvider validUserLastSeen
+	 * @dataProvider validUserLastSeenPlain
 	 *
-	 * @param int $lastSeen
+	 * @param string $userId
+	 * @param string $displayName
+	 * @param string $email
 	 * @param string $expectedString
 	 */
-	public function testValidUser($lastSeen, $expectedString) {
-		$user = $this->createMock('OCP\IUser');
-		$user->expects($this->once())
-			->method('getLastLogin')
-			->willReturn($lastSeen);
+	public function testValidUserPlain($userId, $expectedString) {
 
-		$this->userManager->expects($this->once())
-			->method('get')
-			->with('user')
-			->willReturn($user);
-
-		$this->consoleInput->expects($this->once())
+		$this->consoleInput->expects($this->at(0))
 			->method('getArgument')
 			->with('uid')
-			->willReturn('user');
+			->willReturn($userId);
+		$this->consoleInput->expects($this->at(1))
+			->method('getOption')
+			->with('limit')
+			->willReturn(10);
+		$this->consoleInput->expects($this->at(2))
+			->method('getOption')
+			->with('output')
+			->willReturn(LastSeen::OUTPUT_FORMAT_PLAIN);
+
+		$this->userManager->expects($this->any())
+			->method('get')
+			->with($userId)
+			->willReturn($this->createMock('OCP\IUser'));
 
 		$this->consoleOutput->expects($this->once())
 			->method('writeln')
@@ -86,19 +170,179 @@ class LastSeenTest extends TestCase {
 	}
 
 	public function testInvalidUser() {
-		$this->userManager->expects($this->once())
-			->method('get')
-			->with('user')
-			->willReturn(null);
-
-		$this->consoleInput->expects($this->once())
+		$this->consoleInput->expects($this->at(0))
 			->method('getArgument')
 			->with('uid')
-			->willReturn('user');
+			->willReturn('user3');
+		$this->consoleInput->expects($this->at(1))
+			->method('getOption')
+			->with('limit')
+			->willReturn(10);
+		$this->consoleInput->expects($this->at(2))
+			->method('getOption')
+			->with('output')
+			->willReturn(LastSeen::OUTPUT_FORMAT_PLAIN);
+
+		$this->userManager->expects($this->once())
+			->method('get')
+			->with('user3')
+			->willReturn(null);
 
 		$this->consoleOutput->expects($this->once())
 			->method('writeln')
-			->with($this->stringContains('User does not exist'));
+			->with($this->stringContains('does not exist'));
+
+		self::invokePrivate($this->command, 'execute', [$this->consoleInput, $this->consoleOutput]);
+	}
+
+
+	public function validUserLastSeenJSON() {
+		return [
+			['user', 'User Name', 'user@e.mail',
+				'[{"displayname":"User Name","email":"user@e.mail","lastLogin":null,"userid":"user"}]'],
+			['user1','User1 Name', 'user1@e.mail',
+				'[{"displayname":"User1 Name","email":"user1@e.mail","lastLogin":"2016-09-21T10:25:20","userid":"user1"}]'],
+			['user2', 'User2 Name',	'user2@e.mail',
+				'[{"displayname":"User2 Name","email":"user2@e.mail","lastLogin":"2016-09-05T09:29:58","userid":"user2"}]'],
+		];
+	}
+
+	/**
+	 * @dataProvider validUserLastSeenJSON
+	 *
+	 * @param string $userId
+	 * @param string $displayName
+	 * @param string $email
+	 * @param string $expectedString
+	 */
+	public function testValidUserJSON($userId, $displayName, $email, $expectedString) {
+
+		$user = $this->createMock('OCP\IUser');
+		$user->expects($this->any())
+			->method('getDisplayName')
+			->willReturn($displayName);
+		$user->expects($this->any())
+			->method('getEMailAddress')
+			->willReturn($email);
+
+		$this->consoleInput->expects($this->at(0))
+			->method('getArgument')
+			->with('uid')
+			->willReturn($userId);
+		$this->consoleInput->expects($this->at(1))
+			->method('getOption')
+			->with('limit')
+			->willReturn(10);
+		$this->consoleInput->expects($this->at(2))
+			->method('getOption')
+			->with('output')
+			->willReturn(LastSeen::OUTPUT_FORMAT_JSON);
+		$this->consoleInput->expects($this->at(3))
+			->method('getOption')
+			->with('output')
+			->willReturn(LastSeen::OUTPUT_FORMAT_JSON);
+
+		$this->userManager->expects($this->any())
+			->method('get')
+			->with($userId)
+			->willReturn($user);
+
+		$this->consoleOutput->expects($this->once())
+			->method('writeln')
+			->with($this->stringContains($expectedString));
+
+		self::invokePrivate($this->command, 'execute', [$this->consoleInput, $this->consoleOutput]);
+	}
+
+	public function testValidUsersJSON() {
+
+		$user1 = $this->createMock('OCP\IUser');
+		$user1->expects($this->any())
+			->method('getDisplayName')
+			->willReturn('User1 Name');
+		$user1->expects($this->any())
+			->method('getEMailAddress')
+			->willReturn('user1@e.mail');
+
+		$user2 = $this->createMock('OCP\IUser');
+		$user2->expects($this->any())
+			->method('getDisplayName')
+			->willReturn('User2 Name');
+		$user2->expects($this->any())
+			->method('getEMailAddress')
+			->willReturn('user2@e.mail');
+
+		$this->consoleInput->expects($this->at(0))
+			->method('getArgument')
+			->with('uid')
+			->willReturn(null);
+		$this->consoleInput->expects($this->at(1))
+			->method('getOption')
+			->with('limit')
+			->willReturn(10);
+		$this->consoleInput->expects($this->at(2))
+			->method('getOption')
+			->with('output')
+			->willReturn(LastSeen::OUTPUT_FORMAT_JSON);
+		$this->consoleInput->expects($this->at(3))
+			->method('getOption')
+			->with('output')
+			->willReturn(LastSeen::OUTPUT_FORMAT_JSON);
+
+		$this->userManager->expects($this->at(0))
+			->method('get')
+			->with('user1')
+			->willReturn($user1);
+		$this->userManager->expects($this->at(1))
+			->method('get')
+			->with('user2')
+			->willReturn($user2);
+
+		$this->consoleOutput->expects($this->once())
+			->method('writeln')
+			->with($this->stringContains('[{"displayname":"User1 Name","email":"user1@e.mail","lastLogin":"2016-09-21T10:25:20","userid":"user1"},{"displayname":"User2 Name","email":"user2@e.mail","lastLogin":"2016-09-05T09:29:58","userid":"user2"}]'));
+
+		self::invokePrivate($this->command, 'execute', [$this->consoleInput, $this->consoleOutput]);
+	}
+	public function testInalidUsersJSON() {
+
+		$user1 = $this->createMock('OCP\IUser');
+		$user1->expects($this->any())
+			->method('getDisplayName')
+			->willReturn('User1 Name');
+		$user1->expects($this->any())
+			->method('getEMailAddress')
+			->willReturn('user1@e.mail');
+
+		$this->consoleInput->expects($this->at(0))
+			->method('getArgument')
+			->with('uid')
+			->willReturn(null);
+		$this->consoleInput->expects($this->at(1))
+			->method('getOption')
+			->with('limit')
+			->willReturn(10);
+		$this->consoleInput->expects($this->at(2))
+			->method('getOption')
+			->with('output')
+			->willReturn(LastSeen::OUTPUT_FORMAT_JSON);
+		$this->consoleInput->expects($this->at(3))
+			->method('getOption')
+			->with('output')
+			->willReturn(LastSeen::OUTPUT_FORMAT_JSON);
+
+		$this->userManager->expects($this->at(0))
+			->method('get')
+			->with('user1')
+			->willReturn($user1);
+		$this->userManager->expects($this->at(1))
+			->method('get')
+			->with('user2')
+			->willReturn(null);
+
+		$this->consoleOutput->expects($this->once())
+			->method('writeln')
+			->with($this->stringContains('[{"displayname":"User1 Name","email":"user1@e.mail","lastLogin":"2016-09-21T10:25:20","userid":"user1"},{"displayname":null,"email":null,"lastLogin":"2016-09-05T09:29:58","userid":"user2"}]'));
 
 		self::invokePrivate($this->command, 'execute', [$this->consoleInput, $this->consoleOutput]);
 	}
