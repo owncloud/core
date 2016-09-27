935c002036783f35adedd0c84434f3eafbd57f13
Sergio Bertolin <sbertolin@solidgear.es>
2016-09-27 12:39:50 +0000

    Adapted sharing and external features, random failures in sharing:857
    Conflicts:
    	build/integration/features/sharing-v1.feature

--- build/integration/features/bootstrap/WebDav.php
+++ build/integration/features/bootstrap/WebDav.php
@@ -254,12 +254,12 @@ trait WebDav {
 	}
 
 	/**
-	 * @Then /^as "([^"]*)" gets properties of folder "([^"]*)" with$/
+	 * @Then /^as "([^"]*)" gets properties of (file|folder|entry) "([^"]*)" with$/
 	 * @param string $user
 	 * @param string $path
 	 * @param \Behat\Gherkin\Node\TableNode|null $propertiesTable
 	 */
-	public function asGetsPropertiesOfFolderWith($user, $path, $propertiesTable) {
+	public function asGetsPropertiesOfFolderWith($user, $elementType, $path, $propertiesTable) {
 		$properties = null;
 		if ($propertiesTable instanceof \Behat\Gherkin\Node\TableNode) {
 			foreach ($propertiesTable->getRows() as $row) {
@@ -378,7 +378,7 @@ trait WebDav {
 	}
 
 	public function makeSabrePath($user, $path) {
-		return $this->encodePath($this->getDavFilesPath($user));
+		return $this->encodePath($this->getDavFilesPath($user) . $path);
 	}
 
 	public function getSabreClient($user) {
@@ -583,13 +583,4 @@ trait WebDav {
 		return $response;
 	}
 
-	/**
-	 * @Then /^as "([^"]*)" gets properties of file "([^"]*)" with$/
-	 * @param string $user
-	 * @param string $path
-	 * @param \Behat\Gherkin\Node\TableNode|null $propertiesTable
-	 */
-	public function asGetsPropertiesOfFileWith($user, $path, $propertiesTable) {
-		$this->asGetsPropertiesOfFolderWith($user, $path, $propertiesTable);
-	}
 }
