21e8b8b781411d2837dde0dfd0a43a4e8218e9ae
Philipp Schaffrath <github@philippschaffrath.de>
2016-10-13 13:34:17 +0200

    added dumb Theme value-object

---
+++ lib/private/Theme/Theme.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace OC\Theme;
+
+class Theme {
+
+	/**
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * @var string
+	 */
+	private $directory;
+
+	/**
+	 * Theme constructor.
+	 *
+	 * @param string $name
+	 * @param string $directory
+	 */
+	public function __construct($name = '', $directory = '') {
+		$this->name = $name;
+		$this->directory = $directory;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDirectory()
+	{
+		return $this->directory;
+	}
+
+	/**
+	 * @param string $directory
+	 */
+	public function setDirectory($directory)
+	{
+		$this->directory = $directory;
+	}
+}
