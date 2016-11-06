751200c7136fd885d4d0a8ee4d9164ad42874e21
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-11-06 01:55:27 +0100

    [tx-robot] updated from transifex

--- apps/files_versions/l10n/de.js
+++ apps/files_versions/l10n/de.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Could not revert: %s" : "Konnte %s nicht zurücksetzen",
     "Versions" : "Versionen",
     "Failed to revert {file} to revision {timestamp}." : "Konnte {file} der Revision {timestamp} nicht rückgängig machen.",
+    "_%n byte_::_%n bytes_" : ["%n Byte","%n Bytes"],
     "Restore" : "Wiederherstellen",
     "No other versions available" : "Keine anderen Versionen verfügbar",
     "More versions..." : "Weitere Versionen…"
