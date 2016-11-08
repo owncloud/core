c3b679760ab3a28fbe6891660539f1c41f3d8ffe
Jonathan Kawohl <john@owncloud.com>
2016-11-08 11:03:48 +0100

    took @davitol s recommendations

--- core/Command/User/ResetPassword.php
+++ core/Command/User/ResetPassword.php
@@ -97,8 +97,12 @@ class ResetPassword extends Command {
 			$q = new Question('<question>Confirm the new password: </question>', false);
 			$q->setHidden(true);
 			$confirm = $dialog->ask($input, $output, $q);
-			if ($password !== $confirm || (strlen($password) === 0)) {
-				$output->writeln("<error>Passwords did not match or you didn't enter one!</error>");
+			if (strlen($password) === 0) {
+				$output->writeln("<error>You did not enter a Password!</error>");
+				return 1;
+			}
+			if ($password !== $confirm ) {
+				$output->writeln("<error>Passwords did not match! Please try again!</error>");
 				return 1;
 			}
 		} else {
