fb19605a3ef683190c036471fe5a1e63e1b52c72
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-11-10 01:55:12 +0100

    [tx-robot] updated from transifex

--- apps/federation/l10n/oc.js
+++ apps/federation/l10n/oc.js
@@ -4,6 +4,8 @@ OC.L10N.register(
     "No ownCloud server found" : "Cap de servidor ownCloud pas trobat",
     "Could not add server" : "Impossible d'apondre un servidor",
     "Federation" : "Federacion",
+    "Trusted ownCloud Servers" : "Servidor ownCloud fisable ",
+    "+ Add ownCloud server" : "+ Apondre un servidor Owncloud",
     "ownCloud Server" : "Servidor ownCloud",
     "Add" : "Apondre"
 },
