859ac9ff54df2c1d801cfe85aa4731eb0c6202c9
Sergio Bertolin <sbertolin@solidgear.es>
2016-11-14 13:59:19 +0000

    Added user to makeSabrePath calls

--- build/integration/features/bootstrap/WebDav.php
+++ build/integration/features/bootstrap/WebDav.php
@@ -263,7 +263,7 @@ trait WebDav {
 	 */
 	public function asTheFileOrFolderDoesNotExist($user, $entry, $path) {
 		$client = $this->getSabreClient($user);
-		$response = $client->request('HEAD', $this->makeSabrePath($path));
+		$response = $client->request('HEAD', $this->makeSabrePath($user, $path));
 		if ($response['statusCode'] !== 404) {
 			throw new \Exception($entry . ' "' . $path . '" expected to not exist (status code ' . $response['statusCode'] . ', expected 404)');
 		}
@@ -358,7 +358,7 @@ trait WebDav {
 			];
 		}
 
-		$response = $client->propfind($this->makeSabrePath($path), $properties, $folderDepth);
+		$response = $client->propfind($this->makeSabrePath($user, $path), $properties, $folderDepth);
 
 		return $response;
 	}
