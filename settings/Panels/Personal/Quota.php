e41c9e4caf750c165dc7cdd7c18658f8cf504693
Tom Needham <tom@owncloud.com>
2016-11-19 16:01:02 +0000

    Add quota to personal panels

---
+++ settings/Panels/Personal/Quota.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @author Tom Needham <tom@owncloud.com>
+ *
+ * @copyright Copyright (c) 2016, ownCloud GmbH.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OC\Settings\Panels\Personal;
+
+use OCP\Settings\IPanel;
+use OCP\Template;
+use OCP\IConfig;
+
+class Quota implements IPanel {
+
+	public function __construct(IConfig $config) {
+		$this->config = $config;
+	}
+
+    public function getPriority() {
+        return 100;
+    }
+
+    public function getPanel() {
+		$tmpl = new Template('settings', 'panels/personal/quota');
+		$storageInfo = \OC_Helper::getStorageInfo('/');
+		$tmpl->assign('usage', \OC_Helper::humanFileSize($storageInfo['used']));
+		$tmpl->assign('usage_relative', $storageInfo['relative']);
+		$tmpl->assign('quota', $storageInfo['quota']);
+		$tmpl->assign('total_human', \OC_Helper::humanFileSize($storageInfo['total']));
+        return $tmpl;
+    }
+
+    public function getSectionID() {
+        return 'general';
+    }
+
+}
