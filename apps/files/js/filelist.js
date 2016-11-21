c1b37020229ae4e16f9641cb14ef9883990c27f7
Michael Jobst <mjobst+github@tecratech.de>
2016-11-21 15:03:45 +0100

    Fixed size issues on main detail view and disappearing of share recipients (#26603)
    * fixed size issues on main detail view and disappearing of share recipients
    
    * Changes due to code comments
    
    * Moved reloadProperties() to FileInfoModel
    
    * Solved Scrutinizer issues
    
    * Bugfix: undefined value used on error
    
    * check if options are set for FileInfoModel.initialize()

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -437,7 +437,9 @@
 			// In the future the FileList should work with Backbone.Collection
 			// and contain existing models that can be used.
 			// This method would in the future simply retrieve the matching model from the collection.
-			var model = new OCA.Files.FileInfoModel(this.elementToFile($tr));
+			var model = new OCA.Files.FileInfoModel(this.elementToFile($tr), {
+				filesClient: this.filesClient
+			});
 			if (!model.get('path')) {
 				model.set('path', this.getCurrentDirectory(), {silent: true});
 			}
@@ -883,10 +885,13 @@
 				mimetype: $el.attr('data-mime'),
 				mtime: parseInt($el.attr('data-mtime'), 10),
 				type: $el.attr('data-type'),
-				size: parseInt($el.attr('data-size'), 10),
 				etag: $el.attr('data-etag'),
 				permissions: parseInt($el.attr('data-permissions'), 10)
 			};
+			var size = $el.attr('data-size');
+			if (size) {
+				data.size = parseInt(size, 10);
+			}
 			var icon = $el.attr('data-icon');
 			if (icon) {
 				data.icon = icon;
@@ -1020,7 +1025,7 @@
 		 * Returns whether the given file info must be hidden
 		 *
 		 * @param {OC.Files.FileInfo} fileInfo file info
-		 * 
+		 *
 		 * @return {boolean} true if the file is a hidden file, false otherwise
 		 */
 		_isHiddenFile: function(file) {
@@ -2801,7 +2806,6 @@
 			});
 			uploader.on('fail', function(e, data) {
 				self._uploader.log('filelist handle fileuploadfail', e, data);
-				
 				self._uploads = [];
 
 				//if user pressed cancel hide upload chrome
