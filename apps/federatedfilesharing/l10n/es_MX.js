38165a901901d63690cebe965b004f13c0adf5c9
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-11-24 01:55:26 +0100

    [tx-robot] updated from transifex

--- apps/federatedfilesharing/l10n/es_MX.js
+++ apps/federatedfilesharing/l10n/es_MX.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "federatedfilesharing",
     {
+    "File is already shared with %s" : "El archivo ya se compartió con %s",
     "Open documentation" : "Abrir documentación"
 },
 "nplurals=2; plural=(n != 1);");
