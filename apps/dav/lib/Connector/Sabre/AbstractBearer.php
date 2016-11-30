45983f10532cab6fd1ca0caf2740c165fb2ab4c4
Jonathan Neugebauer <jonathan.neugebauer@mailbox.org>
2016-11-30 08:35:03 +0100

    Update PHPDoc

--- apps/dav/lib/Connector/Sabre/AbstractBearer.php
+++ apps/dav/lib/Connector/Sabre/AbstractBearer.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * @author Lukas Biermann
+ * @author Nina Herrmann
+ * @author Wladislaw Iwanzow
+ * @author Dennis Meis
+ * @author Jonathan Neugebauer
+ *
+ * @copyright Copyright (c) 2016, Project Seminar "PSSL16" at the University of Muenster.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 namespace OCA\DAV\Connector\Sabre;
 
@@ -8,19 +30,11 @@ use Sabre\HTTP\RequestInterface;
 use Sabre\HTTP\ResponseInterface;
 
 /**
- * ownCloud - dav_oauth2
- *
- * HTTP Bearer authentication backend class
+ * HTTP Bearer authentication backend class.
  *
  * This class can be used by authentication objects wishing to use HTTP Bearer
  * Most of the logic is handled, implementors just need to worry about
  * the determineUsername method.
- *
- * This file is licensed under the Affero General Public License version 3 or
- * later. See the COPYING file.
- *
- * @author Jonathan Neugebauer
- * @copyright Jonathan Neugebauer 2016
  */
 abstract class AbstractBearer implements BackendInterface {
 
--- apps/dav/lib/Connector/Sabre/OAuth2.php
+++ apps/dav/lib/Connector/Sabre/OAuth2.php
@@ -1,27 +1,43 @@
 <?php
+/**
+ * @author Lukas Biermann
+ * @author Nina Herrmann
+ * @author Wladislaw Iwanzow
+ * @author Dennis Meis
+ * @author Jonathan Neugebauer
+ *
+ * @copyright Copyright (c) 2016, Project Seminar "PSSL16" at the University of Muenster.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 namespace OCA\DAV\Connector\Sabre;
+
 use OCA\OAuth2\Db\AccessTokenMapper;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Db\DoesNotExistException;
 
 /**
- * ownCloud - dav_oauth2
- *
- * OAuth 2.0 authentication backend class
- *
- * This file is licensed under the Affero General Public License version 3 or
- * later. See the COPYING file.
- *
- * @author Jonathan Neugebauer
- * @copyright Jonathan Neugebauer 2016
+ * OAuth 2.0 authentication backend class.
  */
 class OAuth2 extends AbstractBearer {
 
-    /**
-     * OAuth2 constructor.
-     * @param string $principalPrefix
-     */
+	/**
+	 * OAuth2 constructor.
+	 *
+	 * @param string $principalPrefix
+	 */
 	public function __construct($principalPrefix = 'principals/users/') {
 		$this->principalPrefix = $principalPrefix;
 
