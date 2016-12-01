aa723b024d2450e08d491756a1397fe8b4ab70fc
Tom Needham <needham.thomas@gmail.com>
2016-12-01 15:13:38 +0000

    Port over admin set upload to new route

--- apps/files/js/admin.js
+++ apps/files/js/admin.js
@@ -18,6 +18,12 @@ function switchPublicFolder() {
 	});
 }
 
+function setUpload(val, success, failure) {
+	$.post(OC.generateUrl('apps/files/ajax/setupload.php'), {maxUploadSize: val})
+		.done(success)
+		.error(failure);
+}
+
 $(document).ready(function() {
 	// Execute the function after loading DOM tree
 	switchPublicFolder();
@@ -25,4 +31,24 @@ $(document).ready(function() {
 		// To get rid of onClick()
 		switchPublicFolder();
 	});
+
+	$('#submitFilesAdminSettings').click(function(event) {
+		event.preventDefault();
+		$('#setUploadMessage').remove();
+		setUpload($('#maxUploadSize').val(), function(data) {
+			// Success
+			if(data.data != null) {
+				$('#maxUploadSize').val(data.data);
+			}
+			// Say it saved
+			$('#submitFilesAdminSettings').after('<span class="msg success" id="setUploadMessage">'+t('files', 'Saved')+'</span>');
+			setTimeout(function() {
+				$('#setUploadMessage').remove();
+			},3000);
+		}, function(err) {
+			// Failure
+			$('#submitFilesAdminSettings').after('<span class="msg error" id="setUploadMessage">'+t('files', 'There was an error saving')+'</span>');
+		});
+	});
+
 });
