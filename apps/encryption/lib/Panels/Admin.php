cbfc8481c8aefd5be2dce335829fe569564ca557
Tom Needham <needham.thomas@gmail.com>
2016-12-02 10:19:31 +0000

    kil end php tags and add phpdoc

--- apps/encryption/lib/Panels/Admin.php
+++ apps/encryption/lib/Panels/Admin.php
@@ -16,55 +16,64 @@ use OCP\IUserSession;
 
 class Admin implements ISettings {
 
-    protected $config;
+	/** @var IConfig  */
+	protected $config;
 
-    public function __construct(IConfig $config,
-                                ILogger $logger,
-                                IUserSession $userSession,
-                                IUserManager $userManager,
-                                ISession $session) {
-        $this->config = $config;
-        $this->logger = $logger;
-        $this->userSession = $userSession;
-        $this->userManager = $userManager;
-        $this->session = $session;
-    }
+	/** @var ILogger  */
+	protected $logger;
 
-    public function getPriority() {
-        return 0;
-    }
+	/** @var IUserSession  */
+	protected $userSession;
 
-    public function getSectionID() {
-        return 'security';
-    }
+	/** @var IUserManager  */
+	protected $userManager;
 
-    public function getPanel() {
-        $tmpl = new Template('encryption', 'settings-admin');
-        $crypt = new Crypt(
-        	$this->logger,
-        	$this->userSession,
-        	$this->config,
-        	\OC::$server->getL10N('encryption'));
-        $util = new Util(
-        	new View(),
-        	$crypt,
-        	$this->logger,
-        	$this->userSession,
-        	$this->config,
-        	$this->userManager
-        );
-        // Check if an adminRecovery account is enabled for recovering files after lost pwd
-        $recoveryAdminEnabled = $this->config->getAppValue('encryption', 'recoveryAdminEnabled', '0');
-        $session = new Session($this->session);
-        $encryptHomeStorage = $util->shouldEncryptHomeStorage();
-        $tmpl->assign('recoveryEnabled', $recoveryAdminEnabled);
-        $tmpl->assign('initStatus', $session->getStatus());
-        $tmpl->assign('encryptHomeStorage', $encryptHomeStorage);
-        $tmpl->assign('masterKeyEnabled', $util->isMasterKeyEnabled());
+	/** @var ISession  */
+	protected $session;
 
-        return $tmpl;
+	public function __construct(IConfig $config,
+								ILogger $logger,
+								IUserSession $userSession,
+								IUserManager $userManager,
+								ISession $session) {
+		$this->config = $config;
+		$this->logger = $logger;
+		$this->userSession = $userSession;
+		$this->userManager = $userManager;
+		$this->session = $session;
+	}
 
-    }
+	public function getPriority() {
+		return 0;
+	}
+
+	public function getSectionID() {
+		return 'security';
+	}
+
+	public function getPanel() {
+		$tmpl = new Template('encryption', 'settings-admin');
+		$crypt = new Crypt(
+			$this->logger,
+			$this->userSession,
+			$this->config,
+			\OC::$server->getL10N('encryption'));
+		$util = new Util(
+			new View(),
+			$crypt,
+			$this->logger,
+			$this->userSession,
+			$this->config,
+			$this->userManager
+		);
+		// Check if an adminRecovery account is enabled for recovering files after lost pwd
+		$recoveryAdminEnabled = $this->config->getAppValue('encryption', 'recoveryAdminEnabled', '0');
+		$session = new Session($this->session);
+		$encryptHomeStorage = $util->shouldEncryptHomeStorage();
+		$tmpl->assign('recoveryEnabled', $recoveryAdminEnabled);
+		$tmpl->assign('initStatus', $session->getStatus());
+		$tmpl->assign('encryptHomeStorage', $encryptHomeStorage);
+		$tmpl->assign('masterKeyEnabled', $util->isMasterKeyEnabled());
+		return $tmpl;
+	}
 }
-
-?>
