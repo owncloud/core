f97b14d6fdd3dbe27f485c3384ded8867992b060
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-12-04 01:55:10 +0100

    [tx-robot] updated from transifex

--- apps/files_versions/l10n/fa.js
+++ apps/files_versions/l10n/fa.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Could not revert: %s" : "بازگردانی امکان ناپذیر است: %s",
     "Versions" : "نسخه ها",
     "Failed to revert {file} to revision {timestamp}." : "برگرداندن {file} به نسخه {timestamp} با شکست روبرو شد",
+    "_%n byte_::_%n bytes_" : ["%n بایت"],
     "Restore" : "بازیابی",
     "No other versions available" : "نسخه ی دیگری در دسترس نیست",
     "More versions..." : "نسخه های بیشتر"
