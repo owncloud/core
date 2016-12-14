730d6aeb0d8111309e43f2f0b6f7bf7745a8e992
Thomas Müller <thomas.mueller@tmit.eu>
2016-12-14 21:41:06 +0100

    Fix unit test execution

--- tests/lib/Encryption/Keys/StorageTest.php
+++ tests/lib/Encryption/Keys/StorageTest.php
@@ -24,17 +24,29 @@
 namespace Test\Encryption\Keys;
 
 use OC\Encryption\Keys\Storage;
+use OC\Encryption\Util;
+use OC\Files\View;
 use Test\TestCase;
+use Test\Traits\UserTrait;
 
+/**
+ * Class StorageTest
+ *
+ * @group DB
+ *
+ * @package Test\Encryption\Keys
+ */
 class StorageTest extends TestCase {
+
+	use UserTrait;
 
 	/** @var Storage */
 	protected $storage;
 
-	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	/** @var \PHPUnit_Framework_MockObject_MockObject | Util */
 	protected $util;
 
-	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	/** @var \PHPUnit_Framework_MockObject_MockObject | View */
 	protected $view;
 
 	/** @var \PHPUnit_Framework_MockObject_MockObject */
@@ -232,6 +244,7 @@ class StorageTest extends TestCase {
 			->with($this->equalTo('/user1/files_encryption/encModule/user1.publicKey'))
 			->willReturn(true);
 
+		$this->createUser('user1', '123456');
 		$this->assertSame('key',
 			$this->storage->getUserKey('user1', 'publicKey', 'encModule')
 		);
