d6b06c9a327813f439c88fc60e3cbeb326d699c7
Felix Rupp <github@felixrupp.com>
2017-02-06 17:05:11 +0100

    Add postLogout hook to finish sessions from external session managers (#27048)
    * Add postLogout hook to finish sessions from external session managers like CAS
    
    * Add postLogout hook to finish sessions from external session managers like CAS

--- lib/private/User/Session.php
+++ lib/private/User/Session.php
@@ -10,6 +10,7 @@
  * @author Robin McCorkell <robin@mccorkell.me.uk>
  * @author Thomas Müller <thomas.mueller@tmit.eu>
  * @author Vincent Petry <pvince81@owncloud.com>
+ * @author Felix Rupp <kontakt@felixrupp.com>
  *
  * @copyright Copyright (c) 2016, ownCloud GmbH.
  * @license AGPL-3.0
@@ -66,6 +67,7 @@ use Symfony\Component\EventDispatcher\GenericEvent;
  * - preRememberedLogin(string $uid)
  * - postRememberedLogin(\OC\User\User $user)
  * - logout()
+ * - postLogout()
  *
  * @package OC\User
  */
@@ -725,6 +727,7 @@ class Session implements IUserSession, Emitter {
 		$this->setLoginName(null);
 		$this->unsetMagicInCookie();
 		$this->session->clear();
+		$this->manager->emit('\OC\User', 'postLogout');
 	}
 
 	/**
