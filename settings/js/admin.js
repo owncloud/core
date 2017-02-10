1dd4e8aaf296ffc6bd43ef464f767652ac8c8896
Tom Needham <needham.thomas@gmail.com>
2017-02-10 09:46:29 +0000

    Split out background jobs js from admin panel js

--- settings/js/admin.js
+++ settings/js/admin.js
@@ -35,19 +35,6 @@ $(document).ready(function(){
 		} );
 	});
 
-	$('#backgroundjobs span.crondate').tipsy({gravity: 's', live: true});
-
-	$('#backgroundjobs input').change(function(){
-		if($(this).is(':checked')){
-			var mode = $(this).val();
-			if (mode === 'ajax' || mode === 'webcron' || mode === 'cron') {
-				OC.AppConfig.setValue('core', 'backgroundjobs_mode', mode);
-				// clear cron errors on background job mode change
-				OC.AppConfig.deleteKey('core', 'cronErrors');
-			}
-		}
-	});
-
 	$('#shareAPIEnabled').change(function() {
 		$('#shareAPI p:not(#enable)').toggleClass('hidden', !this.checked);
 	});
