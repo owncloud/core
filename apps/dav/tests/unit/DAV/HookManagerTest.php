052701137804a6d885f5d7674f3f6b52b7a6df9a
Tom Needham <needham.thomas@gmail.com>
2017-02-15 21:57:32 +0000

    Fix delete user dav hook to only delete users addressbooks

--- apps/dav/tests/unit/DAV/HookManagerTest.php
+++ apps/dav/tests/unit/DAV/HookManagerTest.php
@@ -207,13 +207,14 @@ class HookManagerTest extends TestCase {
 		$card = $this->getMockBuilder(CardDavBackend::class)
 			->disableOriginalConstructor()
 			->getMock();
-		$card->expects($this->once())->method('getAddressBooksForUser')->willReturn([
+		$card->expects($this->once())->method('getUsersOwnAddressBooks')->willReturn([
 			['id' => 'personal']
 		]);
-		$card->expects($this->once())->method('deleteAddressBook');
+		$card->expects($this->once())->method('deleteAddressBook')->with('personal');
 
 		$hm = new HookManager($userManager, $syncService, $cal, $card, $this->l10n);
 		$hm->preDeleteUser(['uid' => 'newUser']);
 		$hm->postDeleteUser(['uid' => 'newUser']);
 	}
+
 }
