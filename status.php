a7173150d6b077d90d31a6123e3e3e1b68238dcc
Martin <martin.mattel@diemattels.at>
2017-02-23 23:20:41 +0100

    Moving status code into a function

--- status.php
+++ status.php
@@ -34,21 +34,8 @@ try {
 
 	require_once __DIR__ . '/lib/base.php';
 
-	$systemConfig = \OC::$server->getSystemConfig();
+	$values = \OCP\Util::getStatusInfo();
 
-	$installed = (bool) $systemConfig->getValue('installed', false);
-	$maintenance = (bool) $systemConfig->getValue('maintenance', false);
-	# see core/lib/private/legacy/defaults.php and core/themes/example/defaults.php
-	# for description and defaults
-	$defaults = new \OCP\Defaults();
-	$values= [
-		'installed'=>$installed,
-		'maintenance' => $maintenance,
-		'needsDbUpgrade' => \OCP\Util::needUpgrade(),
-		'version'=>implode('.', \OCP\Util::getVersion()),
-		'versionstring'=>OC_Util::getVersionString(),
-		'edition'=>OC_Util::getEditionString(),
-		'productname'=>$defaults->getName()];
 	if (OC::$CLI) {
 		print_r($values);
 	} else {
