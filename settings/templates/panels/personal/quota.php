6943004e0fdf138b3a6acf5d8444e8a47ebb0124
David Toledo <dtoledo@solidgear.es>
2017-02-27 08:48:11 +0100

    Add percentage to user's quota info
    Fixes https://github.com/owncloud/core/issues/24011

--- settings/templates/panels/personal/quota.php
+++ settings/templates/panels/personal/quota.php
@@ -9,8 +9,8 @@
 	<div style="width:<?php p($_['usage_relative']);?>%"
 		<?php if($_['usage_relative'] > 80): ?> class="quota-warning" <?php endif; ?>>
 		<p id="quotatext">
-			<?php print_unescaped($l->t('You are using <strong>%s</strong> of <strong>%s</strong>',
-			[$_['usage'], $totalSpace ]));?>
+			<?php print_unescaped($l->t('You are using <strong>%s</strong> of <strong>%s</strong> (<strong>%s</strong> %%)',
+			[$_['usage'], $totalSpace,  $_['usage_relative']]));?>
 		</p>
 	</div>
 </div>
