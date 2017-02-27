e5076a1749dd0744f628442037f5c64b65704526
David Toledo <dtoledo@solidgear.es>
2017-02-27 13:41:05 +0100

    Add percentage to user's quota info (#27258)
    * Add percentage to user's quota info
    
    Fixes https://github.com/owncloud/core/issues/24011
    
    * Do not show percentage if the quota is Unlimited
    
    * translate quota
    
    * correct condition and remove print_unescaped

--- settings/templates/panels/personal/quota.php
+++ settings/templates/panels/personal/quota.php
@@ -9,8 +9,13 @@
 	<div style="width:<?php p($_['usage_relative']);?>%"
 		<?php if($_['usage_relative'] > 80): ?> class="quota-warning" <?php endif; ?>>
 		<p id="quotatext">
-			<?php print_unescaped($l->t('You are using <strong>%s</strong> of <strong>%s</strong>',
-			[$_['usage'], $totalSpace ]));?>
+			<?php if ($_['quota'] === \OCP\Files\FileInfo::SPACE_UNLIMITED): ?>	
+                                <?php p($l->t('You are using %s of %s',
+				[$_['usage'], $totalSpace]));?>
+                        <?php else: ?>
+                                <?php p($l->t('You are using %s of %s (%s %%)',
+				[$_['usage'], $totalSpace,  $_['usage_relative']]));?>
+                        <?php endif ?>
 		</p>
 	</div>
 </div>
