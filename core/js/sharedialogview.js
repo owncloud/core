ed02e684f2a15e878a071fe0998f3336f2a7a691
Felix Heidecke <felix@heidecke.me>
2017-03-06 15:10:36 +0100

    Rename SHARE_TYPE_EMAIL to …_GUEST Attach Share Plugin in OC instead of OCA

--- core/js/sharedialogview.js
+++ core/js/sharedialogview.js
@@ -129,7 +129,7 @@
 				'onShareWithFieldChanged'
 			);
 
-			OC.Plugins.attach('OCA.Share.ShareDialogView', this);
+			OC.Plugins.attach('OC.Share.ShareDialogView', this);
 		},
 
 		onShareWithFieldChanged: function() {
