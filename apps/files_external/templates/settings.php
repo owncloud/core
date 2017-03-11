4cf92170ecb010cb00d2023bdf0a3dc9a426f6d5
Isac Andrei <isacandrei19@gmail.com>
2017-03-11 23:33:44 +0200

    27347 hotfix

--- apps/files_external/templates/settings.php
+++ apps/files_external/templates/settings.php
@@ -42,11 +42,11 @@
 		</label>
 	</p>
 	<?php endif; ?>
-	<p class="
-		<?php if ($_['enableExternalStorage']): ?> 
-			hidden 
+	<p
+		<?php if ($_['enableExternalStorage']): ?>
+			class="hidden"
 		<?php endif; ?>
-		files_external_message"
+		id="files_external_message"
 	>
 		<?php p($l->t('External storage has been disabled by the administrator')); ?>
 	</p>
