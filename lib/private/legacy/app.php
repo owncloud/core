f1dc2d9d3b2039d0bfc3528e57b11df2e4c93564
Thomas Müller <thomas.mueller@tmit.eu>
2017-03-15 12:25:16 +0100

    Validate session only if we are not in a basic auth request

--- lib/private/legacy/app.php
+++ lib/private/legacy/app.php
@@ -124,7 +124,10 @@ class OC_App {
 		// once all authentication apps are loaded we can validate the session
 		if (is_null($types) || in_array('authentication', $types)) {
 			if (\OC::$server->getUserSession()) {
-				\OC::$server->getUserSession()->validateSession();
+				$davUser = \OC::$server->getUserSession()->getSession()->get(\OCA\DAV\Connector\Sabre\Auth::DAV_AUTHENTICATED);
+				if (is_null($davUser)) {
+					\OC::$server->getUserSession()->validateSession();
+				}
 			}
 		}
 
