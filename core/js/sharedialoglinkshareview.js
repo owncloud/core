ce6e139cb8c562f282f5acf1e007df0dc7b5d44c
Felix Heidecke <felix@heidecke.me>
2017-03-24 14:50:24 +0100

    Introduce "remove password" button

--- core/js/sharedialoglinkshareview.js
+++ core/js/sharedialoglinkshareview.js
@@ -116,9 +116,13 @@
 				shareType: this.model.get('shareType')
 			};
 
-			// TODO: need a way to clear password (check if "encryptedPassword" was set)
+			// Reset password for the time being
+			if (this.model.get("resetPassword")) {
+				attributes.password = '';
+			}
+
 			if (password) {
-				// only set password explicitly if changed, else leave previous value
+				// only set password explicitly if changed
 				attributes.password = password;
 			}
 
@@ -170,6 +174,7 @@
 					} else {
 						done();
 					}
+					this.model.unset("resetPassword");
 				},
 				error: function (model, xhr) {
 					var msg = xhr.responseJSON.ocs.meta.message;
@@ -234,6 +239,20 @@
 			return this;
 		},
 
+		_onClickReset: function () {
+			var $dialog              = $('.oc-dialog:visible'),
+				$inputPassword 		 = $dialog.find('.linkPassText'),
+				$buttonReset    	 = $dialog.find('.removePassword');
+
+			this.model.set("resetPassword", true);
+
+			$inputPassword
+				.val('')
+				.attr('placeholder', PASSWORD_PLACEHOLDER_MESSAGE);
+
+			$buttonReset.remove();
+		},
+
 		_onClickSave: function() {
 			var self = this;
 			this._save().then(function() {
@@ -243,13 +262,9 @@
 
 		_onClickCancel: function() {
 			this.$dialog.ocdialog('close');
-		},
+			this.model.unset("resetPassword");
 
-		_onClickRemove: function() {
-			this._remove();
-			this.$dialog.ocdialog('close');
 		},
-
 		/**
 		 * @returns {Function} from Handlebars
 		 * @private
@@ -266,9 +281,26 @@
 		 */
 		show: function() {
 			var self = this;
-			var title = t('files_sharing', 'Edit link share: {name}', {name : this.itemModel.getFileInfo().getFullPath() });
+			var title = t('files_sharing', 'Edit link share: {name}', {name: this.itemModel.getFileInfo().getFullPath()});
+			var buttons = [{
+				text: t('core', 'Save'),
+				click: _.bind(this._onClickSave, this),
+				defaultButton: true
+			}, {
+				text: t('core', 'Cancel'),
+				click: _.bind(this._onClickCancel, this)
+			}];
+
 			if (this.model.isNew()) {
-				title = t('files_sharing', 'Create link share: {name}', {name : this.itemModel.getFileInfo().getFullPath() });
+				title = t('files_sharing', 'Create link share: {name}', {name: this.itemModel.getFileInfo().getFullPath()});
+			}
+			else if (this.model.get('encryptedPassword') !== null) {
+				buttons.push({
+					classes: 'removePassword -float-left',
+					text: t('core', 'Remove password'),
+					click: _.bind(this._onClickReset, this),
+					defaultButton: false
+				});
 			}
 
 			// hack the dialogs
@@ -276,16 +308,7 @@
 				'',
 				title,
 				'custom',
-				[
-					{
-						text: t('core', 'Save'),
-						click: _.bind(this._onClickSave, this),
-						defaultButton: true
-					}, {
-						text: t('core', 'Cancel'),
-						click: _.bind(this._onClickCancel, this)
-					}
-				],
+				buttons,
 				null,
 				true,
 				'public-link-modal'
