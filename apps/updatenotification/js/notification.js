5efde9c15c4b7ff88208eaf2789674553928a9c2
Vincent Petry <pvince81@owncloud.com>
2017-04-20 16:01:49 +0200

    Fix update notification
    show() doesn't support HTML, need to call showHtml() directly

--- apps/updatenotification/js/notification.js
+++ apps/updatenotification/js/notification.js
@@ -10,20 +10,18 @@
  *
  */
 
+/* global oc_updateState */
+
 /**
  * this gets only loaded if an update is available and then shows a temporary notification
  */
 $(document).ready(function(){
-	var head = $('html > head'),
-		version = oc_updateState.updateVersion,
+	var version = oc_updateState.updateVersion,
 		docLink = oc_updateState.updateLink,
 		text = t('core', '{version} is available. Get more information on how to update.', {version: version}),
 		element = $('<a>').attr('href', docLink).attr('target','_blank').text(text);
 
-	OC.Notification.show(element, 
-		{
-			isHTML: true,
+	OC.Notification.showHtml(element, {
 			type: 'error'
-		}
-	);
+	});
 });
