ce40499f02717afa0af25bf2f67629a3159a29d3
Vincent Petry <pvince81@owncloud.com>
2017-04-26 13:32:22 +0200

    Set version to 10.0.0

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
-$OC_Version = [10, 0, 0, 11];
+$OC_Version = [10, 0, 0, 12];
 
 // The human readable string
-$OC_VersionString = '10.0.0 RC5';
+$OC_VersionString = '10.0.0';
 
 $OC_VersionCanBeUpgradedFrom = [9, 1];
 
