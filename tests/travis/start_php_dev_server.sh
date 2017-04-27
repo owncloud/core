6ab723fa52d2ff195e276420dde32cfa3bab7e36
Artur Neumann <info@individual-it.net>
2017-04-27 15:14:48 +0545

    fix selenium tests
    8888 seems to be used by sauce connect now
    so we use alternative port for the php dev server
    and the start_php_dev_server.sh script does a little bit more checking about what actually happened

--- tests/travis/start_php_dev_server.sh
+++ tests/travis/start_php_dev_server.sh
@@ -6,7 +6,18 @@
 # @copyright 2017 Artur Neumann info@individual-it.net
 #
 
-
-set -e
-
 php -S $SRV_HOST_NAME:$SRV_HOST_PORT > /dev/null 2>&1 &
+serverPID=$!
+sleep 1
+
+ps -p $serverPID  > /dev/null 2>&1
+
+if [ $? -eq 1 ] ; then
+	echo "could not start PHP development webserver"
+	echo -e "\nnetstat output:\n"
+	netstat -l -n -p
+	exit 1
+else
+	echo "started PHP dev. webserver with PID $serverPID"
+fi
+
