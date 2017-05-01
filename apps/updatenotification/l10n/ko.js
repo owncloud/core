bfd9b11d21584158f1b9300058c3b5d7db87c89b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-05-02 01:59:57 +0200

    [tx-robot] updated from transifex

--- apps/updatenotification/l10n/ko.js
+++ apps/updatenotification/l10n/ko.js
@@ -1,13 +1,17 @@
 OC.L10N.register(
     "updatenotification",
     {
+    "Update notifications" : "업데이트 알림",
     "{version} is available. Get more information on how to update." : "{version}을(를) 사용할 수 있습니다. 업데이트하는 방법에 대해서 알아보십시오.",
     "Updated channel" : "업데이트 된 채널",
+    "Update for %1$s to version %2$s is available." : "%1$s을(를) 버전 %2$s(으)로 업데이트할 수 있습니다.",
     "Updater" : "업데이터",
     "A new version is available: %s" : "새 버전을 사용할 수 있습니다: %s",
     "Open updater" : "업데이터 열기",
     "Your version is up to date." : "최신 버전을 사용하고 있습니다.",
+    "Checked on %s" : "%s에 확인함",
     "Update channel:" : "업데이트 채널:",
-    "You can always update to a newer version / experimental channel. But you can never downgrade to a more stable channel." : "항상 새로운 버전이나 실험 채널로 업그레이드할 수 있지만, 안정 채널로 다운그레이드할 수는 없습니다."
+    "You can always update to a newer version / experimental channel. But you can never downgrade to a more stable channel." : "항상 새로운 버전이나 실험 채널로 업그레이드할 수 있지만, 안정 채널로 다운그레이드할 수는 없습니다.",
+    "Notify members of the following groups about available updates:" : "다음 그룹 구성원에게 업데이트 알림 전달:"
 },
 "nplurals=1; plural=0;");
