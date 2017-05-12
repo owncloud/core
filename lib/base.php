e414515239d89549602f91f66e2bac7d0ae8cc90
Pascal Bach <pascal.bach@nextrem.ch>
2017-05-12 22:57:53 +0200

    Introduce OWNCLOUD_CONFIG_DIR environment variable
    This allosw to change the config location.

--- lib/base.php
+++ lib/base.php
@@ -128,6 +128,8 @@ class OC {
 			self::$configDir = OC::$SERVERROOT . '/' . PHPUNIT_CONFIG_DIR . '/';
 		} elseif(defined('PHPUNIT_RUN') and PHPUNIT_RUN and is_dir(OC::$SERVERROOT . '/tests/config/')) {
 			self::$configDir = OC::$SERVERROOT . '/tests/config/';
+		} elseif($dir = getenv('OWNCLOUD_CONFIG_DIR')) {
+			self::$configDir = rtrim($dir, '/') . '/';
 		} else {
 			self::$configDir = OC::$SERVERROOT . '/config/';
 		}
