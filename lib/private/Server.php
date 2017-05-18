5e31acdd5ae1b17abb72d04b11f348aa36d836ca
Philipp Schaffrath <github@philipp.schaffrath.email>
2017-05-18 15:41:50 +0200

    added alias for IThemeService

--- lib/private/Server.php
+++ lib/private/Server.php
@@ -808,6 +808,7 @@ class Server extends ServerContainer implements IServerContainer {
 		$this->registerService('ThemeService', function ($c) {
 			return new ThemeService($this->getSystemConfig()->getValue('theme'));
 		});
+		$this->registerAlias('IThemeService', 'ThemeService');
 		$this->registerAlias('OCP\IUserSession', 'UserSession');
 		$this->registerAlias('OCP\Security\ICrypto', 'Crypto');
 	}
