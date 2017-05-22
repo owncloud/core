9faf366ca5a38b70c8f18fbc2dd80b82e458350b
Peter Prochaska <info@peter-prochaska.de>
2017-05-22 12:38:14 +0200

    fix logic issue

--- apps/files_sharing/lib/API/Share20OCS.php
+++ apps/files_sharing/lib/API/Share20OCS.php
@@ -31,7 +31,7 @@ use OCP\IURLGenerator;
 use OCP\IUser;
 use OCP\Files\IRootFolder;
 use OCP\Lock\LockedException;
-use OCP\Share;
+use OCP\Share\IShare;
 use OCP\Share\IManager;
 use OCP\Share\Exceptions\ShareNotFound;
 use OCP\Share\Exceptions\GenericShareException;
@@ -442,6 +442,10 @@ class Share20OCS {
 		$groupShares = $this->shareManager->getSharedWith($this->currentUser->getUID(), \OCP\Share::SHARE_TYPE_GROUP, $node, -1, 0);
 
 		$shares = array_merge($userShares, $groupShares);
+
+		$shares = array_filter($shares, function(IShare $share) {
+			return $share->getShareOwner() !== $this->currentUser->getUID();
+ 		});
 
 		$formatted = [];
 		foreach ($shares as $share) {
