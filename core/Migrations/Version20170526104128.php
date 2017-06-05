48b63bb2a19fbbc7fb242b989ca909fb64361421
Tom Needham <needham.thomas@gmail.com>
2017-06-05 16:13:45 +0200

    Handle 4 byte database encodings, max key length 191 chars

--- core/Migrations/Version20170526104128.php
+++ core/Migrations/Version20170526104128.php
@@ -12,25 +12,48 @@ use OCP\Migration\ISchemaMigration;
 class Version20170526104128 implements ISchemaMigration {
 
 	public function changeSchema(Schema $schema, array $options) {
+		// Get the table
 		$prefix = $options['tablePrefix'];
 		$table = $schema->getTable("{$prefix}account_terms");
+
 		// Check column length
-		if($table->getColumn('term')->getLength() === 255) {
+		if($table->getColumn('term')->getLength() === 191) {
 			// we don't need to adjust it
 			return;
 		}
 
 		// Need to shorten the column by one character
-		// Check if we have any terms taking up the full 256 chars (unlikely)
+		// Check if we have any terms taking up 192 or more chars (unlikely)
 
 		/** @var IDBConnection $db */
 		$db = \OC::$server->getDatabaseConnection();
+
+
 		$qb = $db->getQueryBuilder();
-		$qb->delete('account_terms')
-			->where($qb->expr()->gte($qb->expr()->length('term'), new Literal(256)));
-		$qb->execute();
+		$qb->select(['id', 'term'])
+			->from('account_terms')
+			->where($qb->expr()->gte($qb->expr()->length('term'), new Literal(192)));
+		$results = $qb->execute();
+
+		// Now shorten these terms
+		$db->beginTransaction();
+		while($longTerm = $results->fetch()) {
+			$qb->update('account_terms')
+				->where($qb->expr()->eq('id', $qb->createNamedParameter($longTerm['id'])))
+				->set('term', $qb->createNamedParameter($this->trimTerm($longTerm['term'])))
+				->execute();
+		}
+		$db->commit();
 
 		// Now update the column length
-		$table->getColumn('term')->setLength(255);
+		$table->getColumn('term')->setLength(191);
     }
+
+	/**
+	 * @param $longTerm
+	 * @return string the shortened string ready for the new db column
+	 */
+    public function trimTerm($longTerm) {
+		return (string) substr($longTerm, 0, 191);
+	}
 }
