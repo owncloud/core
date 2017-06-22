c9f83f931fccd0d40432141e806155d23e722621
Phil Davis <phil@jankaritech.com>
2017-06-22 11:36:20 +0545

    Implement group add delete removeuser in occ command
    Signed-off-by: Phil Davis <phil@jankaritech.com>

---
+++ core/Command/Group/RemoveUser.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @author Phil Davis <phil@jankaritech.com>
+ *
+ * @copyright Copyright (c) 2017, ownCloud GmbH
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OC\Core\Command\Group;
+
+use OCP\IGroupManager;
+use OCP\IUserManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class RemoveUser extends Command {
+	/** @var \OCP\IGroupManager */
+	protected $groupManager;
+
+	/**
+	 * @param IGroupManager $groupManager
+	 */
+	public function __construct(IGroupManager $groupManager, IUserManager $userManager) {
+		parent::__construct();
+		$this->groupManager = $groupManager;
+		$this->userManager = $userManager;
+	}
+
+	protected function configure() {
+		$this
+			->setName('group:removeuser')
+			->setDescription('remove user(s) from a group')
+			->addArgument(
+				'group',
+				InputArgument::REQUIRED,
+				'Name of the group'
+			)
+			->addOption(
+				'user',
+				'u',
+				InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY,
+				'user that should be removed from the group'
+			);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$groupName = $input->getArgument('group');
+		$group = $this->groupManager->get($groupName);
+		if (!$group) {
+			$output->writeln('<error>Group "' . $groupName . '" does not exist</error>');
+			return 1;
+		}
+
+		$users = $input->getOption('user');
+
+		if (!count($users)) {
+			$output->writeln('<error>No users specified</error>');
+			return 1;
+		}
+
+		foreach ($users as $userName) {
+			$user = $this->userManager->get($userName);
+			if ($user) {
+				if ($group->inGroup($user)) {
+					$group->removeUser($user);
+					$output->writeln('User "' . $user->getUID() . '" removed from group "' . $group->getGID() . '"');
+				} else {
+					$output->writeln('<error>User "' . $userName . '" could not be found in group "' . $group->getGID() . '"</error>');
+				}
+			} else {
+				$output->writeln('<error>User "' . $userName . '" could not be found - not removed from group "' . $group->getGID() . '"</error>');
+			}
+		}
+	}
+}
---
+++ tests/Core/Command/Group/DeleteTest.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * @author Phil Davis <phil@jankaritech.com>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace Tests\Core\Command\Group;
+
+use OC\Core\Command\Group\Delete;
+use Test\TestCase;
+
+class DeleteTest extends TestCase {
+	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	protected $groupManager;
+	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	protected $consoleInput;
+	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	protected $consoleOutput;
+
+	/** @var \Symfony\Component\Console\Command\Command */
+	protected $command;
+
+	protected function setUp() {
+		parent::setUp();
+
+		$groupManager = $this->groupManager = $this->getMockBuilder('OCP\IGroupManager')
+			->disableOriginalConstructor()
+			->getMock();
+		$this->consoleInput = $this->createMock('Symfony\Component\Console\Input\InputInterface');
+		$this->consoleOutput = $this->createMock('Symfony\Component\Console\Output\OutputInterface');
+
+		/** @var \OCP\IGroupManager $groupManager */
+		$this->command = new Delete($groupManager);
+	}
+
+
+	public function validGroupLastSeen() {
+		return [
+			[true, 'The specified group was deleted'],
+			[false, 'The specified group could not be deleted'],
+		];
+	}
+
+	/**
+	 * @dataProvider validGroupLastSeen
+	 *
+	 * @param bool $deleteSuccess
+	 * @param string $expectedString
+	 */
+	public function testValidGroup($deleteSuccess, $expectedString) {
+		$group = $this->createMock('OCP\IGroup');
+		$group->expects($this->once())
+			->method('delete')
+			->willReturn($deleteSuccess);
+
+		$this->groupManager->expects($this->once())
+			->method('get')
+			->with('group')
+			->willReturn($group);
+
+		$this->consoleInput->expects($this->once())
+			->method('getArgument')
+			->with('group')
+			->willReturn('group');
+
+		$this->consoleOutput->expects($this->once())
+			->method('writeln')
+			->with($this->stringContains($expectedString));
+
+		self::invokePrivate($this->command, 'execute', [$this->consoleInput, $this->consoleOutput]);
+	}
+
+	public function testInvalidGroup() {
+		$this->groupManager->expects($this->once())
+			->method('get')
+			->with('group')
+			->willReturn(null);
+
+		$this->consoleInput->expects($this->once())
+			->method('getArgument')
+			->with('group')
+			->willReturn('group');
+
+		$this->consoleOutput->expects($this->once())
+			->method('writeln')
+			->with($this->stringContains('Group does not exist'));
+
+		self::invokePrivate($this->command, 'execute', [$this->consoleInput, $this->consoleOutput]);
+	}
+}
