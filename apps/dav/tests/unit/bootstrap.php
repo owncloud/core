641f2e5113b6283ec604b242e5fa2d37d0490145
Phil Davis <phil@jankaritech.com>
2017-06-27 18:57:42 +0545

    Was overzealous changing lib to unit
    Signed-off-by: Phil Davis <phil@jankaritech.com>

--- apps/dav/tests/unit/bootstrap.php
+++ apps/dav/tests/unit/bootstrap.php
@@ -23,7 +23,7 @@ if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 
-require_once __DIR__.'/../../../../unit/base.php';
+require_once __DIR__.'/../../../../lib/base.php';
 
 if(!class_exists('PHPUnit_Framework_TestCase')) {
 	require_once('PHPUnit/Autoload.php');
