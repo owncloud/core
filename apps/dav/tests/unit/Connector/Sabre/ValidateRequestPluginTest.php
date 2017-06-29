685af44643d03e86eddb142951d08ac34276f824
Piotr Mrowczynski <mrow4a@yahoo.com>
2017-06-29 20:47:49 +0200

    Add plugin which verifies that request headers as directed for proper endpoint

---
+++ apps/dav/tests/unit/Connector/Sabre/ValidateRequestPluginTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @author Piotr Mrowczynski <piotr@owncloud.com>
+ *
+ * @copyright Copyright (c) 2017, ownCloud GmbH
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\DAV\Tests\unit\Connector\Sabre;
+
+use OCA\DAV\Connector\Sabre\ValidateRequestPlugin;
+use Test\TestCase;
+use OCP\IConfig;
+
+/**
+ * Class ValidateRequestPluginTest
+ *
+ * @package OCA\DAV\Tests\unit\Connector\Sabre
+ */
+class ValidateRequestPluginTest extends TestCase {
+	/** @var \Sabre\DAV\Server */
+	private $server;
+
+	/** @var ValidateRequestPlugin */
+	private $plugin;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->server = new \Sabre\DAV\Server();
+	}
+
+	/**
+	 * @expectedException \Sabre\DAV\Exception\ServiceUnavailable
+	 * @expectedExceptionMessage Specified OC-Chunk-Offset header is allowed only in dav endpoint
+	 */
+	public function testNewChunkingInOldEndpoint() {
+		$headers['OC-Chunk-Offset'] = 0;
+		$this->plugin = new ValidateRequestPlugin('webdav');
+
+		$this->server->httpRequest = new \Sabre\HTTP\Request('PUT', 'http://server/root_url/remote.php/webdav/testchunkold.txt', $headers);
+		$this->plugin->initialize($this->server);
+
+		$this->plugin->checkValidity();
+	}
+
+	/**
+	 * @expectedException \Sabre\DAV\Exception\ServiceUnavailable
+	 * @expectedExceptionMessage Specified  header (HTTP_OC_CHUNKED/OC-Chunked header) is allowed only in webdav endpoint
+	 */
+	public function testOldChunkingInNewEndpoint1() {
+		$headers['HTTP_OC_CHUNKED'] = 1;
+
+		$this->plugin = new ValidateRequestPlugin('dav');
+		$this->server->httpRequest = new \Sabre\HTTP\Request('PUT', 'http://server/root_url/remote.php/dav/files/user/testnew.txt', $headers);
+		$this->plugin->initialize($this->server);
+
+		$this->plugin->checkValidity();
+	}
+
+	/**
+	 * @expectedException \Sabre\DAV\Exception\ServiceUnavailable
+	 * @expectedExceptionMessage Specified  header (HTTP_OC_CHUNKED/OC-Chunked header) is allowed only in webdav endpoint
+	 */
+	public function testOldChunkingInNewEndpoint2() {
+		$headers['OC-Chunked'] = 1;
+
+		$this->plugin = new ValidateRequestPlugin('dav');
+		$this->server->httpRequest = new \Sabre\HTTP\Request('PUT', 'http://server/root_url/remote.php/dav/files/user/testnew.txt', $headers);
+		$this->plugin->initialize($this->server);
+
+		$this->plugin->checkValidity();
+	}
+
+}
