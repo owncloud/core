d8a73198b588ef938d9aaa9ea13ec2cbaec4fc51
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2017-07-14 18:23:26 +0300

    Fix 2 issues with postgres introduced by doctrine/dbal

--- lib/private/DB/ConnectionFactory.php
+++ lib/private/DB/ConnectionFactory.php
@@ -129,6 +129,10 @@ class ConnectionFactory {
 				$additionalConnectionParams['platform'] = new OCSqlitePlatform();
 				$eventManager->addEventSubscriber(new SQLiteSessionInit(true, $journalMode));
 				break;
+			case 'pgsql':
+			case 'postgresql':
+				$additionalConnectionParams['platform'] = new OCPostgreSqlPlatform();
+				break;
 		}
 		/** @var Connection $connection */
 		$connection = DriverManager::getConnection(
