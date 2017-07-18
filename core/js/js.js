0aa24fcb526888e3c84528ae84ce242166fbc48c
Felix Heidecke <felix@heidecke.me>
2017-07-18 10:53:01 +0200

    Introduce new menu-layout, logo and background image

--- core/js/js.js
+++ core/js/js.js
@@ -1559,20 +1559,6 @@ function initCore() {
 	setupMainMenu();
 	setupUserMenu();
 
-	// move triangle of apps dropdown to align with app name triangle
-	// 2 is the additional offset between the triangles
-	if ($('#navigation').length) {
-		$('#header #owncloud + .menutoggle').one('click', function () {
-			var caret = $('.header-appname + .caret');
-			var caretPosition = caret.offset().left - caret.width() / 2;
-
-			// only position the menu arrow if the caret is in its reach
-			if (caretPosition <= 255) {
-				$('head').append('<style>#navigation:after { left: ' + caretPosition + 'px; }</style>');
-			}
-		});
-	}
-
 	// just add snapper for logged in users
 	if ($('#app-navigation').length && !$('html').hasClass('lte9')) {
 
