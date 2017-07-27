e5b10e1809367940780a6327de2dc4235018b887
Felix Heidecke <felix@heidecke.me>
2017-07-27 12:10:36 +0200

    Add email validation, show email field even if the model !isNew

--- core/js/sharedialoglinkshareview.js
+++ core/js/sharedialoglinkshareview.js
@@ -244,7 +244,7 @@
 			var isPasswordSet = !!this.model.get('encryptedPassword');
 
 			// only show email field for new shares and if enabled globally
-			var showEmailField = this.model.isNew() && this.configModel.isMailPublicNotificationEnabled();
+			var showEmailField = this.configModel.isMailPublicNotificationEnabled();
 
 			this.$el.html(this.template({
 				cid: this.cid,
