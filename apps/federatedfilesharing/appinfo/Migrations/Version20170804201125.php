f5b1f6837d2ad36a195e1581854c8fd9b7b0a0d0
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2017-08-07 17:15:35 +0300

    Port tables to migration

--- apps/federatedfilesharing/appinfo/Migrations/Version20170804201125.php
+++ apps/federatedfilesharing/appinfo/Migrations/Version20170804201125.php
@@ -2,21 +2,31 @@
 
 namespace OCA\FederatedFileSharing\Migrations;
 use Doctrine\DBAL\Schema\Schema;
-use OC\DB\MDB2SchemaReader;
 use OCP\Migration\ISchemaMigration;
 
 /** Creates initial schema */
 class Version20170804201125 implements ISchemaMigration {
 	public function changeSchema(Schema $schema, array $options) {
 		$prefix = $options['tablePrefix'];
-		if ($schema->hasTable("{$prefix}federated_reshares")) {
-			return;
+		if (!$schema->hasTable("{$prefix}federated_reshares")) {
+			$table = $schema->createTable("{$prefix}federated_reshares");
+			$table->addColumn('share_id', 'bigint', [
+				'unsigned' => false,
+				'notnull' => true,
+				'length' => 11,
+			]);
+
+			$table->addColumn('remote_id', 'bigint', [
+				'unsigned' => false,
+				'notnull' => true,
+				'length' => 11,
+				'comment' => 'share ID at the remote server'
+			]);
+
+			$table->addUniqueIndex(
+				['share_id'],
+				'share_id_index'
+			);
 		}
-		// not that valid ....
-		$schemaReader = new MDB2SchemaReader(
-			\OC::$server->getConfig(),
-			\OC::$server->getDatabaseConnection()->getDatabasePlatform()
-		);
-		$schemaReader->loadSchemaFromFile(__DIR__ . '/../database.xml', $schema);
 	}
 }
