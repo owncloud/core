4bc251500e1f55f0278d00929509e8eead8c5b0e
Artur Neumann <artur@jankaritech.com>
2017-08-17 10:05:01 +0545

    unit tests for chunking file size calculation

--- tests/lib/FileChunkingTest.php
+++ tests/lib/FileChunkingTest.php
@@ -67,4 +67,64 @@ class FileChunkingTest extends \Test\TestCase {
 
 		$this->assertEquals($expected, $fileChunking->isComplete());
 	}
+
+	/**
+	 * data provider for testGetCurrentSize
+	 * [[array of chunksizes], [chunk ids to ignore], expected size]
+	 * 
+	 * @return array[][]|number[][]|number[][][]
+	 */
+	public function dataGetCurrentSize() {
+		return [
+			[[1, 2, 3], [ ], 6],
+			[[100, 200, 300], [ ], 600],
+			[[1, 2, 0, 4], [ ], 7],
+			[[10, 20, 30, 40], [2], 70],
+			[[10, 20, 30, 40], [0, 2], 60],
+			[[10, 20, 30, 40], [0, 2, 3], 20],
+		];
+	}
+
+	/**
+	 * @dataProvider dataGetCurrentSize
+	 * 
+	 * @param array $chunkSizes
+	 * @param array $ignoreChunks
+	 * @param int $expected
+	 * @return void
+	 */
+	public function testGetCurrentSize(
+		array $chunkSizes, array $ignoreChunks, $expected
+	) {
+		$fileName = "file";
+		$transferId = "42";
+		$fileChunking = $this->getMockBuilder('\OC_FileChunking')
+			->setMethods(['getCache'])
+			->setConstructorArgs(
+				[[
+					'name' => $fileName,
+					'transferid' => $transferId,
+					'chunkcount' => count($chunkSizes),
+				]]
+			)
+			->getMock();
+
+		$cache = $this->createMock('\OC\Cache\File');
+		$chunkNo = 0;
+		foreach ($chunkSizes as $size) {
+			$chunkSizeMap [] = [ 
+				$fileName . "-chunking-" . $transferId . "-" . $chunkNo,
+				$size 
+			];
+			$chunkNo++;
+		}
+
+		$cache->expects($this->atLeastOnce())
+			->method('size')
+			->will($this->returnValueMap($chunkSizeMap));
+
+		$fileChunking->method('getCache')->willReturn($cache);
+
+		$this->assertEquals($expected, $fileChunking->getCurrentSize($ignoreChunks));
+	}
 }
