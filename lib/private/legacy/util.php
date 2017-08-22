df46d150f6bd3d1b3afd41df8b3c6a77b0537c22
Phil Davis <phil@jankaritech.com>
2017-08-22 08:28:31 +0545

    BSD case for 32 bit filemtime and install warning

--- lib/private/legacy/util.php
+++ lib/private/legacy/util.php
@@ -1250,12 +1250,30 @@ class OC_Util {
 	}
 
 	/**
-	 * Checks whether the server is running on Mac OS X
+	 * Checks whether the server is running on the given OS type
 	 *
-	 * @return bool true if running on Mac OS X, false otherwise
+	 * @param string $osType linux|mac|bsd
+	 * @return bool true if running on that OS type, false otherwise
 	 */
-	public static function runningOnMac() {
-		return (strtoupper(substr(PHP_OS, 0, 6)) === 'DARWIN');
+	public static function runningOn($osType) {
+		$osType = strtolower($osType);
+
+		switch($osType) {
+			case 'linux':
+				return (strtolower(substr(PHP_OS, 0, 5)) === 'linux');
+				break;
+
+			case 'mac':
+				return (strtolower(substr(PHP_OS, 0, 6)) === 'darwin');
+				break;
+
+			case 'bsd':
+				return (strpos(strtolower(PHP_OS), 'bsd') !== false);
+				break;
+
+			default;
+				return false;
+		}
 	}
 
 	/**
