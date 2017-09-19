55cf106bc895aa15868b54c7d96b8af951ddf182
Phil Davis <phil@jankaritech.com>
2017-09-19 21:22:11 +0545

    Code standard phpcs

--- tests/integration/features/bootstrap/CapabilitiesContext.php
+++ tests/integration/features/bootstrap/CapabilitiesContext.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * ownCloud
+ *
+ * @author Joas Schilling <coding@schilljs.com>
+ * @author Sergio Bertolin <sbertolin@owncloud.com>
+ * @author Phillip Davis <phil@jankaritech.com>
+ * @copyright 2017 ownCloud GmbH
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License,
+ * as published by the Free Software Foundation;
+ * either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ */
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
@@ -15,6 +37,7 @@ class CapabilitiesContext implements Context, SnippetAcceptingContext {
 	/**
 	 * @Then /^fields of capabilities match with$/
 	 * @param \Behat\Gherkin\Node\TableNode|null $formData
+	 * @return void
 	 */
 	public function checkCapabilitiesResponse(\Behat\Gherkin\Node\TableNode $formData) {
 		$capabilitiesXML = $this->getCapabilitiesXml();
@@ -33,6 +56,9 @@ class CapabilitiesContext implements Context, SnippetAcceptingContext {
 		}
 	}
 
+	/**
+	 * @return void
+	 */
 	protected function setupAppConfigs() {
 		// Remember the current capabilities
 		$this->getCapabilitiesCheckResponse();
@@ -48,6 +74,9 @@ class CapabilitiesContext implements Context, SnippetAcceptingContext {
 		$this->modifyServerConfig('core', 'shareapi_enforce_expire_date', 'no');
 	}
 
+	/**
+	 * @return void
+	 */
 	protected function restoreAppConfigs() {
 		// Restore the previous capabilities settings
 		$this->restoreCommonSharingConfigs();
