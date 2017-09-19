808778a144146522a3a2818939c04ad0d742d2c9
Vincent Petry <pvince81@owncloud.com>
2017-09-19 10:44:13 +0200

    Workaround for Oracle trigger for fileid

--- lib/private/repair/repairmismatchfilecachepath.php
+++ lib/private/repair/repairmismatchfilecachepath.php
@@ -412,6 +412,19 @@ class RepairMismatchFileCachePath extends BasicEmitter implements \OC\RepairStep
 
 		// If we reused the fileid then this is the id to return
 		if($reuseFileId !== null) {
+			// with Oracle, the trigger gets in the way and does not let us specify
+			// a fileid value on insert
+			if ($this->connection->getDatabasePlatform() instanceof OraclePlatform) {
+				$lastFileId = $this->connection->lastInsertId('*PREFIX*filecache');
+				if ($reuseFileId !== $lastFileId) {
+					// use update to set it directly
+					$qb = $this->connection->getQueryBuilder();
+					$qb->update('filecache')
+						->set('fileid', $qb->createNamedParameter($reuseFileId))
+						->where($qb->expr()->eq('fileid', $qb->createNamedParameter($lastFileId)));
+					$qb->execute();
+				}
+			}
 			return $reuseFileId;
 		} else {
 			// Else we inserted a new row with auto generated id, use that
