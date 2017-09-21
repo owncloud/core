c2502e636debbdb4835c86445bca6a335e7adf85
Hemant Mann <hemant.mann121@gmail.com>
2017-09-22 02:24:53 +0530

    New Cacheable Flysystem Adapter

---
+++ lib/private/Files/Storage/CacheableFlysystem.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * @author Hemant Mann <hemant.mann121@gmail.com>
+ *
+ * @copyright Copyright (c) 2017, ownCloud GmbH
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OC\Files\Storage;
+
+use League\Flysystem\FileNotFoundException;
+use Icewind\Streams\IteratorDirectory;
+
+/**
+ * Generic Cacheable adapter between flysystem adapters and owncloud's storage system
+ *
+ * To use: subclass and call $this->buildFlysystem with the flysystem adapter of choice
+ */
+abstract class CacheableFlysystem extends \OC\Files\Storage\Flysystem {
+	/**
+	 * Stores the results in cache for the current request to prevent multiple requests to the API
+	 * @var array
+	 */
+	protected $cacheContents = [];
+
+	/**
+	 * Get the location which will be used as a key in cache
+	 * If Storage is not case sensitive then convert the key to lowercase
+	 * @param  string $path Path to file/folder
+	 * @return string
+	 */
+	public function getCacheLocation($path) {
+		$location = $this->buildPath($path);
+		if ($location === '') {
+			$location = '/';
+		}
+
+		if ($this->isCaseInsensitiveStorage) {
+			$location = strtolower($location);
+		}
+		return $location;
+	}
+
+	/**
+	 * Check if Cache Contains the data for given path, if not then get the data
+	 * from flysystem and store it in cache for this request lifetime
+	 * @param  string $path Path to file/folder
+	 * @return array|boolean
+	 */
+	public function getFlysystemMetadata($path) {
+		$location = $this->getCacheLocation($path);
+		if (!isset($this->cacheContents[$location])) {
+			try {
+				$this->cacheContents[$location] = $this->flysystem->getMetadata($location);
+			} catch (FileNotFoundException $e) {
+				$this->cacheContents[$location] = false;
+			}
+		}
+		return $this->cacheContents[$location];
+	}
+
+	/**
+	 * Store the list of files/folders in the cache so that subsequent requests in the
+	 * same request life cycle does not call the flysystem API
+	 * @param  array $contents Return value of $this->flysystem->listContents
+	 */
+	public function updateCache($contents) {
+		foreach ($contents as $object) {
+			$path = $object['path'];
+			$this->cacheContents[$path] = $object;
+		}
+	}
+
+	/**
+	 * Check for existence of file/folder for the given path
+	 * @param  string  $path Path to file/folder
+	 * @return boolean
+	 */
+	public function hasPath($path) {
+		return (bool) $this->getFlysystemMetadata($path);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function opendir($path) {
+		try {
+			$content = $this->flysystem->listContents($this->buildPath($path));
+			$this->updateCache($content);
+		} catch (FileNotFoundException $e) {
+			return false;
+		}
+		$names = array_map(function ($object) {
+			return $object['basename'];
+		}, $content);
+		return IteratorDirectory::wrap($names);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function file_exists($path) {
+		return $this->hasPath($path);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function filesize($path) {
+		if ($this->is_dir($path)) {
+			return 0;
+		} else {
+			$info = $this->getFlysystemMetadata($path);
+			return $info['size'];
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function filemtime($path) {
+		$info = $this->getFlysystemMetadata($path);
+		return $info['timestamp'];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function stat($path) {
+		$info = $this->getFlysystemMetadata($path);
+		return [
+			'mtime' => $info['timestamp'],
+			'size' => $info['size']
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function filetype($path) {
+		if ($path === '' or $path === '/' or $path === '.') {
+			return 'dir';
+		}
+		$info = $this->getFlysystemMetadata($path);
+		return $info['type'];
+	}
+}
