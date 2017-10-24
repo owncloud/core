be7510914df601d1120b068f00d650c4405df405
Phil Davis <phil@jankaritech.com>
2017-10-24 15:24:07 +0545

    TestHelpers code review

--- tests/TestHelpers/LoggingHelper.php
+++ tests/TestHelpers/LoggingHelper.php
@@ -23,14 +23,14 @@ namespace TestHelpers;
 
 /**
  * Helper to read and analyze the owncloud log file
- * 
+ *
  * @author Artur Neumann <artur@jankaritech.com>
  *
  */
 class LoggingHelper {
 	/**
 	 * returns the log file path
-	 * 
+	 *
 	 * @throws \Exception
 	 * @return string
 	 */
@@ -76,7 +76,7 @@ class LoggingHelper {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param string $logLevel (debug|info|warning|error|fatal)
 	 * @return void
 	 * @throws \InvalidArgumentException
@@ -120,7 +120,7 @@ class LoggingHelper {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param string $backend (owncloud|syslog|errorlog)
 	 * @return void
 	 * @throws \InvalidArgumentException
@@ -179,7 +179,7 @@ class LoggingHelper {
 	}
 
 	/**
-	 * 
+	 *
 	 * @return void
 	 * @throws \Exception
 	 */
@@ -193,13 +193,14 @@ class LoggingHelper {
 
 	/**
 	 * reads x last lines from a file
-	 * Slightly modified version of 
+	 * Slightly modified version of
 	 * http://www.geekality.net/2011/05/28/php-tail-tackling-large-files/
-	 * 
+	 *
 	 * @param string $filepath file to read
 	 * @param int $noOfLinesToRead no of lines to read
 	 * @param bool $adaptive make the file buffer adaptive
 	 * @return array lines of the file to read
+	 * @throws \Exception
 	 * @author Torleif Berger, Lorenzo Stanco
 	 * @link http://stackoverflow.com/a/15025877/995958
 	 * @license http://creativecommons.org/licenses/by/3.0/
@@ -213,7 +214,7 @@ class LoggingHelper {
 		if ($f === false) {
 			throw new \Exception("could not read file '$filepath'");
 		}
-		
+
 		// Sets buffer size, according to the number of lines to retrieve.
 		// This gives a performance boost when reading a few lines from the file.
 		if (!$adaptive) {
@@ -221,46 +222,45 @@ class LoggingHelper {
 		} else {
 			$buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
 		}
-		
+
 		// Jump to last character
 		fseek($f, -1, SEEK_END);
-		
+
 		// Read it and adjust line number if necessary
 		// Otherwise the result would be wrong if file doesn't end with a blank line
 		if (fread($f, 1) != "\n") {
 			$lines -= 1;
 		}
-		
+
 		// Start reading
 		$output = '';
-		$chunk = '';
-		
+
 		// While we would like more
 		while (ftell($f) > 0 && $lines >= 0) {
-			
+
 			// Figure out how far back we should jump
 			$seek = min(ftell($f), $buffer);
-			
+
 			// Do the jump (backwards, relative to where we are)
 			fseek($f, -$seek, SEEK_CUR);
-			
+
 			// Read a chunk and prepend it to our output
 			$output = ($chunk = fread($f, $seek)) . $output;
-			
+
 			// Jump back to where we started reading
 			fseek($f, -mb_strlen($chunk, '8bit'), SEEK_CUR);
-			
+
 			// Decrease our line counter
 			$lines -= substr_count($chunk, "\n");
 		}
-		
+
 		// While we have too many lines
 		// (Because of buffer size we might have read too many)
 		while ($lines++ < 0) {
 			// Find first newline and remove all text before that
 			$output = substr($output, strpos($output, "\n") + 1);
 		}
-		
+
 		// Close file and return
 		fclose($f);
 		$output = explode("\n", $output);
