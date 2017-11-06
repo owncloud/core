176737509ca17120bcb32709e6de68aeee0cf0d1
Artur Neumann <artur@jankaritech.com>
2017-11-06 17:20:49 +0545

    UI tests to restrict sharing

--- tests/ui/features/bootstrap/SharingContext.php
+++ tests/ui/features/bootstrap/SharingContext.php
@@ -61,18 +61,31 @@ class SharingContext extends RawMinkContext implements Context {
 	 * @param string $folder
 	 * @param string $remote
 	 * @param string $user
+	 * @param int $maxRetries
+	 * @param boolean $quiet
 	 * @return void
 	 */
-	public function theFileFolderIsSharedWithTheUser($folder, $remote, $user) {
+	public function theFileFolderIsSharedWithTheUser(
+		$folder, $remote, $user, $maxRetries = 5, $quiet = false
+	) {
 		$this->filesPage->waitTillPageIsloaded($this->getSession());
+		try {
+			$this->filesPage->closeSharingDialog();
+		} catch (Exception $e) {
+			//we don't care
+		}
 		$this->sharingDialog = $this->filesPage->openSharingDialog(
 			$folder, $this->getSession()
 		);
 		$user = $this->featureContext->substituteInLineCodes($user);
 		if ($remote === "remote") {
-			$this->sharingDialog->shareWithRemoteUser($user, $this->getSession());
+			$this->sharingDialog->shareWithRemoteUser(
+				$user, $this->getSession(), $maxRetries, $quiet
+			);
 		} else {
-			$this->sharingDialog->shareWithUser($user, $this->getSession());
+			$this->sharingDialog->shareWithUser(
+				$user, $this->getSession(), $maxRetries, $quiet
+			);
 		}
 		$this->iCloseTheShareDialog();
 	}
@@ -85,6 +98,11 @@ class SharingContext extends RawMinkContext implements Context {
 	 */
 	public function theFileFolderIsSharedWithTheGroup($folder, $group) {
 		$this->filesPage->waitTillPageIsloaded($this->getSession());
+		try {
+			$this->filesPage->closeSharingDialog();
+		} catch (Exception $e) {
+			//we don't care
+		}
 		$this->sharingDialog = $this->filesPage->openSharingDialog(
 			$folder, $this->getSession()
 		);
@@ -287,18 +305,38 @@ class SharingContext extends RawMinkContext implements Context {
 	}
 
 	/**
-	 * @Then it should not be possible to share the file/folder :name
-	 * @param string $name
+	 * @Then /^it should not be possible to share the (?:file|folder) "([^"]*)"(?: with "([^"]*)")?$/
+	 * @param string $fileName
 	 * @return void
 	 */
-	public function itShouldNotBePossibleToShare($name) {
+	public function itShouldNotBePossibleToShare($fileName, $shareWith = null) {
+		$sharingWasPossible = false;
 		try {
-			$this->theFileFolderIsSharedWithTheUser($name, null, null);
+			$this->theFileFolderIsSharedWithTheUser($fileName, null, $shareWith, 2, true);
+			$sharingWasPossible = true;
 		} catch (ElementNotFoundException $e) {
-			PHPUnit_Framework_Assert::assertContains(
-				'could not find shar',
-				$e->getMessage()
-			);
+			$possibleMessages = [
+				'could not find share-with-field',
+				'could not find sharing button in fileRow',
+				'could not share with \'' . $shareWith . '\''
+			];
+			foreach ($possibleMessages as $message) {
+				$foundMessage = strpos($e->getMessage(), $message);
+				if ($foundMessage !== false) {
+					break;
+				}
+			}
+			if ($foundMessage === false) {
+				throw new Exception(
+					'exception message has to contain "could not find share-with-field",' .
+					' "could not find sharing button in fileRow" or' .
+					' "could not share with \'...\'"but was: "' .
+					$e->getMessage() . '"'
+				);
+			}
+		}
+		if ($sharingWasPossible === true) {
+			throw new Exception("It was possible to share the file");
 		}
 	}
 
@@ -327,11 +365,11 @@ class SharingContext extends RawMinkContext implements Context {
 	protected function setupSharingConfigs() {
 		$settings = [
 			[
-			'capabilitiesApp' => 'files_sharing',
-			'capabilitiesParameter' => 'api_enabled',
-			'testingApp' => 'core',
-			'testingParameter' => 'shareapi_enabled',
-			'testingState' => true
+				'capabilitiesApp' => 'files_sharing',
+				'capabilitiesParameter' => 'api_enabled',
+				'testingApp' => 'core',
+				'testingParameter' => 'shareapi_enabled',
+				'testingState' => true
 			],
 			[
 				'capabilitiesApp' => 'files_sharing',
@@ -359,6 +397,13 @@ class SharingContext extends RawMinkContext implements Context {
 				'capabilitiesParameter' => 'share_with_group_members_only',
 				'testingApp' => 'core',
 				'testingParameter' => 'shareapi_only_share_with_group_members',
+				'testingState' => false
+			],
+			[
+				'capabilitiesApp' => 'files_sharing',
+				'capabilitiesParameter' => 'share_with_membership_groups_only',
+				'testingApp' => 'core',
+				'testingParameter' => 'shareapi_only_share_with_membership_groups',
 				'testingState' => false
 			],
 			[
---
+++ tests/ui/features/sharing/restrictSharing.feature
@@ -0,0 +1,61 @@
+Feature: restrict Sharing
+
+As admin
+I would like to be able to restrict the sharing function
+So that users can only share files with specific users and groups
+
+	Background:
+		Given these users exist:
+		|username|password|displayname|email       |
+		|user1   |1234    |User One   |u1@oc.com.np|
+		|user2   |1234    |User Two   |u2@oc.com.np|
+		|user3   |1234    |User Three |u2@oc.com.np|
+		And these groups exist:
+		|groupname|
+		|grp1     |
+		|grp2     |
+		And the user "user1" is in the group "grp1"
+		And the user "user2" is in the group "grp1"
+		And the user "user3" is in the group "grp2"
+		And I am on the login page
+		And I login with username "user1" and password "1234"
+		And I logout
+		And I login with username "user3" and password "1234"
+		And I logout
+		And I login with username "user2" and password "1234"
+		
+	Scenario: Restrict users to only share with users in their groups
+		When the setting "Restrict users to only share with users in their groups" in the section "Sharing" is enabled
+		And I am on the files page
+		Then it should not be possible to share the folder "simple-folder" with "User Three"
+		When the folder "simple-folder" is shared with the user "User One"
+		And I logout
+		And I login with username "user1" and password "1234"
+		Then the folder "simple-folder (2)" should be listed
+
+	Scenario: Restrict users to only share with groups they are member of
+		When the setting "Restrict users to only share with groups they are member of" in the section "Sharing" is enabled
+		And I am on the files page
+		Then it should not be possible to share the folder "simple-folder" with "grp2"
+		When the folder "simple-folder" is shared with the group "grp1"
+		And I logout
+		And I login with username "user1" and password "1234"
+		Then the folder "simple-folder (2)" should be listed
+
+	Scenario: Do not restrict users to only share with groups they are member of
+		When the setting "Restrict users to only share with groups they are member of" in the section "Sharing" is disabled
+		And I am on the files page
+		When the folder "simple-folder" is shared with the group "grp2"
+		And I logout
+		And I login with username "user3" and password "1234"
+		Then the folder "simple-folder (2)" should be listed
+
+	Scenario: Forbid sharing with groups
+		When the setting "Allow sharing with groups" in the section "Sharing" is disabled
+		And I am on the files page
+		Then it should not be possible to share the folder "simple-folder" with "grp1"
+		And it should not be possible to share the folder "simple-folder" with "grp2"
+		When the folder "simple-folder" is shared with the user "User One"
+		And I logout
+		And I login with username "user1" and password "1234"
+		Then the folder "simple-folder (2)" should be listed
