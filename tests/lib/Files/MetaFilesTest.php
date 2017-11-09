5296aa2d63469a9dde041f53a22cf5b41576aa76
Thomas Müller <thomas.mueller@tmit.eu>
2017-11-09 14:43:29 +0100

    Enhance tests

--- tests/lib/Files/MetaFilesTest.php
+++ tests/lib/Files/MetaFilesTest.php
@@ -27,6 +27,7 @@ use OC\Files\Meta\MetaFileIdNode;
 use OC\Files\Meta\MetaFileVersionNode;
 use OC\Files\Meta\MetaRootNode;
 use OC\Files\Meta\MetaVersionCollection;
+use OC\Files\Node\File;
 use OC\Files\View;
 use OCA\Files_Versions\Hooks;
 use OCP\Files\Folder;
@@ -89,10 +90,17 @@ class MetaFilesTest extends TestCase {
 		$metaNodeOfFile = \OC::$server->getRootFolder()->get("meta/{$info->getId()}/v/$versionId");
 		$this->assertInstanceOf(MetaFileVersionNode::class, $metaNodeOfFile);
 		$this->assertEquals($versionId, $metaNodeOfFile->getName());
+		$this->assertEquals(4, $metaNodeOfFile->getSize());
+		$this->assertEquals('text/plain', $metaNodeOfFile->getMimetype());
+		$this->assertEquals($info->getMTime(), $metaNodeOfFile->getMTime());
+		$this->assertTrue(is_string($metaNodeOfFile->getMTime()));
+		$this->assertTrue(strlen($metaNodeOfFile->getMTime()) > 0);
+
 		/** @var MetaFileVersionNode $metaNodeOfFile */
 		$this->assertEquals('1234', $metaNodeOfFile->getContent());
 
 		// restore a version using move
+		/** @var File $target */
 		$target = \OC::$server->getRootFolder()->get($fileName);
 		$this->assertEquals('1234567890', $target->getContent());
 		$metaNodeOfFile->copy($fileName);
