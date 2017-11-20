20db0f06f257aef69ef4da6cb341ea6cb2e10093
Tom Needham <needham.thomas@gmail.com>
2017-11-20 14:08:38 +0000

    Include console output for file scanner with repair

--- apps/files/command/scan.php
+++ apps/files/command/scan.php
@@ -36,6 +36,7 @@ use OCP\IUserManager;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\Table;
 use OC\Repair\RepairMismatchFileCachePath;
