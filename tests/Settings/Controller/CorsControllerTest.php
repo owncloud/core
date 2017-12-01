7a999e1f35f6b9c083b3b5df0b60032c00ca3859
Phil Davis <phil@jankaritech.com>
2017-12-01 20:36:28 +1100

    Handle case of first CORS whitelist add-delete

--- tests/Settings/Controller/CorsControllerTest.php
+++ tests/Settings/Controller/CorsControllerTest.php
@@ -72,6 +72,7 @@ class CorsControllerTest extends TestCase {
 		$this->config = $this->createMock(IConfig::class);
 		$this->config->method('getUserValue')->willReturn('["http:\/\/www.test.com"]');
 		$this->config->method('setUserValue')->willReturn(true);
+		$this->config->method('deleteUserValue')->willReturn(true);
 
 		$this->corsController = new CorsController(
 			'core',
@@ -155,7 +156,7 @@ class CorsControllerTest extends TestCase {
 		// the error message that invalid domain ID passed, would never be triggered
 		$this->config
 			->expects($this->once())
-			->method("setUserValue");
+			->method("deleteUserValue");
 
 		// The argument for removing domain is the ID of the white-listed domain
 		// and not the domain itself
