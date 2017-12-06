6ad614f340705b4cd33c441ee0e0930ab79510ad
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2017-12-06 16:21:16 +0300

    Remove undocumented fallback to the app-theme with default_enabled attribute

--- lib/base.php
+++ lib/base.php
@@ -581,6 +581,7 @@ class OC {
 			\OC_App::loadApps(['theme']);
 			self::initSession();
 		}
+
 		\OC::$server->getEventLogger()->end('init_session');
 
 		// incognito mode for now
@@ -827,22 +828,6 @@ class OC {
 	}
 
 	/**
-	 * Enables the defaultEnabled app theme
-	 * __do not__ call this for every request, as this parses all apps info.xml
-	 * files in order to determine which app is a default enabled theme while
-	 * not accessing the database which might not be available.
-	 */
-	public static function loadDefaultEnabledAppTheme() {
-		$defaultEnabledAppTheme = \OC_App::getDefaultEnabledAppTheme();
-
-		if ($defaultEnabledAppTheme !== false) {
-			/** @var \OC\Theme\ThemeService $themeService */
-			$themeService = \OC::$server->query('ThemeService');
-			$themeService->setAppTheme($defaultEnabledAppTheme);
-		}
-	}
-
-	/**
 	 * Handle the request
 	 */
 	public static function handleRequest() {
@@ -859,8 +844,6 @@ class OC {
 			$setupHelper = new OC\Setup(\OC::$server->getConfig(), \OC::$server->getIniWrapper(),
 				\OC::$server->getL10N('lib'), new \OC_Defaults(), \OC::$server->getLogger(),
 				\OC::$server->getSecureRandom());
-
-			self::loadDefaultEnabledAppTheme();
 
 			$controller = new OC\Core\Controller\SetupController($setupHelper);
 			$controller->run($_POST);
