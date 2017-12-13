55083c003fad36ab3439a0e620e70e3d60096506
SergioBertolinSG <sbertolin@solidgear.es>
2017-12-13 17:07:08 +0100

    Removed double step and allow test-coverage to run tests using external storage
    Added missing functionality in test-coverage

--- tests/drone/test-coverage.sh
+++ tests/drone/test-coverage.sh
@@ -15,6 +15,8 @@ rm -rf data config/config.php
 if [[ "${DB_TYPE}" == "none" || "${DB_TYPE}" == "sqlite" ]]; then
   ./occ maintenance:install -vvv --database=sqlite --database-name=owncloud --database-table-prefix=oc_ --admin-user=admin --admin-pass=admin --data-dir=$(pwd)/data
 else
+  DATABASEUSER=owncloud
+  DATABASENAME=owncloud
   case "${DB_TYPE}" in
     mariadb)
       wait-for-it mariadb:3306
@@ -24,9 +26,19 @@ else
       wait-for-it mysql:3306
       DB=mysql
       ;;
+    mysqlmb4)
+      wait-for-it mysqlmb4:3306
+      DB=mysql
+      ;;
     postgres)
       wait-for-it postgres:5432
       DB=pgsql
+      ;;
+    oracle)
+      wait-for-it oracle:1521
+      DB=oci
+      DATABASEUSER=autotest
+      DATABASENAME='XE'
       ;;
     *)
       echo "Unsupported database type!"
@@ -34,7 +46,7 @@ else
       ;;
   esac
 
-  ./occ maintenance:install -vvv --database=${DB} --database-host=${DB_TYPE} --database-user=owncloud --database-pass=owncloud --database-name=owncloud --database-table-prefix=oc_ --admin-user=admin --admin-pass=admin --data-dir=$(pwd)/data
+  ./occ maintenance:install -vvv --database=${DB} --database-host=${DB_TYPE} --database-user=${DATABASENAME} --database-pass=owncloud --database-name=${DATABASEUSER} --database-table-prefix=oc_ --admin-user=admin --admin-pass=admin --data-dir=$(pwd)/data
 fi
 
 ./occ app:enable files_sharing
@@ -44,8 +56,36 @@ fi
 ./occ app:enable federation
 ./occ app:enable federatedfilesharing
 
+if [[ "${DB_TYPE}" == "none" || "${DB_TYPE}" == "sqlite" ]]; then
+  GROUP=""
+else
+  GROUP="--group DB"
+fi
+
 if [[ -z ${FILES_EXTERNAL_TYPE} ]]; then
   exec phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest.xml ${GROUP} --coverage-clover tests/autotest-clover-${DB_TYPE}.xml
 else
+  ./occ app:enable files_external
+  case "${FILES_EXTERNAL_TYPE}" in
+    webdav)
+      wait-for-it owncloud_external:80
+       cat > config/config.webdav.php <<DELIM
+ <?php
+ return array(
+     'run'=>true,
+     'host'=>'owncloud_external:80/owncloud/remote.php/webdav/',
+     'user'=>'admin',
+     'password'=>'admin',
+     'root'=>'',
+     'wait'=> 0
+ );
+DELIM
+      ;;
+    *)
+      echo "Unsupported files external type!"
+      exit 1
+      ;;
+  esac
+
   exec phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --coverage-clover tests/autotest-external-clover-${DB_TYPE}.xml
 fi
