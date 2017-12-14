4416d965ca40e95f3f9ee248106fba843b7ebfbf
SergioBertolinSG <sbertolin@solidgear.es>
2017-12-14 10:57:44 +0100

    The unification has arrived
    This line was forgotten in the rename
    
    Name mismatch
    
    databasename and databaseuser were mixed
    
    Keeping the naming
    
    Not running javascript tests when coverage is enabled

--- tests/drone/test-phpunit.sh
+++ tests/drone/test-phpunit.sh
@@ -62,5 +62,43 @@ else
   GROUP="--group DB"
 fi
 
-exec ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest.xml ${GROUP} --log-junit tests/autotest-results-${DB_TYPE}.xml
+set_up_external_storage() {
+    ./occ app:enable files_external
+    case "${FILES_EXTERNAL_TYPE}" in
+    webdav)
+        wait-for-it owncloud_external:80
+        cat > config/config.webdav.php <<DELIM
+ <?php
+ return array(
+     'run'=>true,
+     'host'=>'owncloud_external:80/owncloud/remote.php/webdav/',
+     'user'=>'admin',
+     'password'=>'admin',
+     'root'=>'',
+     'wait'=> 0
+ );
+DELIM
+        ;;
+    *)
+        echo "Unsupported files external type!"
+        exit 1
+        ;;
+    esac
+}
 
+
+if [[ ${ENABLE_COVERAGE} == "true" ]]; then
+    if [[ -n ${FILES_EXTERNAL_TYPE} ]]; then
+        set_up_external_storage
+        exec phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --coverage-clover tests/autotest-external-clover-${DB_TYPE}.xml
+    else
+        exec phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest.xml ${GROUP} --coverage-clover tests/autotest-clover-${DB_TYPE}.xml
+    fi
+else
+    if [[ -n ${FILES_EXTERNAL_TYPE} ]]; then
+        set_up_external_storage
+        exec ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --log-junit tests/autotest-external-results-${DB_TYPE}.xml
+    else
+        exec ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest.xml ${GROUP} --log-junit tests/autotest-results-${DB_TYPE}.xml    
+    fi
+fi
