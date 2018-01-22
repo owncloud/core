3d90619ab485845b10f5c9918480a3d420f0a195
Sujith H <sharidasan@owncloud.com>
2018-01-22 17:19:00 +0530

    Validation for new storage key location
    Extra validation required if the new storage
    key location is being provided to the command.
    When is_dir returns null it wasn't captured by
    the command. And hence the command was executed
    successfully. This change would help to prevent
    such scenario. If is_dir returns null throw exception.
    
    Signed-off-by: Sujith H <sharidasan@owncloud.com>

--- core/Command/Encryption/ChangeKeyStorageRoot.php
+++ core/Command/Encryption/ChangeKeyStorageRoot.php
@@ -136,7 +136,8 @@ class ChangeKeyStorageRoot extends Command {
 	 * @throws \Exception
 	 */
 	protected function prepareNewRoot($newRoot) {
-		if ($this->rootView->is_dir($newRoot) === false) {
+		$validateNewRootDir = $this->rootView->is_dir($newRoot);
+		if (($validateNewRootDir === false) or ($validateNewRootDir === null)) {
 			throw new \Exception("New root folder doesn't exist. Please create the folder or check the permissions and try again.");
 		}
 
@@ -150,7 +151,6 @@ class ChangeKeyStorageRoot extends Command {
 		}
 
 	}
-
 
 	/**
 	 * move system key folder
