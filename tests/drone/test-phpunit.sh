d160221e3e187489b8ced424bf70d4558b0c25ea
SergioBertolinSG <sbertolin@solidgear.es>
2018-01-22 10:35:28 +0100

    Added quotes to variables and variable for drone exec
    Group variable doesn't like quotes

--- tests/drone/test-phpunit.sh
+++ tests/drone/test-phpunit.sh
@@ -46,7 +46,7 @@ else
       ;;
   esac
 
-  ./occ maintenance:install -vvv --database=${DB} --database-host=${DB_TYPE} --database-user=${DATABASEUSER} --database-pass=owncloud --database-name=${DATABASENAME} --database-table-prefix=oc_ --admin-user=admin --admin-pass=admin --data-dir=$(pwd)/data
+  ./occ maintenance:install -vvv --database="${DB}" --database-host="${DB_TYPE}" --database-user="${DATABASEUSER}" --database-pass=owncloud --database-name="${DATABASENAME}" --database-table-prefix=oc_ --admin-user=admin --admin-pass=admin --data-dir=$(pwd)/data
 fi
 
 ./occ app:enable files_sharing
@@ -88,23 +88,23 @@ DELIM
 }
 
 FILES_EXTERNAL_BACKEND_PATH=apps/files_external/tests/Storage
-FILES_EXTERNAL_TEST_TO_RUN=${FILES_EXTERNAL_TYPE}Test.php
+FILES_EXTERNAL_TEST_TO_RUN="${FILES_EXTERNAL_TYPE}"Test.php
 
 
-if [[ ${ENABLE_COVERAGE} == "true" ]]; then
-    if [[ -n ${FILES_EXTERNAL_TYPE} ]]; then
+if [[ "${ENABLE_COVERAGE}" == "true" ]]; then
+    if [[ -n "${FILES_EXTERNAL_TYPE}" ]]; then
         set_up_external_storage
-        phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --coverage-clover tests/autotest-external-clover-${DB_TYPE}.xml
-        phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --coverage-clover tests/autotest-external-clover-${DB_TYPE}-${FILES_EXTERNAL_TYPE}.xml ${FILES_EXTERNAL_BACKEND_PATH}/${FILES_EXTERNAL_TEST_TO_RUN}
+        phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --coverage-clover tests/autotest-external-clover-"${DB_TYPE}".xml
+        phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --coverage-clover tests/autotest-external-clover-"${DB_TYPE}"-"${FILES_EXTERNAL_TYPE}".xml "${FILES_EXTERNAL_BACKEND_PATH}"/"${FILES_EXTERNAL_TEST_TO_RUN}"
     else
-        phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest.xml ${GROUP} --coverage-clover tests/autotest-clover-${DB_TYPE}.xml
+        phpdbg -d memory_limit=4096M -rr ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest.xml ${GROUP} --coverage-clover tests/autotest-clover-"${DB_TYPE}".xml
     fi
 else
-    if [[ -n ${FILES_EXTERNAL_TYPE} ]]; then
+    if [[ -n "${FILES_EXTERNAL_TYPE}" ]]; then
         set_up_external_storage
-        ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --log-junit tests/autotest-external-results-${DB_TYPE}.xml
-        ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --log-junit tests/autotest-external-results-${DB_TYPE}-${FILES_EXTERNAL_TYPE}.xml ${FILES_EXTERNAL_BACKEND_PATH}/${FILES_EXTERNAL_TEST_TO_RUN}
+        ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --log-junit tests/autotest-external-results-"${DB_TYPE}".xml
+        ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest-external.xml ${GROUP} --log-junit tests/autotest-external-results-"${DB_TYPE}"-"${FILES_EXTERNAL_TYPE}".xml "${FILES_EXTERNAL_BACKEND_PATH}"/"${FILES_EXTERNAL_TEST_TO_RUN}"
     else
-        ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest.xml ${GROUP} --log-junit tests/autotest-results-${DB_TYPE}.xml    
+        ./lib/composer/bin/phpunit --configuration tests/phpunit-autotest.xml ${GROUP} --log-junit tests/autotest-results-"${DB_TYPE}".xml
     fi
 fi
