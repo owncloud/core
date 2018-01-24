5d0d849a9af62fd5d45d43bc8fc17dc0ae4d2b44
Thomas Müller <thomas.mueller@tmit.eu>
2018-01-24 09:26:35 +0100

    [stable10] When executing the data-fingerprint command a log entry is written to allow analysis who executed the command and when - refs #30154

--- tests/Core/Command/Maintenance/DataFingerprintTest.php
+++ tests/Core/Command/Maintenance/DataFingerprintTest.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @author Roeland Jago Douma <rullzer@owncloud.com>
+ * @author Thomas Müller <thomas.mueller@tmit.eu>
  *
  * @copyright Copyright (c) 2018, ownCloud GmbH
  * @license AGPL-3.0
@@ -24,31 +25,36 @@ namespace Tests\Core\Command\Maintenance;
 use OC\Core\Command\Maintenance\DataFingerprint;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\IConfig;
+use OCP\ILogger;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Test\TestCase;
 
 class DataFingerprintTest extends TestCase {
-	/** @var IConfig|\PHPUnit_Framework_MockObject_MockObject */
-	protected $config;
-	/** @var \PHPUnit_Framework_MockObject_MockObject */
-	protected $consoleInput;
-	/** @var \PHPUnit_Framework_MockObject_MockObject */
-	protected $consoleOutput;
-	/** @var ITimeFactory|\PHPUnit_Framework_MockObject_MockObject */
-	protected $timeFactory;
+	/** @var IConfig | \PHPUnit_Framework_MockObject_MockObject */
+	private $config;
+	/** @var InputInterface | \PHPUnit_Framework_MockObject_MockObject */
+	private $consoleInput;
+	/** @var OutputInterface | \PHPUnit_Framework_MockObject_MockObject */
+	private $consoleOutput;
+	/** @var ITimeFactory | \PHPUnit_Framework_MockObject_MockObject */
+	private $timeFactory;
+	/** @var ILogger | \PHPUnit_Framework_MockObject_MockObject */
+	private $logger;
 
 	/** @var \Symfony\Component\Console\Command\Command */
-	protected $command;
+	private $command;
 
 	protected function setUp() {
 		parent::setUp();
 
-		$this->config = $this->createMock('OCP\IConfig');
-		$this->timeFactory = $this->createMock('OCP\AppFramework\Utility\ITimeFactory');
-		$this->consoleInput = $this->createMock('Symfony\Component\Console\Input\InputInterface');
-		$this->consoleOutput = $this->createMock('Symfony\Component\Console\Output\OutputInterface');
+		$this->config = $this->createMock(IConfig::class);
+		$this->timeFactory = $this->createMock(ITimeFactory::class);
+		$this->consoleInput = $this->createMock(InputInterface::class);
+		$this->consoleOutput = $this->createMock(OutputInterface::class);
+		$this->logger = $this->createMock(ILogger::class);
 
-		/** @var \OCP\IConfig $config */
-		$this->command = new DataFingerprint($this->config, $this->timeFactory);
+		$this->command = new DataFingerprint($this->config, $this->timeFactory, $this->logger);
 	}
 
 	public function testSetFingerPrint() {
@@ -58,6 +64,13 @@ class DataFingerprintTest extends TestCase {
 		$this->config->expects($this->once())
 			->method('setSystemValue')
 			->with('data-fingerprint', md5(42));
+
+		$osUser = get_current_user();
+		$server = gethostname();
+
+		$this->logger->expects($this->once())
+			->method('info')
+			->with("Data fingerprint was set by $osUser@$server to a1d0c6e83f027327d8461063f4ac58a6");
 
 		self::invokePrivate($this->command, 'execute', [$this->consoleInput, $this->consoleOutput]);
 	}
