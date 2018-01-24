855f42124749dfffd9f5bac52cb1d347d96f0f71
Thomas Müller <thomas.mueller@tmit.eu>
2018-01-24 12:06:54 +0100

    Add explicit warning and ask for explicit confirmation on data-fingerprint

--- core/Command/Maintenance/DataFingerprint.php
+++ core/Command/Maintenance/DataFingerprint.php
@@ -25,8 +25,11 @@ use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\IConfig;
 use OCP\ILogger;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 
 class DataFingerprint extends Command {
@@ -54,10 +57,23 @@ class DataFingerprint extends Command {
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		$osUser = get_current_user();
-		$server = gethostname();
-		$fingerprint = md5($this->timeFactory->getTime());
-		$this->config->setSystemValue('data-fingerprint', $fingerprint);
-		$this->logger->info("Data fingerprint was set by $osUser@$server to $fingerprint");
+		$io = new SymfonyStyle($input, $output);
+		$warning = <<<EOD
+Setting the data fingerprint will notify desktop and mobile clients that a server backup has been restored. 
+Users will be prompted to resolve any conflicts between newer and older file versions.
+
+Setting the data fingerprint has a large impact on all connected clients!
+
+Only perform this operation if you really know what you are doing.
+EOD;
+
+		$io->warning($warning);
+		if ($io->confirm('Do you want to set the data fingerprint?', false)) {
+			$osUser = get_current_user();
+			$server = gethostname();
+			$fingerprint = md5($this->timeFactory->getTime());
+			$this->config->setSystemValue('data-fingerprint', $fingerprint);
+			$this->logger->info("Data fingerprint was set by $osUser@$server to $fingerprint");
+		}
 	}
 }
