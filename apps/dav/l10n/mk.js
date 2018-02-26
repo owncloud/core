0f9a22f2de1b4f907e5c3d1655dbf31ddb470e79
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2018-02-26 02:04:15 +0100

    [tx-robot] updated from transifex

--- apps/dav/l10n/mk.js
+++ apps/dav/l10n/mk.js
@@ -4,6 +4,8 @@ OC.L10N.register(
     "Contact birthdays" : "Родендени на контактите",
     "Personal" : "Лично",
     "Contacts" : "Контакти",
-    "Technical details" : "Технички детали"
+    "Technical details" : "Технички детали",
+    "Remote Address: %s" : "Далечинска адреса: %s",
+    "Request ID: %s" : "Барање ID: %s"
 },
 "nplurals=2; plural=(n % 10 == 1 && n % 100 != 11) ? 0 : 1;");
