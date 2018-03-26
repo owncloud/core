294bc9eb65dd0239dd4ce05fbdabc6585ac7ec37
Sujith H <sharidasan@owncloud.com>
2018-03-26 22:26:48 +0530

    Add symfony events for delete and create share
    Add symfony events for delete share.
    And move create share to share manager.
    
    Signed-off-by: Sujith H <sharidasan@owncloud.com>

--- lib/private/Server.php
+++ lib/private/Server.php
@@ -863,7 +863,8 @@ class Server extends ServerContainer implements IServerContainer, IServiceLoader
 				$c->getL10N('core'),
 				$factory,
 				$c->getUserManager(),
-				$c->getLazyRootFolder()
+				$c->getLazyRootFolder(),
+				$c->getEventDispatcher()
 			);
 
 			return $manager;
