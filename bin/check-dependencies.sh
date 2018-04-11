a42f73b6653ffb3a7770724c3471bbef6733244f
Matthew Setter <matthew.setter@gmail.com>
2018-04-11 10:56:26 +0200

    Improved the dependency check and install script
    This commit allows for other distributions (and versions) to be
    supported, requiring only that they be added to the supported
    distributions list and that a dependencies config file be created in the
    dependencies directory.
    
    Other than that:
    
    - the script's output is more meaningful and easier to read
    - apt output is stored in a log file
    - the script is now easier to maintain

--- bin/check-dependencies.sh
+++ bin/check-dependencies.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-set -e
-
 ## 
 ## This script checks if the required dependency packages have been installed
 ## If one or more have not been installed, then it installs them.
@@ -10,40 +7,74 @@ set -e
 ## Author: Matthew Setter <matthew@matthewsetter.com>
 ##
 
+SUPPORTED_DISTRIBUTIONS=( 
+  "Ubuntu 17.10"
+)
+LOG_FILE=output.log
+
+usage="$(basename "$0") [-h -s -d] -- script to install ownCloud's core dependencies
+
+where:
+    -h  show this help text
+    -s  show a list of distributions which this script supports
+    -d  do not install packages
+"
+
+##
+## Determine the user's distribution
+##
+function get_distribution()
+{
+  version=$( grep -E "^VERSION_ID" /etc/os-release | awk -F'=' '{print $2}' | tr -d '"')
+  name=$( grep -E "^NAME" /etc/os-release | awk -F'=' '{print $2}' | tr -d '"')
+
+  echo "${name} ${version}"
+}
+
+function show_supported_distributions()
+{
+  echo "Supported Linux Distributions:"
+  printf ' - %s\n' "${SUPPORTED_DISTRIBUTIONS[@]}"
+  echo
+}
+
+##
+## This checks if the distribution is supported
+##
+function check_distribution()
+{
+  distro=$(get_distribution)
+
+  # Console colours
+  GREEN='\e[0;32m'      # Red
+  NC='\e[0m'          # No Color
+
+  case "${SUPPORTED_DISTRIBUTIONS[@]}" in 
+    "${distro}")
+      echo -e "Detected allowed distribution: ${distro}. ${GREEN}Can continue${NC}."
+      echo
+      ;;
+    *)
+      echo "This script does not support ${distro}"
+      echo "Exiting."
+      exit -1
+  esac
+}
+
 function check_dependencies()
 {
-  core_dependencies=(
-    apache2 
-    libapache2-mod-php7.1 
-    mariadb-server 
-    openssl 
-    php-imagick 
-    php-smbclient 
-    php-ssh2 
-    php7.1-common 
-    php7.1-curl 
-    php7.1-gd 
-    php7.1-imap 
-    php7.1-intl 
-    php7.1-json 
-    php7.1-ldap 
-    php7.1-mbstring 
-    php7.1-mcrypt 
-    php7.1-mysql 
-    php7.1-pgsql 
-    php7.1-sqlite3 
-    php7.1-xml 
-    php7.1-zip
-  )
-
   missing_dependencies=()
+
+  # Determine the correct dependencies configuration file to read in
+  distro_lower=$( echo $(get_distribution) | awk '{ gsub(" ", "-"); print tolower($0) }' )
+  source "bin/dependencies/${distro_lower}.cfg"
 
   # Console colours
   RED='\033[0;31m'      # Red
-  GREEN='\032[0;31m'    # Green
   NC='\033[0m'          # No Color
 
-  echo "Checking that the required packages have been installed"
+  echo "Checking that the core dependencies have been installed."
+  echo
 
   for package in ${!core_dependencies[@]}
   do
@@ -60,16 +91,26 @@ function check_dependencies()
 
   if (( ${#missing_dependencies[@]} != 0 ))
   then 
-    echo
-    echo "Some dependencies are missing. They will now be installed."
+    if [[ -z "$DRY_RUN" ]]
+    then
+      echo
+      echo "Some dependencies are missing. They will now be installed."
+      echo 
+      echo "Updating APT cache"
+      echo
+      apt-get update --quiet &>${LOG_FILE}
 
-    echo 
-    echo "Updating APT cache"
-    apt-get update --quiet 2>/dev/null
+      echo "Installing missing dependencies"
+      echo
+      IFS=$' '
+      apt-get install --quiet --assume-yes ${missing_dependencies[*]} &>>${LOG_FILE}
 
-    echo "Installing missing dependencies"
-    IFS=$' '
-    apt-get install --quiet --assume-yes ${missing_dependencies[*]}
+      echo "All dependencies have now been installed."
+      echo "Check ${LOG_FILE}, if you want to know anything more about the operation of the script."
+    else
+      echo
+      echo "Some core dependencies are missing, but they will not be installed as a dry-run was requested."
+    fi
   else 
     echo 
     echo "All dependencies are installed."
@@ -79,16 +120,29 @@ function check_dependencies()
   fi
 }
 
-function check_distribution()
-{
-  distribution=$( grep -i "version_id" /etc/os-release | awk -F'=' '{print $2}' | tr -d '"')
-  if [ $distribution != "17.10" ]
-  then 
-    echo "This script only supports Ubuntu 17.10"
-    echo "Exiting."
-    exit -1
-  fi 
-}
+while getopts ':hsd' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    s) 
+      show_supported_distributions
+      exit
+       ;;
+    d)
+      DRY_RUN="--dry-run"
+      ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
 
 if [ $( whoami ) != "root" ]
 then
