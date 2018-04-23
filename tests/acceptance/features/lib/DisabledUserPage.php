240142eb4217675007422d33f6558f1298bef940
Paurakh Sharma Humagain <paurakh011@gmail.com>
2018-04-23 12:08:32 +0545

    UI test to disable user

---
+++ tests/acceptance/features/lib/DisabledUserPage.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * ownCloud
+ *
+ * @author Paurakh Sharma Humagain <paurakh@jankaritech.com>
+ * @copyright Copyright (c) 2018 Paurakh Sharma Humagain paurakh@jankaritech.com
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License,
+ * as published by the Free Software Foundation;
+ * either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace Page;
+
+use Behat\Mink\Session;
+
+/**
+ * Disabled page.
+ */
+class DisabledUserPage extends OwncloudPage {
+	
+	/**
+	 * @var string $path
+	 */
+	protected $path = '/index.php/login';
+	protected $userDisabledXpath = ".//li[@class='error']";
+	
+	/**
+	 * 
+	 * @param Session $session
+	 * @param int $timeout_msec
+	 *
+	 * @return void
+	 * @throws \Exception
+	 */
+	public function waitTillPageIsLoaded(
+		Session $session,
+		$timeout_msec = STANDARDUIWAITTIMEOUTMILLISEC
+	) {
+			$currentTime = \microtime(true);
+			$end = $currentTime + ($timeout_msec / 1000);
+		while ($currentTime <= $end) {
+			if ($this->findAll("xpath", $this->userDisabledXpath)) {
+				break;
+			}
+					\usleep(STANDARDSLEEPTIMEMICROSEC);
+					$currentTime = \microtime(true);
+		}
+			
+		if ($currentTime > $end) {
+				throw new \Exception(
+					__METHOD__ . " timeout waiting for page to load"
+				);
+		}
+			
+			$this->waitForOutstandingAjaxCalls($session);
+	}
+}
+	
