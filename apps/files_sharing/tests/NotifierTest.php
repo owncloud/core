765c0c9c40b3978527c867c538f5a5de662d6b48
Juan Pablo Villafáñez <jvillafanez@solidgear.es>
2018-04-27 15:39:22 +0200

    use the displayname in the notifications
    Fix retarget problem after accept / decline the share
    
    Fix deduplication logic and add tests for it
    
    Fix icon url for non-root ownCloud servers

--- apps/files_sharing/tests/NotifierTest.php
+++ apps/files_sharing/tests/NotifierTest.php
@@ -29,6 +29,9 @@ use OCA\Files_Sharing\Notifier;
 use OCP\Notification\INotification;
 use OCP\Notification\IAction;
 use OCP\IGroupManager;
+use OCP\IUserManager;
+use OCP\IConfig;
+use OCP\IUser;
 
 class NotifierTest extends \Test\TestCase {
 
@@ -46,6 +49,12 @@ class NotifierTest extends \Test\TestCase {
 	/** @var IGroupManager */
 	private $groupManager;
 
+	/** @var IUserManager */
+	private $userManager;
+
+	/** @var IConfig */
+	private $config;
+
 	public function setUp() {
 		parent::setUp();
 
@@ -60,11 +69,21 @@ class NotifierTest extends \Test\TestCase {
 			->disableOriginalConstructor()
 			->getMock();
 
+		$this->userManager = $this->getMockBuilder(IUserManager::class)
+			->disableOriginalConstructor()
+			->getMock();
+
+		$this->config = $this->getMockBuilder(IConfig::class)
+			->disableOriginalConstructor()
+			->getMock();
+
 		$this->notifier = new Notifier(
 			\OC::$server->getL10NFactory(),
 			$this->notificationManager,
 			$this->shareManager,
-			$this->groupManager
+			$this->groupManager,
+			$this->userManager,
+			$this->config
 		);
 	}
 
@@ -93,19 +112,19 @@ class NotifierTest extends \Test\TestCase {
 			[
 				'local_share',
 				['owner', 'owner', 'folder'],
-				'User owner shared "folder" with you',
+				'User "owner" shared "folder" with you',
 			],
 			// with behalf
 			[
 				'local_share',
 				['owner', 'alf', 'folder'],
-				'User owner shared "folder" with you (on behalf of alf)',
+				'User "owner" shared "folder" with you (on behalf of "alf")',
 			],
 			// auto-accepted share
 			[
 				'local_share_accepted',
 				['owner', 'behalf_user', 'folder'],
-				'User owner shared "folder" with you (on behalf of behalf_user)',
+				'User "owner" shared "folder" with you (on behalf of "behalf_user")',
 			],
 		];
 	}
@@ -190,7 +209,7 @@ class NotifierTest extends \Test\TestCase {
 
 		$notification->expects($this->once())
 			->method('setParsedSubject')
-			->with('User owner shared "folder" with you');
+			->with('User "owner" shared "folder" with you');
 
 		$notification->expects($this->any())
 			->method('addParsedAction')
@@ -203,6 +222,125 @@ class NotifierTest extends \Test\TestCase {
 
 		$this->notificationManager->expects($this->never())
 			->method('markProcessed');
+
+		$notification = $this->notifier->prepare($notification, 'en_US');
+	}
+
+	public function providesPrepareWithConfig() {
+		return [
+			// without behalf
+			[
+				'local_share',
+				['owner', 'owner', 'folder'],
+				['displayname owner', 'displayname owner'],
+				['email@owner', 'email@owner'],
+				'id',
+				'User "displayname owner (owner)" shared "folder" with you',
+			],
+			[
+				'local_share',
+				['owner', 'owner', 'folder'],
+				['displayname owner', 'displayname owner'],
+				['email@owner', 'email@owner'],
+				'email',
+				'User "displayname owner (email@owner)" shared "folder" with you',
+			],
+			[
+				'local_share',
+				['owner', 'owner', 'folder'],
+				['displayname owner', 'displayname owner'],
+				['email@owner', 'email@owner'],
+				'',
+				'User "displayname owner" shared "folder" with you',
+			],
+			// with behalf
+			[
+				'local_share',
+				['owner', 'alf', 'folder'],
+				['displayname owner', 'da1f'],
+				['email@owner', 'a1f@ex.com'],
+				'id',
+				'User "displayname owner (owner)" shared "folder" with you (on behalf of "da1f (alf)")',
+			],
+			[
+				'local_share',
+				['owner', 'alf', 'folder'],
+				['displayname owner', 'da1f'],
+				['email@owner', 'a1f@ex.com'],
+				'email',
+				'User "displayname owner (email@owner)" shared "folder" with you (on behalf of "da1f (a1f@ex.com)")',
+			],
+			[
+				'local_share',
+				['owner', 'alf', 'folder'],
+				['displayname owner', 'da1f'],
+				['email@owner', 'a1f@ex.com'],
+				'',
+				'User "displayname owner" shared "folder" with you (on behalf of "da1f")',
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider providesPrepareWithConfig
+	 */
+	public function testPrepareWithCustomConfig($subject, $subjectParams, $displaynames, $emails, $extraField, $expectedSubject) {
+		$user1 = $this->createMock(IUser::class);
+		$user1->method('getDisplayName')->willReturn($displaynames[0]);
+		$user1->method('getEMailAddress')->willReturn($emails[0]);
+
+		$user2 = $this->createMock(IUser::class);
+		$user2->method('getDisplayName')->willReturn($displaynames[1]);
+		$user2->method('getEMailAddress')->willReturn($emails[1]);
+
+		$this->userManager->method('get')
+			->will($this->returnValueMap([
+				[$subjectParams[0], $user1],
+				[$subjectParams[1], $user2],
+		]));
+
+		$this->config->method('getAppValue')
+			->will($this->returnValueMap([
+				['core', 'user_additional_info_field', '', $extraField]
+		]));
+
+		$share = $this->createMock(IShare::class);
+		$share->method('getShareType')->willReturn(\OCP\Share::SHARE_TYPE_USER);
+
+		$this->shareManager->method('getShareById')->willReturn($share);
+
+		$notification = $this->createMock(INotification::class);
+
+		$notification->method('getApp')->willReturn('files_sharing');
+		$notification->method('getSubject')->willReturn($subject);
+		$notification->method('getSubjectParameters')->willReturn($subjectParams);
+
+		$action1 = $this->createMock(IAction::class);
+		$action1->method('getLabel')->willReturn('accept');
+		$action1->expects($this->once())
+			->method('setParsedLabel')
+			->with('Accept')
+			->will($this->returnSelf());
+
+		$action2 = $this->createMock(IAction::class);
+		$action2->method('getLabel')->willReturn('decline');
+		$action2->expects($this->once())
+			->method('setParsedLabel')
+			->with('Decline')
+			->will($this->returnSelf());
+		$action2->expects($this->never())
+			->method('setPrimary');
+
+		$actions = [$action1, $action2];
+		$notification->method('getActions')->willReturn($actions);
+
+		$notification->expects($this->once())
+			->method('setParsedSubject')
+			->with($expectedSubject);
+
+		$notification->expects($this->any())
+			->method('addParsedAction')
+			->withConsecutive([$action1], [$action2]);
 
 		$notification = $this->notifier->prepare($notification, 'en_US');
 	}
