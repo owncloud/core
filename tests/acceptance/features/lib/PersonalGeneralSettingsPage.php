fff062da77450deb823e714de43489827e2180d2
Artur Neumann <artur@jankaritech.com>
2018-05-01 16:59:55 +0545

    run UI tests in drone on Firefox

--- tests/acceptance/features/lib/PersonalGeneralSettingsPage.php
+++ tests/acceptance/features/lib/PersonalGeneralSettingsPage.php
@@ -130,7 +130,14 @@ class PersonalGeneralSettingsPage extends OwncloudPage {
 	 * @return void
 	 */
 	public function changeEmailAddress($newEmailAddress, Session $session) {
-		$this->fillField($this->emailAddressInputID, $newEmailAddress);
+		$emailField = $this->findById($this->emailAddressInputID);
+		if (\is_null($emailField)) {
+			throw new ElementNotFoundException(
+				__METHOD__ .
+				" could not find email field with id $this->emailAddressInputID"
+			);
+		}
+		$this->cleanInputAndSetValue($emailField, $newEmailAddress);
 		$changeEmailButton = $this->findById($this->changeEmailButtonID);
 		$this->assertElementNotNull(
 			$changeEmailButton,
