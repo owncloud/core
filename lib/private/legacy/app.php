f6f40e1a5cfc2b38969fe074fc94a74dce3ab717
Tom Needham <tom@owncloud.com>
2018-05-03 11:35:09 +0100

    Remove redundant session validation for dav requests

--- lib/private/legacy/app.php
+++ lib/private/legacy/app.php
@@ -121,27 +121,16 @@ class OC_App {
 		}
 
 		// prevent app.php from printing output
-		ob_start();
+		\ob_start();
 		foreach ($apps as $app) {
 			if ((is_null($types) or self::isType($app, $types)) && !in_array($app, self::$loadedApps)) {
 				self::loadApp($app);
 			}
 		}
-		ob_end_clean();
+		\ob_end_clean();
 
-		// once all authentication apps are loaded we can validate the session
-		if (is_null($types) || in_array('authentication', $types)) {
-			if (\OC::$server->getUserSession()) {
-				$request = \OC::$server->getRequest();
-				$session = \OC::$server->getUserSession();
-				$davUser = \OC::$server->getUserSession()->getSession()->get(\OCA\DAV\Connector\Sabre\Auth::DAV_AUTHENTICATED);
-				if (is_null($davUser)) {
-					$session->validateSession();
-				}
-			}
-		}
-		if (is_array($types)) {
-			self::$loadedTypes = array_merge(self::$loadedTypes, $types);
+		if (\is_array($types)) {
+			self::$loadedTypes = \array_merge(self::$loadedTypes, $types);
 		}
 
 		\OC_Hook::emit('OC_App', 'loadedApps');
