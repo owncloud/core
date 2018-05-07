ab35eb33aaa7fbe0142bfba165f74f4b7c0bd979
Tom Needham <tom@owncloud.com>
2018-05-07 11:37:09 +0100

    Hide iOS app recommendation if no app id is set

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -11,7 +11,9 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="referrer" content="never">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-		<meta name="apple-itunes-app" content="app-id=<?php p($theme->getiTunesAppId()); ?>">
+		<?php if($theme->getiTunesAppId() !== '') { ?>
+			<meta name="apple-itunes-app" content="app-id=<?php p($theme->getiTunesAppId()); ?>">
+		<?php } ?>
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">
 		<meta name="apple-mobile-web-app-title" content="<?php p((!empty($_['application']) && $_['appid']!='files')? $_['application']:'ownCloud'); ?>">
