92b08586b73f6433a5c49aa1f87165fc5af9e787
Thomas Müller <thomas.mueller@tmit.eu>
2018-06-15 14:41:20 +0200

    Add unit tests for getMountPoint

--- tests/lib/Files/MetaFilesTest.php
+++ tests/lib/Files/MetaFilesTest.php
@@ -31,6 +31,7 @@ use OC\Files\Node\File;
 use OC\Files\View;
 use OCA\Files_Versions\Hooks;
 use OCP\Files\Folder;
+use OCP\Files\Mount\IMountPoint;
 use OCP\IImage;
 use Test\TestCase;
 use Test\Traits\UserTrait;
@@ -73,31 +74,31 @@ class MetaFilesTest extends TestCase {
 
 		// work on node api
 		/** @var Folder $metaNodeOfFile */
-		$metaNodeOfFile = \OC::$server->getRootFolder()->get("meta");
+		$metaNodeOfFile = \OC::$server->getRootFolder()->get('meta');
 		$this->assertInstanceOf(MetaRootNode::class, $metaNodeOfFile);
 		$this->assertEquals([], $metaNodeOfFile->getDirectoryListing());
-		$this->assertEquals("/meta", $metaNodeOfFile->getPath());
-		$this->assertEquals("meta", $metaNodeOfFile->getName());
+		$this->assertEquals('/meta', $metaNodeOfFile->getPath());
+		$this->assertEquals('meta', $metaNodeOfFile->getName());
 
 		$metaNodeOfFile = \OC::$server->getRootFolder()->get("meta/{$info->getId()}");
 		$this->assertInstanceOf(MetaFileIdNode::class, $metaNodeOfFile);
 		$this->assertEquals("/meta/{$info->getId()}", $metaNodeOfFile->getPath());
-		$this->assertEquals("{$info->getId()}", $metaNodeOfFile->getName());
+		$this->assertEquals((string)($info->getId()), $metaNodeOfFile->getName());
 		$children = $metaNodeOfFile->getDirectoryListing();
-		$this->assertEquals(1, count($children));
+		$this->assertCount(1, $children);
 		$this->assertInstanceOf(MetaVersionCollection::class, $children[0]);
 
 		$metaNodeOfFile = \OC::$server->getRootFolder()->get("meta/{$info->getId()}/v");
 		$this->assertInstanceOf(MetaVersionCollection::class, $metaNodeOfFile);
 		$this->assertEquals("/meta/{$info->getId()}/v", $metaNodeOfFile->getPath());
-		$this->assertEquals("v", $metaNodeOfFile->getName());
+		$this->assertEquals('v', $metaNodeOfFile->getName());
 		$children = $metaNodeOfFile->getDirectoryListing();
-		$this->assertEquals(0, count($children));
+		$this->assertCount(0, $children);
 
 		// write again to get another version
 		$view->file_put_contents($fileName, '1234567890');
 		$children = $metaNodeOfFile->getDirectoryListing();
-		$this->assertEquals(1, count($children));
+		$this->assertCount(1, $children);
 		$this->assertInstanceOf(MetaFileVersionNode::class, $children[0]);
 
 		$versionId = $children[0]->getName();
@@ -111,6 +112,7 @@ class MetaFilesTest extends TestCase {
 		$this->assertEquals('text/plain', $metaNodeOfFile->getMimetype());
 		$this->assertInternalType('string', $metaNodeOfFile->getEtag());
 		$this->assertTrue(strlen($metaNodeOfFile->getEtag()) > 0);
+		$this->assertInstanceOf(IMountPoint::class, $metaNodeOfFile->getMountPoint());
 		$thumbnail = $metaNodeOfFile->getThumbnail([]);
 		$this->assertInstanceOf(IImage::class, $thumbnail);
 
