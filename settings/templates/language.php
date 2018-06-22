91c0a2148450d410540b72de6b8fa368591b3ec8
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2018-06-23 00:52:51 +0300

    Extract template for selector

---
+++ settings/templates/language.php
@@ -0,0 +1,16 @@
+<select id="<?php p($_['selectId'])?>" name="lang" data-placeholder="<?php p($l->t('Language'));?>">
+	<option value="<?php p($_['activelanguage']['code']);?>">
+		<?php p($_['activelanguage']['name']);?>
+	</option>
+	<?php foreach ($_['commonlanguages'] as $language):?>
+		<option value="<?php p($language['code']);?>">
+			<?php p($language['name']);?>
+		</option>
+	<?php endforeach;?>
+	<optgroup label="––––––––––"></optgroup>
+	<?php foreach ($_['languages'] as $language):?>
+		<option value="<?php p($language['code']);?>">
+			<?php p($language['name']);?>
+		</option>
+	<?php endforeach;?>
+</select>
