9f9ea5d47f802cbceda757dfe20c86d2d6598ed6
Patrick Jahns <github@patrickjahns.de>
2018-06-25 00:47:12 +0200

    test starting docker containers in travis for acceptance tests

---
+++ tests/travis/start_docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+docker pull owncloudci/php:7.1
+docker run --rm \
+    -v ${TRAVIS_BUILD_DIR}:/var/www/owncloud \
+    -e APACHE_WEBROOT=/var/www/owncloud \
+    -p ${SRV_HOST_PORT}:80 \
+    owncloudci/php:7.1 \
+    bash -c "chown -r www-data. /var/www/owncloud && /usr/local/bin/apachectl -e debug -D "FOREGROUND"
+
+docker run --rm \
+    -v ${TRAVIS_BUILD_DIR}:/var/www/owncloud \
+    -e APACHE_WEBROOT=/var/www/owncloud \
+    -p ${REMOTE_FED_SRV_HOST_PORT}:80 \
+    owncloudci/php:7.1 \
+    bash -c "chown -r www-data. /var/www/owncloud && /usr/local/bin/apachectl -e debug -D "FOREGROUND"
