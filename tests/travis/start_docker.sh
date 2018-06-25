1262786383f9bbf68c6600d2becccf6ec53c9dca
Patrick Jahns <github@patrickjahns.de>
2018-06-25 16:16:15 +0200

    detach from container

--- tests/travis/start_docker.sh
+++ tests/travis/start_docker.sh
@@ -5,6 +5,7 @@ docker run --rm \
     -v ${TRAVIS_BUILD_DIR}:/var/www/owncloud \
     -e APACHE_WEBROOT=/var/www/owncloud \
     -p ${SRV_HOST_PORT}:80 \
+    -d \
     owncloudci/php:7.1 \
     bash -c "chown -R www-data. /var/www/owncloud && /usr/local/bin/apachectl -e debug -D "FOREGROUND"
 
@@ -12,5 +13,6 @@ docker run --rm \
     -v ${TRAVIS_BUILD_DIR}:/var/www/owncloud \
     -e APACHE_WEBROOT=/var/www/owncloud \
     -p ${REMOTE_FED_SRV_HOST_PORT}:80 \
+    -d \
     owncloudci/php:7.1 \
     bash -c "chown -R www-data. /var/www/owncloud && /usr/local/bin/apachectl -e debug -D "FOREGROUND"
