9b172c18ec157acdca52134f16ec9533e5f5e792
Patrick Jahns <github@patrickjahns.de>
2018-06-25 11:31:50 +0200

    propper flag for chowning folders

--- tests/travis/start_docker.sh
+++ tests/travis/start_docker.sh
@@ -6,11 +6,11 @@ docker run --rm \
     -e APACHE_WEBROOT=/var/www/owncloud \
     -p ${SRV_HOST_PORT}:80 \
     owncloudci/php:7.1 \
-    bash -c "chown -r www-data. /var/www/owncloud && /usr/local/bin/apachectl -e debug -D "FOREGROUND"
+    bash -c "chown -R www-data. /var/www/owncloud && /usr/local/bin/apachectl -e debug -D "FOREGROUND"
 
 docker run --rm \
     -v ${TRAVIS_BUILD_DIR}:/var/www/owncloud \
     -e APACHE_WEBROOT=/var/www/owncloud \
     -p ${REMOTE_FED_SRV_HOST_PORT}:80 \
     owncloudci/php:7.1 \
-    bash -c "chown -r www-data. /var/www/owncloud && /usr/local/bin/apachectl -e debug -D "FOREGROUND"
+    bash -c "chown -R www-data. /var/www/owncloud && /usr/local/bin/apachectl -e debug -D "FOREGROUND"
