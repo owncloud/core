ad8d515f26d5067d75623b470b84b2d633426a4f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2018-06-30 02:01:10 +0200

    [tx-robot] updated from transifex

--- apps/federatedfilesharing/l10n/pt_BR.js
+++ apps/federatedfilesharing/l10n/pt_BR.js
@@ -10,8 +10,6 @@ OC.L10N.register(
     "Sharing %s failed, could not find %s, maybe the server is currently unreachable." : "O compartilhamento %s falhou, porque não foi possível encontrar %s, talvez o servidor esteja inacessível.",
     "\"%1$s\" shared \"%3$s\" with you (on behalf of \"%2$s\")" : "\"%1$s\" compartilhado \"%3$s\" com você (em nome de \"%2$s\")",
     "\"%1$s\" shared \"%3$s\" with you" : "\"%1$s\" compartilhado \"%3$s\" com você",
-    "\"%1$s\" invited you to view \"%3$s\" on %4$s (on behalf of \"%2$s\")" : "\"%1$s\" convidou você para ver \"%3$s\" em %4$s (em nome de\"%2$s\")",
-    "\"%1$s\" invited you to view \"%3$s\" on %4$s" : "\"%1$s\" convidou você para ver \"%3$s\" em %4$s",
     "Accept" : "Aceitar",
     "Decline" : "Rejeitar",
     "Share with me through my #ownCloud Federated Cloud ID, see %s" : "Compartilhe comigo através do meu #ownCloud Nuvem ID Federada, veja %s",
