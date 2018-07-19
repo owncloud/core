0b8edef161ef69b4cb983cdada3efbe229bf3a58
Martin <martin.mattel@diemattels.at>
2018-07-19 10:06:04 +0200

    files:scan : add number of items per second on printed result

--- apps/files/lib/Command/Scan.php
+++ apps/files/lib/Command/Scan.php
@@ -429,7 +429,7 @@ class Scan extends Base {
 		$output->writeln("");
 
 		$headers = [
-			'Folders', 'Files', 'Elapsed time'
+			'Folders', 'Files', 'Elapsed time', 'Items per second'
 		];
 
 		$this->showSummary($headers, null, $output);
@@ -444,11 +444,13 @@ class Scan extends Base {
 	 */
 	protected function showSummary($headers, $rows, OutputInterface $output) {
 		$niceDate = $this->formatExecTime();
+		$itemsPerSecond = $this->getItemsPerSecond();
 		if (!$rows) {
 			$rows = [
 				$this->foldersCounter,
 				$this->filesCounter,
 				$niceDate,
+				$itemsPerSecond
 			];
 		}
 		$table = new Table($output);
@@ -456,6 +458,22 @@ class Scan extends Base {
 			->setHeaders($headers)
 			->setRows([$rows]);
 		$table->render();
+	}
+
+	/**
+	 * Get items per second processed, no fractions
+	 *
+	 * @return string
+	 */
+	protected function getItemsPerSecond() {
+		$items = $this->foldersCounter + $this->filesCounter;
+		if ($this->execTime === 0) {
+			// catch div by 0
+			$itemsPerSecond = 0;
+		} else {
+			$itemsPerSecond = $items / $this->execTime;
+		}
+		return \sprintf("%.0f", $itemsPerSecond);
 	}
 
 	/**
