f3b537b5c0a7f9eb2d7c00ae2b1f691482fde851
Vincent Petry <pvince81@owncloud.com>
2018-07-27 17:21:24 +0200

    Reorder webpack entry order to match what we had on master
    Should fix select2 issues

--- build/entry.js
+++ build/entry.js
@@ -2,46 +2,51 @@ var $ = require('jquery');
 window.jQuery = $;
 window.$ = $;
 
+require('es6-promise/dist/es6-promise.auto');
+
+require('select2/select2.css');
+
+require("jquery-ui/themes/base/jquery-ui.css");
+
+require("jquery-migrate");
 require("jquery-ui");
 require("jquery-ui/custom");
-require("jquery-ui/themes/base/jquery-ui.css");
-require("jquery-migrate");
 
 var _ = require('underscore');
 window._ = _;
 
-var handlebars = require('handlebars/dist/handlebars');
-window.Handlebars = handlebars;
-
-var backbone = require('backbone');
-window.Backbone = backbone;
-
-require('select2');
-require('select2/select2.css');
-
 var moment = require('moment');
 window.moment = moment;
 
-var snap = require('snapjs/dist/latest/snap');
-window.Snap = snap.Snap;
-
-require('davclient.js/lib/client');
-window.dav = dav;
+var handlebars = require('handlebars/dist/handlebars');
+window.Handlebars = handlebars;
 
 var md5 = require('blueimp-md5');
 window.md5 = md5;
 
 require('bootstrap/js/tooltip');
+
+var backbone = require('backbone');
+window.Backbone = backbone;
+
+require('davclient.js/lib/client');
+window.dav = dav;
+
+var clipboard = require('clipboard');
+window.Clipboard = clipboard;
+
+var bowser = require('bowser');
+window.bowser = bowser;
+
+var snap = require('snapjs/dist/latest/snap');
+window.Snap = snap.Snap;
+
+require('select2');
+
 require('strengthify/jquery.strengthify');
 
 var jstz = require('jstimezonedetect');
 window.jstz = jstz;
 
 require('jcrop');
-var clipboard = require('clipboard');
-window.Clipboard = clipboard;
 
-require('es6-promise/dist/es6-promise.auto');
-
-var bowser = require('bowser');
-window.bowser = bowser;
