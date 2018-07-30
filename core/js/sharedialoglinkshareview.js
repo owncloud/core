d6d075f42364b44778b39b8a50227417bd95d384
Felix Heidecke <felix@heidecke.me>
2018-07-30 18:34:13 +0200

    Add hidden password field on share to fool the browser password-popup

--- core/js/sharedialoglinkshareview.js
+++ core/js/sharedialoglinkshareview.js
@@ -41,7 +41,8 @@
 			'{{/if}}' +
 			'<div id="linkPass-{{cid}}" class="public-link-modal--item linkPass">' +
 				'<label class="public-link-modal--label" for="linkPassText-{{cid}}">{{passwordLabel}}</label>' +
-				'<input class="public-link-modal--input linkPassText" id="linkPassText-{{cid}}" type="password" placeholder="{{passwordPlaceholder}}" />' +
+				'<input class="hidden-visually" type="password" name="fakepasswordremembered">' +
+				'<input class="public-link-modal--input linkPassText" id="linkPassText-{{cid}}" type="password" placeholder="{{passwordPlaceholder}}" autocomplete="new-password" />' +
 				'<span class="error-message hidden"></span>' +
 			'</div>' +
 			'<div class="expirationDateContainer"></div>' +
