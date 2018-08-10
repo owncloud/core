2608ce5a52f5e4d9caf65cde374a3b1503807741
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2018-08-10 02:01:39 +0200

    [tx-robot] updated from transifex

--- apps/updatenotification/l10n/eu.js
+++ apps/updatenotification/l10n/eu.js
@@ -1,6 +1,9 @@
 OC.L10N.register(
     "updatenotification",
     {
+    "Update notifications" : "Eguneraketa jakinarazpenak",
+    "{version} is available. Get more information on how to update." : "{version} eskuragarri dago. Lortu nola eguneratzeari buruzko informazio gehiago.",
+    "Updated channel" : "Eguneratutako kanala",
     "Updater" : "Eguneratzailea",
     "A new version is available: %s" : "Bertsio berri bat eskuragarri dago: %s",
     "Update channel:" : "Eguneraketa kanala:",
