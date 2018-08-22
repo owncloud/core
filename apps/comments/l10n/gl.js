695487004e838e8b97ce1f3f9557272eae23a720
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2018-08-22 02:01:42 +0200

    [tx-robot] updated from transifex

--- apps/comments/l10n/gl.js
+++ apps/comments/l10n/gl.js
@@ -1,8 +1,16 @@
 OC.L10N.register(
     "comments",
     {
+    "Type in a new comment..." : "Escribe un novo comentario...",
+    "Delete comment" : "Borrar comentario",
+    "Post" : "Comentar",
     "Cancel" : "Cancelar",
+    "Edit comment" : "Editar comentario",
+    "Comments" : "Comentarios",
+    "More comments..." : "Máis comentarios...",
     "Save" : "Gardar",
-    "Comment" : "Comentario"
+    "{count} unread comments" : "{count} comentarios sen ler",
+    "Comment" : "Comentario",
+    "You commented" : "Comentaches"
 },
 "nplurals=2; plural=(n != 1);");
