01abc3c7911606b290e46bb13516a158d8a4c382
Vincent Petry <pvince81@owncloud.com>
2018-08-28 22:05:03 +0200

    Remove old Share::shareItem()

--- apps/files_sharing/tests/DeleteOrphanedSharesJobTest.php
+++ apps/files_sharing/tests/DeleteOrphanedSharesJobTest.php
@@ -153,21 +153,4 @@ class DeleteOrphanedSharesJobTest extends \Test\TestCase {
 
 		$this->assertCount(0, $this->getShares(), 'Orphaned shares deleted');
 	}
-
-	public function testKeepNonFileShares() {
-		$this->loginAsUser($this->user1);
-
-		\OCP\Share::registerBackend('test', 'Test\Share\Backend');
-
-		$this->assertTrue(
-			\OCP\Share::shareItem('test', 'test.txt', \OCP\Share::SHARE_TYPE_USER, $this->user2, \OCP\Constants::PERMISSION_READ),
-			'Failed asserting that user 1 successfully shared something with user 2.'
-		);
-
-		$this->assertCount(1, $this->getShares());
-
-		$this->job->run([]);
-
-		$this->assertCount(1, $this->getShares(), 'Non-file shares kept');
-	}
 }
