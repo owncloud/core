4f12cd7cc4d7aef1e717582efcbebb4623f68945
Phil Davis <phil@jankaritech.com>
2018-09-12 15:56:59 +0545

    Refactor destinationHeaderValue computation

--- tests/acceptance/features/bootstrap/WebDav.php
+++ tests/acceptance/features/bootstrap/WebDav.php
@@ -277,6 +277,17 @@ trait WebDav {
 	}
 
 	/**
+	 * @param string $user
+	 * @param string $fileDestination
+	 *
+	 * @return string
+	 */
+	private function destinationHeaderValue($user, $fileDestination) {
+		$fullUrl = $this->getBaseUrl() . '/' . $this->getDavFilesPath($user);
+		return $fullUrl . '/' . \ltrim($fileDestination, '/');
+	}
+
+	/**
 	 * @Given /^user "([^"]*)" has moved (?:file|folder|entry) "([^"]*)" to "([^"]*)"$/
 	 *
 	 * @param string $user
@@ -288,8 +299,9 @@ trait WebDav {
 	public function userHasMovedFile(
 		$user, $fileSource, $fileDestination
 	) {
-		$fullUrl = $this->getBaseUrl() . '/' . $this->getDavFilesPath($user);
-		$headers['Destination'] = $fullUrl . $fileDestination;
+		$headers['Destination'] = $this->destinationHeaderValue(
+			$user, $fileDestination
+		);
 		$this->response = $this->makeDavRequest(
 			$user, "MOVE", $fileSource, $headers
 		);
@@ -310,8 +322,9 @@ trait WebDav {
 	public function userMovesFileUsingTheAPI(
 		$user, $fileSource, $fileDestination
 	) {
-		$fullUrl = $this->getBaseUrl() . '/' . $this->getDavFilesPath($user);
-		$headers['Destination'] = $fullUrl . $fileDestination;
+		$headers['Destination'] = $this->destinationHeaderValue(
+			$user, $fileDestination
+		);
 		$this->response = $this->makeDavRequest(
 			$user, "MOVE", $fileSource, $headers
 		);
@@ -349,8 +362,9 @@ trait WebDav {
 	public function userCopiesFileUsingTheAPI(
 		$user, $fileSource, $fileDestination
 	) {
-		$fullUrl = $this->getBaseUrl() . '/' . $this->getDavFilesPath($user);
-		$headers['Destination'] = $fullUrl . $fileDestination;
+		$headers['Destination'] = $this->destinationHeaderValue(
+			$user, $fileDestination
+		);
 		$this->response = $this->makeDavRequest(
 			$user, "COPY", $fileSource, $headers
 		);
@@ -1880,16 +1894,16 @@ trait WebDav {
 	 *
 	 * @param string $user user
 	 * @param string $id upload id
-	 * @param string $dest destination path
+	 * @param string $destination destination path
 	 * @param array $headers extra headers
 	 *
 	 * @return void
 	 */
-	private function moveNewDavChunkToFinalFile($user, $id, $dest, $headers) {
+	private function moveNewDavChunkToFinalFile($user, $id, $destination, $headers) {
 		$source = "/uploads/$user/$id/.file";
-		$destination = $this->getBaseUrl() . '/' . $this->getDavFilesPath($user) . $dest;
-
-		$headers['Destination'] = $destination;
+		$headers['Destination'] = $this->destinationHeaderValue(
+			$user, $destination
+		);
 
 		$this->response = $this->makeDavRequest(
 			$user, 'MOVE', $source, $headers, null, "uploads"
