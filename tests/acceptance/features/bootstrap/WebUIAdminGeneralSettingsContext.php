faf02441a634ae76e7f24422685fb640ae9a3861
paurakhsharma <paurakh011@gmail.com>
2018-10-01 17:06:46 +0545

    WebUI test admin sets email server settings

---
+++ tests/acceptance/features/bootstrap/WebUIAdminGeneralSettingsContext.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * ownCloud
+ *
+ * @author Paurakh Sharma Humagain <paurakh@jankaritech.com>
+ * @copyright Copyright (c) 2018 Paurakh Sharma Humagain paurakh@jankaritech.com
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License,
+ * as published by the Free Software Foundation;
+ * either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\RawMinkContext;
+use Page\AdminGeneralSettingsPage;
+
+require_once 'bootstrap.php';
+
+/**
+//  * WebUI AdminGeneralSettings context.
+ */
+class WebUIAdminGeneralSettingsContext extends RawMinkContext implements Context {
+	private $adminGeneralSettingsPage;
+	
+	/**
+	 *
+	 * @var WebUIGeneralContext
+	 */
+	private $webUIGeneralContext;
+	
+	/**
+	 * WebUIAdminAdminSettingsContext constructor.
+	 *
+	 * @param AdminGeneralSettingsPage $adminGeneralSettingsPage
+	 */
+	public function __construct(
+		AdminGeneralSettingsPage $adminGeneralSettingsPage
+	) {
+		$this->adminGeneralSettingsPage = $adminGeneralSettingsPage;
+	}
+
+	/**
+	 * @Given the administrator has browsed to the admin general settings page
+	 * @When the administrator browses to the admin general settings page
+	 *
+	 * @return void
+	 */
+	public function theAdministratorHasBrowsedToTheAdminGeneralSettingsPage() {
+		$this->webUIGeneralContext->adminLogsInUsingTheWebUI();
+		$this->adminGeneralSettingsPage->open();
+		$this->adminGeneralSettingsPage->waitForAjaxCallsToStartAndFinish(
+			$this->getSession()
+		);
+	}
+
+	/**
+	 * @When the administrator sets the following settings in email server setting using the webUI
+	 *
+	 * @param TableNode $emailSettingsTable table of email server settings headings: must be: | setting | and | value |
+	 *
+	 * @return void
+	 */
+	public function administratorSetsTheFollowingSettingsInEmailServerSettingUsingTheWebui(TableNode $emailSettingsTable) {
+		$this->adminGeneralSettingsPage->setEmailServerSettings($emailSettingsTable);
+		
+	}
+
+	/**
+	 * @When the administrator clicks on send test email in the admin general settings page using the webUI
+	 *
+	 * @return void
+	 */
+	public function theAdministratorClicksOnSendTestEmailInTheAdminGeneralSettingsPageUsingTheWebui() {
+		$this->adminGeneralSettingsPage->sendTestEmail($this->getSession());
+	}
+
+	/**
+	 * This will run before EVERY scenario.
+	 * It will set the properties for this object.
+	 *
+	 * @BeforeScenario @webUI
+	 *
+	 * @param BeforeScenarioScope $scope
+	 *
+	 * @return void
+	 */
+	public function before(BeforeScenarioScope $scope) {
+		// Get the environment
+		$environment = $scope->getEnvironment();
+		// Get all the contexts you need in this context
+		$this->webUIGeneralContext = $environment->getContext('WebUIGeneralContext');
+	}
+}
---
+++ tests/acceptance/features/lib/AdminGeneralSettingsPage.php
@@ -0,0 +1,152 @@
+<?php
+
+/**
+ * ownCloud
+ *
+ * @author Paurakh Sharma Humagain <paurakh@jankaritech.com>
+ * @copyright Copyright (c) 2018 Paurakh Sharma Humagain paurakh@jankaritech.com
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License,
+ * as published by the Free Software Foundation;
+ * either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+namespace Page;
+
+use Behat\Gherkin\Node\TableNode;
+use SensioLabs\Behat\PageObjectExtension\PageObject\Exception\ElementNotFoundException;
+use WebDriver\Session;
+
+/**
+ * Admin General Settings page.
+ */
+class AdminGeneralSettingsPage extends OwncloudPage {
+
+	/**
+	 *
+	 * @var string $path
+	 */
+	protected $path = '/index.php/settings/admin?sectionid=general';
+
+	protected $sendModeTypeId = 'mail_smtpmode';
+	protected $encryptionTypeId = 'mail_smtpsecure';
+	protected $mailFromAddressFieldId = 'mail_from_address';
+	protected $mailDomainFieldId = 'mail_domain';
+	protected $authMethodTypeId = 'mail_smtpauthtype';
+	protected $authRequiredCheckboxXpath = '//label[@for="mail_smtpauth"]';
+	protected $authRequiredCheckboxId = 'mail_smtpauth';
+	protected $serverAddressFieldId = 'mail_smtphost';
+	protected $serverPortFieldId = 'mail_smtpport';
+	protected $sendTestEmailBtnId = 'sendtestemail';
+
+	/**
+	 * set email server settings
+	 *
+	 * @param TableNode $emailSettingsTable
+	 *
+	 * @return void
+	 */
+	public function setEmailServerSettings($emailSettingsTable) {
+		foreach ($emailSettingsTable as $row) {
+			if ($row['setting'] === 'send mode') {
+
+				$this->selectFieldOption($this->sendModeTypeId, $row['value']);
+
+			} elseif ($row['setting'] === 'encryption') {
+
+				$this->selectFieldOption($this->encryptionTypeId, $row['value']);
+
+			} elseif ($row['setting'] === 'from address') {
+
+				$this->fillField($this->mailFromAddressFieldId, $row['value']);
+
+			} elseif ($row['setting'] === 'mail domain') {
+
+				$this->fillField($this->mailDomainFieldId, $row['value']);
+
+			} elseif ($row['setting'] === 'authentication method') {
+
+				$this->selectFieldOption($this->authMethodTypeId, $row['value']);
+
+			} elseif ($row['setting'] === 'authentication required') {
+
+				$this->checkRequiredAuthentication($row['value']);
+
+			} elseif ($row['setting'] === 'server address') {
+
+				$this->fillField($this->serverAddressFieldId, $row['value']);
+
+			} elseif ($row['setting'] === 'port') {
+
+				$this->fillField($this->serverPortFieldId, $row['value']);
+
+			}
+		}
+		$this->waitForAjaxCallsToStartAndFinish($this->getSession());
+	}
+
+	/**
+	 * send test email
+	 *
+	 * @param Session $session
+	 *
+	 * @return void
+	 */
+	public function sendTestEmail($session) {
+		$sendTestEmailBtn = $this->findById($this->sendTestEmailBtnId);
+		$sendTestEmailBtn->click();
+		$this->waitForAjaxCallsToStartAndFinish($session);
+	}
+
+	/**
+	 * enable/disable authentication
+	 *
+	 * @param string $requiredState ('true' | 'false')
+	 *
+	 * @return void
+	 * @throws \Exception
+	 */
+	public function checkRequiredAuthentication($requiredState) {
+		$checkbox = $this->find("xpath", $this->authRequiredCheckboxXpath);
+		$checkCheckbox = $this->findById($this->authRequiredCheckboxId);
+		
+		if ($checkbox === null) {
+			throw new ElementNotFoundException(
+				__METHOD__ .
+				" xpath $this->authRequiredCheckboxXpath " .
+				"could not find label for checkbox"
+			);
+		}
+		
+		if ($checkCheckbox === null) {
+			throw new ElementNotFoundException(
+				__METHOD__ .
+				" id $checkCheckbox " .
+				"could not find checkbox"
+			);
+		}
+		
+		if ($requiredState == 'true') {
+			if (!$checkCheckbox->isChecked()) {
+				$checkbox->click();
+			}
+		} elseif ($requiredState == 'false') {
+			if ($checkCheckbox->isChecked()) {
+				$checkbox->click();
+			}
+		} else {
+			throw new \Exception(
+				__METHOD__ . " invalid action: $action"
+			);
+		}
+	}
+}
