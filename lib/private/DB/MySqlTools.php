d8226f58098f34a7a17a3eeeedb10a22cd04f256
Vincent Petry <pvince81@owncloud.com>
2018-11-07 17:17:42 +0100

    Detect MariaDB > 10.3.1 and assume mb4 support

--- lib/private/DB/MySqlTools.php
+++ lib/private/DB/MySqlTools.php
@@ -22,17 +22,13 @@
 namespace OC\DB;
 
 use OCP\IDBConnection;
+use Doctrine\DBAL\DBALException;
 
 /**
 * Various MySQL specific helper functions.
 */
 class MySqlTools {
-
-	/**
-	 * @param Connection $connection
-	 * @return bool
-	 */
-	public function supports4ByteCharset(IDBConnection $connection) {
+	private function detectBarracuda(IDBConnection $connection) {
 		foreach (['innodb_file_format' => 'Barracuda', 'innodb_large_prefix' => 'ON', 'innodb_file_per_table' => 'ON'] as $var => $val) {
 			$result = $connection->executeQuery("SHOW VARIABLES LIKE '$var'");
 			$rows = $result->fetch();
@@ -44,6 +40,44 @@ class MySqlTools {
 				return false;
 			}
 		}
+
 		return true;
+	}
+
+	/**
+	 * Detect MariaDB server version, including hack for some mariadb distributions
+	 * that starts with the prefix '5.5.5-'
+	 *
+	 * @param string $versionString Version string as returned by mariadb server, i.e. '5.5.5-Mariadb-10.0.8-xenial'
+	 * @return string
+	 * @throws DBALException
+	 */
+	private function getMariaDbMysqlVersionNumber($versionString) {
+		if (!\preg_match('/^(?:5\.5\.5-)?(mariadb-)?(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)/i', $versionString, $versionParts)) {
+			throw DBALException::invalidPlatformVersionSpecified(
+				$versionString,
+				'^(?:5\.5\.5-)?(mariadb-)?<major_version>.<minor_version>.<patch_version>'
+			);
+		}
+		return $versionParts['major'] . '.' . $versionParts['minor'] . '.' . $versionParts['patch'];
+	}
+
+	/**
+	 * @param Connection $connection
+	 * @return bool
+	 */
+	public function supports4ByteCharset(IDBConnection $connection) {
+		if ($this->detectBarracuda($connection)) {
+			return true;
+		}
+
+		$version = $connection->getDatabaseVersionString();
+		$mariadb = \stripos($version, 'mariadb') !== false;
+		if ($mariadb && \version_compare($this->getMariaDbMysqlVersionNumber($version), '10.3.1', '>=')) {
+			// mb4 supported by default on MariaDB since 10.3.1 and innodb_file_format vars were removed
+			return true;
+		}
+
+		return false;
 	}
 }
