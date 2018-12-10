cf59e6db376404834414d072938cbbea9ca8cbc8
Artur Neumann <info@individual-it.net>
2018-12-10 10:16:13 +0545

    fix snakeoil tests

--- tests/acceptance/features/bootstrap/WebDav.php
+++ tests/acceptance/features/bootstrap/WebDav.php
@@ -1191,7 +1191,7 @@ trait WebDav {
 		$response = $client->request(
 			'HEAD', $this->makeSabrePath($user, $path)
 		);
-		if ($response['statusCode'] < 401 && $response['statusCode'] > 404) {
+		if ($response['statusCode'] < 401 || $response['statusCode'] > 404) {
 			throw new \Exception(
 				"$entry '$path' expected to not exist " .
 				"(status code {$response['statusCode']}, expected 401 - 404)"
