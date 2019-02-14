d35a23610a239a888d239278c16da6d5a52a7193
David Toledo <dtoledo@solidgear.es>
2019-02-14 09:20:52 +0100

    Happy new year for status.php Copyright

--- status.php
+++ status.php
@@ -13,7 +13,7 @@
  * @author Philipp Schaffrath <github@philippschaffrath.de>
  * @author Thomas Müller <thomas.mueller@tmit.eu>
  *
- * @copyright Copyright (c) 2018, ownCloud GmbH
+ * @copyright Copyright (c) 2019, ownCloud GmbH
  * @license AGPL-3.0
  *
  * This code is free software: you can redistribute it and/or modify
