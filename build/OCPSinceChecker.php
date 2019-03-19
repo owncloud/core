2fb67031bcfce06366c4bb49e6825659b8dd45b2
Phil Davis <phil@jankaritech.com>
2019-03-19 18:37:58 +0545

    Apply code style to build dir in master

--- build/OCPSinceChecker.php
+++ build/OCPSinceChecker.php
@@ -19,7 +19,6 @@
  *
  */
 
-
 require_once __DIR__ . '/../lib/composer/autoload.php';
 
 /**
@@ -28,7 +27,6 @@ require_once __DIR__ . '/../lib/composer/autoload.php';
  * this class checks all methods for the presence of the @since tag
  */
 class SinceTagCheckVisitor extends \PhpParser\NodeVisitorAbstract {
-
 	const INVALID_VERSIONS = ['9.2'];
 
 	/** @var string */
@@ -42,62 +40,62 @@ class SinceTagCheckVisitor extends \PhpParser\NodeVisitorAbstract {
 	protected $errors = [];
 
 	public function enterNode(\PhpParser\Node $node) {
-		if($this->deprecatedClass) {
+		if ($this->deprecatedClass) {
 			return;
 		}
 
-		if($node instanceof \PhpParser\Node\Stmt\Namespace_) {
+		if ($node instanceof \PhpParser\Node\Stmt\Namespace_) {
 			$this->namespace = $node->name;
 		}
 
-		if($node instanceof \PhpParser\Node\Stmt\Interface_ or
+		if ($node instanceof \PhpParser\Node\Stmt\Interface_ or
 			$node instanceof \PhpParser\Node\Stmt\Class_) {
 			$this->className = $node->name;
 
 			/** @var \PhpParser\Comment\Doc[] $comments */
 			$comments = $node->getAttribute('comments');
 
-			if(\count($comments) === 0) {
+			if (\count($comments) === 0) {
 				$this->errors[] = 'PHPDoc is needed for ' . $this->namespace . '\\' . $this->className . '::' . $node->name;
 				return;
 			}
 
 			$comment = $comments[\count($comments) - 1];
 			$text = $comment->getText();
-			if(\strpos($text, '@deprecated') !== false) {
+			if (\strpos($text, '@deprecated') !== false) {
 				$this->deprecatedClass = true;
 			}
 
-			if($this->deprecatedClass === false && \strpos($text, '@since') === false && \strpos($text, '@deprecated') === false) {
+			if ($this->deprecatedClass === false && \strpos($text, '@since') === false && \strpos($text, '@deprecated') === false) {
 				$type = $node instanceof \PhpParser\Node\Stmt\Interface_ ? 'interface' : 'class';
 				$this->errors[] = '@since or @deprecated tag is needed in PHPDoc for ' . $type . ' ' . $this->namespace . '\\' . $this->className;
 				return;
 			}
 
-			if($this->deprecatedClass === false && ($ver = $this->checkInvalidVersions($text)) !== null) {
+			if ($this->deprecatedClass === false && ($ver = $this->checkInvalidVersions($text)) !== null) {
 				$type = $node instanceof \PhpParser\Node\Stmt\Interface_ ? 'interface' : 'class';
 				$this->errors[] = 'Specified version ' . $ver . ' does not exist for ' . $type . ' ' . $this->namespace . '\\' . $this->className;
 				return;
 			}
 		}
 
-		if($node instanceof \PhpParser\Node\Stmt\ClassMethod) {
+		if ($node instanceof \PhpParser\Node\Stmt\ClassMethod) {
 			/** @var \PhpParser\Node\Stmt\ClassMethod $node */
 			/** @var \PhpParser\Comment\Doc[] $comments */
 			$comments = $node->getAttribute('comments');
 
-			if(\count($comments) === 0) {
+			if (\count($comments) === 0) {
 				$this->errors[] = 'PHPDoc is needed for ' . $this->namespace . '\\' . $this->className . '::' . $node->name;
 				return;
 			}
 			$comment = $comments[\count($comments) - 1];
 			$text = $comment->getText();
-			if(\strpos($text, '@since') === false && \strpos($text, '@deprecated') === false) {
+			if (\strpos($text, '@since') === false && \strpos($text, '@deprecated') === false) {
 				$this->errors[] = '@since or @deprecated tag is needed in PHPDoc for ' . $this->namespace . '\\' . $this->className . '::' . $node->name;
 				return;
 			}
 
-			if(($ver = $this->checkInvalidVersions($text)) !== null) {
+			if (($ver = $this->checkInvalidVersions($text)) !== null) {
 				$this->errors[] = 'Specified version ' . $ver . ' does not exist for ' . $this->namespace . '\\' . $this->className;
 				return;
 			}
@@ -126,7 +124,6 @@ class SinceTagCheckVisitor extends \PhpParser\NodeVisitorAbstract {
 
 echo 'Parsing all files in lib/public for the presence of @since or @deprecated on each method...' . PHP_EOL . PHP_EOL;
 
-
 $parser = (new \PhpParser\ParserFactory())->create(\PhpParser\ParserFactory::PREFER_PHP7);
 
 /* iterate over all .php files in lib/public */
@@ -136,7 +133,7 @@ $Regex = new RegexIterator($Iterator, '/^.+\.php$/i', RecursiveRegexIterator::GE
 
 $errors = [];
 
-foreach($Regex as $file) {
+foreach ($Regex as $file) {
 	$stmts = $parser->parse(\file_get_contents($file[0]));
 
 	$visitor = new SinceTagCheckVisitor();
@@ -147,7 +144,7 @@ foreach($Regex as $file) {
 	$errors = \array_merge($errors, $visitor->getErrors());
 }
 
-if(\count($errors)) {
+if (\count($errors)) {
 	echo \join(PHP_EOL, $errors) . PHP_EOL . PHP_EOL;
 	exit(1);
 }
