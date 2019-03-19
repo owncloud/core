31708eda42289bcf9e43e8ad7b4ee86c105495e7
Phil Davis <phil@jankaritech.com>
2019-03-19 18:25:01 +0545

    Apply code style to build dir in stable10

--- build/license.php
+++ build/license.php
@@ -45,40 +45,39 @@ class Licenses {
  *
  */
 EOD;
-		$this->licenseText = str_replace('@YEAR@', date("Y"), $this->licenseText);
+		$this->licenseText = \str_replace('@YEAR@', \date("Y"), $this->licenseText);
 	}
 
 	/**
 	 * @param string|string[] $folder
 	 * @param string|bool $gitRoot
 	 */
-	function exec($folder, $gitRoot = false) {
-
-		if (is_array($folder)) {
-			foreach($folder as $f) {
+	public function exec($folder, $gitRoot = false) {
+		if (\is_array($folder)) {
+			foreach ($folder as $f) {
 				$this->exec($f, $gitRoot);
 			}
 			return;
 		}
 
-		if ($gitRoot !== false && substr($gitRoot, -1) !== '/') {
+		if ($gitRoot !== false && \substr($gitRoot, -1) !== '/') {
 			$gitRoot .= '/';
 		}
 
-		if (is_file($folder)) {
+		if (\is_file($folder)) {
 			$this->handleFile($folder, $gitRoot);
 			return;
 		}
 
-		$excludes = array_map(function($item) use ($folder) {
+		$excludes = \array_map(function ($item) use ($folder) {
 			return $folder . '/' . $item;
 		}, ['vendor', '3rdparty', '.git', 'l10n', 'templates']);
 
 		$iterator = new RecursiveDirectoryIterator($folder, RecursiveDirectoryIterator::SKIP_DOTS);
-		$iterator = new RecursiveCallbackFilterIterator($iterator, function($item) use ($folder, $excludes){
+		$iterator = new RecursiveCallbackFilterIterator($iterator, function ($item) use ($folder, $excludes) {
 			/** @var SplFileInfo $item */
-			foreach($excludes as $exclude) {
-				if (substr($item->getPath(), 0, strlen($exclude)) === $exclude) {
+			foreach ($excludes as $exclude) {
+				if (\substr($item->getPath(), 0, \strlen($exclude)) === $exclude) {
 					return false;
 				}
 			}
@@ -93,8 +92,8 @@ EOD;
 		}
 	}
 
-	function writeAuthorsFile() {
-		ksort($this->authors);
+	public function writeAuthorsFile() {
+		\ksort($this->authors);
 		$template = "ownCloud is written by:
 @AUTHORS@
 
@@ -104,25 +103,25 @@ With help from many libraries and frameworks including:
 	jQuery
 	…
 ";
-		$authors = implode(PHP_EOL, array_map(function($author){
+		$authors = \implode(PHP_EOL, \array_map(function ($author) {
 			return " - ".$author;
 		}, $this->authors));
-		$template = str_replace('@AUTHORS@', $authors, $template);
-		file_put_contents(__DIR__.'/../AUTHORS', $template);
+		$template = \str_replace('@AUTHORS@', $authors, $template);
+		\file_put_contents(__DIR__.'/../AUTHORS', $template);
 	}
 
-	function handleFile($path, $gitRoot) {
-		$source = file_get_contents($path);
+	public function handleFile($path, $gitRoot) {
+		$source = \file_get_contents($path);
 		if ($this->isMITLicensed($source)) {
 			echo "MIT licensed file: $path" . PHP_EOL;
 			return;
 		}
 		$source = $this->eatOldLicense($source);
 		$authors = $this->getAuthors($path, $gitRoot);
-		$license = str_replace('@AUTHORS@', $authors, $this->licenseText);
+		$license = \str_replace('@AUTHORS@', $authors, $this->licenseText);
 
 		$source = "<?php" . PHP_EOL . $license . PHP_EOL . $source;
-		file_put_contents($path,$source);
+		\file_put_contents($path, $source);
 		echo "License updated: $path" . PHP_EOL;
 	}
 
@@ -131,11 +130,11 @@ With help from many libraries and frameworks including:
 	 * @return bool
 	 */
 	private function isMITLicensed($source) {
-		$lines = explode(PHP_EOL, $source);
-		while(!empty($lines)) {
+		$lines = \explode(PHP_EOL, $source);
+		while (!empty($lines)) {
 			$line = $lines[0];
-			array_shift($lines);
-			if (strpos($line, 'The MIT License') !== false) {
+			\array_shift($lines);
+			if (\strpos($line, 'The MIT License') !== false) {
 				return true;
 			}
 		}
@@ -148,51 +147,51 @@ With help from many libraries and frameworks including:
 	 * @return string
 	 */
 	private function eatOldLicense($source) {
-		$lines = explode(PHP_EOL, $source);
-		while(!empty($lines)) {
+		$lines = \explode(PHP_EOL, $source);
+		while (!empty($lines)) {
 			$line = $lines[0];
-			if (strpos($line, '<?php') !== false) {
-				array_shift($lines);
+			if (\strpos($line, '<?php') !== false) {
+				\array_shift($lines);
 				continue;
 			}
-			if (strpos($line, '/**') !== false) {
-				array_shift($lines);
+			if (\strpos($line, '/**') !== false) {
+				\array_shift($lines);
 				continue;
 			}
-			if (strpos($line, '*/') !== false ) {
-				array_shift($lines);
+			if (\strpos($line, '*/') !== false) {
+				\array_shift($lines);
 				break;
 			}
-			if (strpos($line, '*') !== false) {
-				array_shift($lines);
+			if (\strpos($line, '*') !== false) {
+				\array_shift($lines);
 				continue;
 			}
-			if (trim($line) === '') {
-				array_shift($lines);
+			if (\trim($line) === '') {
+				\array_shift($lines);
 				continue;
 			}
 			break;
 		}
 
-		return implode(PHP_EOL, $lines);
+		return \implode(PHP_EOL, $lines);
 	}
 
 	private function getAuthors($file, $gitRoot) {
 		// only add authors that changed code and not the license header
-		$licenseHeaderEndsAtLine = trim(shell_exec("grep -n '*/' $file | head -n 1 | cut -d ':' -f 1"));
-		$buildDir = getcwd();
+		$licenseHeaderEndsAtLine = \trim(\shell_exec("grep -n '*/' $file | head -n 1 | cut -d ':' -f 1"));
+		$buildDir = \getcwd();
 		if ($gitRoot) {
-			chdir($gitRoot);
-			$file = substr($file, strlen($gitRoot));
+			\chdir($gitRoot);
+			$file = \substr($file, \strlen($gitRoot));
 		}
-		$out = shell_exec("git blame --line-porcelain -L $licenseHeaderEndsAtLine, $file | sed -n 's/^author //p;s/^author-mail //p' | sed 'N;s/\\n/ /' | sort -f | uniq");
+		$out = \shell_exec("git blame --line-porcelain -L $licenseHeaderEndsAtLine, $file | sed -n 's/^author //p;s/^author-mail //p' | sed 'N;s/\\n/ /' | sort -f | uniq");
 		if ($gitRoot) {
-			chdir($buildDir);
+			\chdir($buildDir);
 		}
-		$authors = explode(PHP_EOL, $out);
+		$authors = \explode(PHP_EOL, $out);
 
-		$authors = array_filter($authors, function($author) {
-			return !in_array($author, [
+		$authors = \array_filter($authors, function ($author) {
+			return !\in_array($author, [
 				'',
 				'Not Committed Yet <not.committed.yet>',
 				'Jenkins for ownCloud <owncloud-bot@tmit.eu>',
@@ -201,26 +200,26 @@ With help from many libraries and frameworks including:
 		});
 
 		if ($gitRoot) {
-			$authors = array_map([$this, 'checkCoreMailMap'], $authors);
-			$authors = array_unique($authors);
+			$authors = \array_map([$this, 'checkCoreMailMap'], $authors);
+			$authors = \array_unique($authors);
 		}
 
-		$authors = array_map(function($author){
+		$authors = \array_map(function ($author) {
 			$this->authors[$author] = $author;
 			return " * @author $author";
 		}, $authors);
-		return implode(PHP_EOL, $authors);
+		return \implode(PHP_EOL, $authors);
 	}
 
 	private function checkCoreMailMap($author) {
 		if (empty($this->mailMap)) {
-			$content = file_get_contents(__DIR__ . '/../.mailmap');
-			$entries = explode("\n", $content);
+			$content = \file_get_contents(__DIR__ . '/../.mailmap');
+			$entries = \explode("\n", $content);
 			foreach ($entries as $entry) {
-				if (strpos($entry, '> ') === false) {
+				if (\strpos($entry, '> ') === false) {
 					$this->mailMap[$entry] = $entry;
 				} else {
-					list($use, $actual) = explode('> ', $entry);
+					list($use, $actual) = \explode('> ', $entry);
 					$this->mailMap[$actual] = $use . '>';
 				}
 			}
@@ -237,7 +236,6 @@ $licenses = new Licenses;
 if (isset($argv[1])) {
 	$licenses->exec($argv[1], isset($argv[2]) ? $argv[1] : false);
 } else {
-
 	$licenses->exec([
 		__DIR__ . '/../apps/comments',
 		__DIR__ . '/../apps/dav',
