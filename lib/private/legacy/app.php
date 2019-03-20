ea15cf2dba2887ad7fe8d899e173b817e6f94d25
Vincent Petry <pvince81@owncloud.com>
2019-03-20 13:35:24 +0100

    Set proper value when upgrading app patch version

--- lib/private/legacy/app.php
+++ lib/private/legacy/app.php
@@ -914,7 +914,7 @@ class OC_App {
 				return true;
 			}
 			// update app version in db
-			\OC::$server->getConfig()->setAppValue($app, 'installed_version', $versions[$app]);
+			\OC::$server->getConfig()->setAppValue($app, 'installed_version', $info['version']);
 		}
 		return false;
 	}
