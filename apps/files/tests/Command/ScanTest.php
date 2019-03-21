9f050712cc0d15925911b29da37c62733e2daec7
Phil Davis <phil@jankaritech.com>
2019-03-21 14:37:22 +0545

    do not suppress mkdir() in setUp

--- apps/files/tests/Command/ScanTest.php
+++ apps/files/tests/Command/ScanTest.php
@@ -133,7 +133,7 @@ class ScanTest extends TestCase {
 
 		$this->dataDir = \OC::$server->getConfig()->getSystemValue('datadirectory', \OC::$SERVERROOT . '/data-autotest');
 
-		@\mkdir($this->dataDir . '/' . $this->scanUser1->getUID() . '/files/toscan', 0777, true);
+		\mkdir($this->dataDir . '/' . $this->scanUser1->getUID() . '/files/toscan', 0777, true);
 	}
 
 	protected function tearDown() {
