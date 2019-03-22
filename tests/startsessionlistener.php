2e5093561b5f168f87022f2c722e4c2f97178914
Phil Davis <phil@jankaritech.com>
2019-03-22 16:52:26 +0545

    Adjust startsessionlistener.php for PHPUnit7 method signatures

--- tests/startsessionlistener.php
+++ tests/startsessionlistener.php
@@ -10,39 +10,39 @@
  * Starts a new session before each test execution
  */
 class StartSessionListener implements PHPUnit\Framework\TestListener {
-	public function addError(PHPUnit\Framework\Test $test, Exception $e, $time) {
+	public function addError(PHPUnit\Framework\Test $test, \Throwable $t, float $time): void {
 	}
 
-	public function addFailure(PHPUnit\Framework\Test $test, PHPUnit\Framework\AssertionFailedError $e, $time) {
+	public function addWarning(\PHPUnit\Framework\Test $test, \PHPUnit\Framework\Warning $e, float $time): void {
 	}
 
-	public function addIncompleteTest(PHPUnit\Framework\Test $test, Exception $e, $time) {
+	public function addFailure(PHPUnit\Framework\Test $test, PHPUnit\Framework\AssertionFailedError $e, float $time): void {
 	}
 
-	public function addRiskyTest(PHPUnit\Framework\Test $test, Exception $e, $time) {
+	public function addIncompleteTest(PHPUnit\Framework\Test $test, \Throwable $t, float $time): void {
 	}
 
-	public function addSkippedTest(PHPUnit\Framework\Test $test, Exception $e, $time) {
+	public function addRiskyTest(PHPUnit\Framework\Test $test, \Throwable $t, float $time): void {
 	}
 
-	public function startTest(PHPUnit\Framework\Test $test) {
+	public function addSkippedTest(PHPUnit\Framework\Test $test, \Throwable $t, float $time): void {
 	}
 
-	public function endTest(PHPUnit\Framework\Test $test, $time) {
+	public function startTestSuite(PHPUnit\Framework\TestSuite $suite): void {
+	}
+
+	public function endTestSuite(PHPUnit\Framework\TestSuite $suite): void {
+	}
+
+	public function startTest(PHPUnit\Framework\Test $test): void {
+	}
+
+	public function endTest(PHPUnit\Framework\Test $test, float $time): void {
 		// reopen the session - only allowed for memory session
 		if (\OC::$server->getSession() instanceof \OC\Session\Memory) {
 			/** @var $session \OC\Session\Memory */
 			$session = \OC::$server->getSession();
 			$session->reopen();
 		}
-	}
-
-	public function startTestSuite(PHPUnit\Framework\TestSuite $suite) {
-	}
-
-	public function endTestSuite(PHPUnit\Framework\TestSuite $suite) {
-	}
-
-	public function addWarning(\PHPUnit\Framework\Test $test, \PHPUnit\Framework\Warning $e, $time) {
 	}
 }
