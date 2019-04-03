7683befca03db68eaa85b4d66e7a5b9a7dba3272
karakayasemi <karakayasemi@itu.edu.tr>
2019-04-03 18:51:03 +0300

    move expiration date html elements under expiration date checkbox

--- tests/lib/Share20/ManagerTest.php
+++ tests/lib/Share20/ManagerTest.php
@@ -940,7 +940,7 @@ class ManagerTest extends \Test\TestCase {
 	}
 
 	/**
-	 * @expectedException  \InvalidArgumentException
+	 * @expectedException \InvalidArgumentException
 	 */
 	public function testvalidateExpirationDateEnforceButNotSetNewShare() {
 		$share = $this->manager->newShare();
