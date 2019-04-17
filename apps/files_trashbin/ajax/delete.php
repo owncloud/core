4e1f56768a6263ad3f708755329c55b304edc964
Phil Davis <phil@jankaritech.com>
2019-04-17 17:39:49 +0545

    Use consistent app name in writeLog() calls

--- apps/files_trashbin/ajax/delete.php
+++ apps/files_trashbin/ajax/delete.php
@@ -65,7 +65,7 @@ foreach ($list as $file) {
 	OCA\Files_Trashbin\Trashbin::delete($filename, \OCP\User::getUser(), $timestamp);
 	if (OCA\Files_Trashbin\Trashbin::file_exists($filename, $timestamp)) {
 		$error[] = $filename;
-		\OCP\Util::writeLog('trashbin', 'can\'t delete ' . $filename . ' permanently.', \OCP\Util::ERROR);
+		\OCP\Util::writeLog('files_trashbin', 'can\'t delete ' . $filename . ' permanently.', \OCP\Util::ERROR);
 	}
 	// only list deleted files if not deleting everything
 	elseif (!$deleteAll) {
