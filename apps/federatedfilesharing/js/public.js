8d3b8dd733e26ee3e1ffd11d5c409f6106f4b0cb
Phil Davis <phil@jankaritech.com>
2019-04-17 15:36:59 +0545

    Fixup federatedfilesharing translation calls

--- apps/federatedfilesharing/js/public.js
+++ apps/federatedfilesharing/js/public.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#header #details').prepend(
 		'<span id="save">' +
-		'	<button id="save-button">'+t('files_sharing', 'Add to your ownCloud')+'</button>' +
+		'	<button id="save-button">'+t('federatedfilesharing', 'Add to your ownCloud')+'</button>' +
 		'	<form class="save-form hidden" action="#">' +
 		'		<input type="text" id="remote_address" placeholder="example.com/owncloud"/>' +
 		'		<button id="save-button-confirm" class="icon-confirm svg" disabled></button>' +
