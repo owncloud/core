50b3d114427d60451a8538ff0c2009f7eafe73d6
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2019-05-03 13:23:03 +0200

    Fix broken migration due to change in the SyncService's constructor

--- core/Migrations/Version20170221114437.php
+++ core/Migrations/Version20170221114437.php
@@ -1,8 +1,6 @@
 <?php
 namespace OC\Migrations;
 
-use OC\User\AccountMapper;
-use OC\User\AccountTermMapper;
 use OC\User\Database;
 use OC\User\Sync\AllUsersIterator;
 use OC\User\SyncService;
@@ -10,22 +8,24 @@ use OCP\Migration\ISimpleMigration;
 use OCP\Migration\IOutput;
 
 class Version20170221114437 implements ISimpleMigration {
+	/** @var SyncService */
+	private $syncService;
+	/** @var Database */
+	private $dbBackend;
+
+	public function __construct(SyncService $syncService, Database $dbBackend) {
+		$this->syncService = $syncService;
+		$this->dbBackend = $dbBackend;
+	}
 
 	/**
 	 * @param IOutput $out
 	 */
 	public function run(IOutput $out) {
-		$backend = new Database();
-		$config = \OC::$server->getConfig();
-		$logger = \OC::$server->getLogger();
-		$connection = \OC::$server->getDatabaseConnection();
-		$accountMapper = new AccountMapper($config, $connection, new AccountTermMapper($connection));
-		$syncService = new SyncService($config, $logger, $accountMapper);
-
 		// insert/update known users
 		$out->info("Insert new users ...");
-		$out->startProgress($backend->countUsers());
-		$syncService->run($backend, new AllUsersIterator($backend), function () use ($out) {
+		$out->startProgress($this->dbBackend->countUsers());
+		$this->syncService->run($this->dbBackend, new AllUsersIterator($this->dbBackend), function () use ($out) {
 			$out->advance();
 		});
 		$out->finishProgress();
