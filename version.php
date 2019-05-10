1de4ca6cf323d114e49f76fca000f9f133332715
Patrick Jahns <github@patrickjahns.de>
2019-05-10 20:31:10 +0200

    Set version to 10.2.0RC3

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
-$OC_Version = [10, 2, 0, 2];
+$OC_Version = [10, 2, 0, 3];
 
 // The human readable string
-$OC_VersionString = '10.2.0 RC2';
+$OC_VersionString = '10.2.0 RC3';
 
 $OC_VersionCanBeUpgradedFrom = [[8, 2, 11],[9, 0, 9],[9, 1]];
 
