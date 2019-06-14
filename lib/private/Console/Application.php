b6fd756ae3ac72c5100cc03469c5b92a054c01ef
Sujith H <sharidasan@owncloud.com>
2019-06-14 22:20:12 +0530

    [stable10] Backport of Log exceptions from exceptionHandler method
    Log exceptions from exceptionHandler method
    in console.php.
    Also reduce the size of try/catch block. And
    restrict it to the require statement.
    
    Signed-off-by: Sujith H <sharidasan@owncloud.com>

--- lib/private/Console/Application.php
+++ lib/private/Console/Application.php
@@ -52,10 +52,10 @@ class Application {
 	 * @param EventDispatcherInterface $dispatcher
 	 * @param IRequest $request
 	 */
-	public function __construct(IConfig $config, EventDispatcherInterface $dispatcher, IRequest $request) {
+	public function __construct(IConfig $config, SymfonyApplication $application, EventDispatcherInterface $dispatcher, IRequest $request) {
 		$defaults = new OC_Defaults;
 		$this->config = $config;
-		$this->application = new SymfonyApplication($defaults->getName(), \OC_Util::getVersionString());
+		$this->application = $application;
 		$this->dispatcher = $dispatcher;
 		$this->request = $request;
 	}
