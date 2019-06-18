d470e91a869780332f90ca20bc61378303e37aec
Thomas Boerger <thomas@webhippie.de>
2019-06-18 14:33:41 +0200

    Replace drone matrix config by jsonnet definition
    To be prepared for the Drone 1.x.x migration we have built a
    jsonnet-based multi pipeline definition which is a hard requirement for
    the new build infrastructure. After upgrading Drone to the current LTS
    version we directly got to merge this, and rebase all remaining pull
    requests.

--- tests/drone/test-javascript.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-if [[ "$(pwd)" == "$(cd "$(dirname "$0")"; pwd -P)" ]]; then
-  echo "Can only be executed from project root!"
-  exit 1
-fi
-
-exec ./build/node_modules/.bin/karma start tests/karma.config.js --single-run
