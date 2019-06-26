e29b07a10e84a04c47947c8d8ea83517c45e192d
Thomas Müller <thomas.mueller@tmit.eu>
2019-06-26 16:44:46 +0200

    Adjust API acceptance tests to use the new webdav trash bin api

--- apps/dav/lib/TrashBin/TrashBinManager.php
+++ apps/dav/lib/TrashBin/TrashBinManager.php
@@ -54,6 +54,9 @@ class TrashBinManager {
 				$path = $view->getPath($fileId);
 			}
 			$fileInfo = $view->getFileInfo($path);
+			if ($fileInfo === false) {
+				throw new NotFound();
+			}
 			if ($fileInfo->getMimetype() !== 'httpd/unix-directory') {
 				throw new InvalidResourceType();
 			}
