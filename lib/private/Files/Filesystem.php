95318244115fff7309d3409f482573d69f1cc65e
Thomas DAGET <tdaget@users.noreply.github.com>
2019-10-26 02:47:41 +0200

    Update Filesystem.php

--- lib/private/Files/Filesystem.php
+++ lib/private/Files/Filesystem.php
@@ -632,13 +632,13 @@ class Filesystem {
 	*/
 	private static function is_preg_error() {
 		$errors = array(
-		        PREG_NO_ERROR               => 'Code 0 : No errors',
-		        PREG_INTERNAL_ERROR         => 'Code 1 : Internal PCRE error, most likely a syntax error',
-		        PREG_BACKTRACK_LIMIT_ERROR  => 'Code 2 : Backtrack limit was exhausted',
-		        PREG_RECURSION_LIMIT_ERROR  => 'Code 3 : Recursion limit was exhausted',
-		        PREG_BAD_UTF8_ERROR         => 'Code 4 : The offset did not correspond to the begin of a valid UTF-8 code point',
-		        PREG_BAD_UTF8_OFFSET_ERROR  => 'Code 5 : Malformed UTF-8 data',
-		        PREG_JIT_STACKLIMIT_ERROR   => 'Code 6 : Just-in-time compiler stack limit reached',
+			PREG_NO_ERROR               => 'Code 0 : No errors',
+			PREG_INTERNAL_ERROR         => 'Code 1 : Internal PCRE error, most likely a syntax error',
+			PREG_BACKTRACK_LIMIT_ERROR  => 'Code 2 : Backtrack limit was exhausted',
+			PREG_RECURSION_LIMIT_ERROR  => 'Code 3 : Recursion limit was exhausted',
+			PREG_BAD_UTF8_ERROR         => 'Code 4 : The offset did not correspond to the begin of a valid UTF-8 code point',
+			PREG_BAD_UTF8_OFFSET_ERROR  => 'Code 5 : Malformed UTF-8 data',
+			PREG_JIT_STACKLIMIT_ERROR   => 'Code 6 : Just-in-time compiler stack limit reached',
 		);
 
 		return $errors[\preg_last_error()];
