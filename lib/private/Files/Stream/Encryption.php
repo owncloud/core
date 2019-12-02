d8c09974d95a50cc5f4a73731b8fe194313c60a2
martink-p <47943787+martink-p@users.noreply.github.com>
2019-12-02 15:56:07 +0100

    Update lib/private/Files/Stream/Encryption.php
    Co-Authored-By: Phil Davis <phil@jankaritech.com>

--- lib/private/Files/Stream/Encryption.php
+++ lib/private/Files/Stream/Encryption.php
@@ -307,7 +307,7 @@ class Encryption extends Wrapper {
 
 	/**
 	* stream_read wrapper to read a block of the requested size
-	* There is a problem with the underlying php functions not returing the requested amount of data in any case.
+	* There is a problem with the underlying php functions not returning the requested amount of data in any case.
 	* Thus, this function will call the read mechanism of its parent until it has read the currect amount.
 	*/
 	private function stream_read_block($blockSize) {
