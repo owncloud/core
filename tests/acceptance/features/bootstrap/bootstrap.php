0abb6039337f552226f76addee28e1d68c292ab4
Phil Davis <phil@jankaritech.com>
2019-12-03 16:30:28 +0545

    Revert "Behat bootstrap.php does not need core lib/conposer/autoload.php"
    This reverts commit eff18339a61836f674f9773c04b78fd9e66947a4.

--- tests/acceptance/features/bootstrap/bootstrap.php
+++ tests/acceptance/features/bootstrap/bootstrap.php
@@ -20,6 +20,8 @@
  *
  */
 
+require __DIR__ . '/../../../../lib/composer/autoload.php';
+
 $classLoader = new \Composer\Autoload\ClassLoader();
 $classLoader->addPsr4("Page\\", __DIR__ . "/../lib", true);
 $classLoader->addPsr4("TestHelpers\\", __DIR__ . "/../../../TestHelpers", true);
