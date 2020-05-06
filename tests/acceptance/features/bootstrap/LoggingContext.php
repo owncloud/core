f7490b609ecf9d87269f5ad25ea2e912ea0ddde3
Phil Davis <phil@jankaritech.com>
2020-05-06 18:18:00 +0545

    Replace deprecated assertRegexp with assertMatchesRegularExpression in acceptance tests

--- tests/acceptance/features/bootstrap/LoggingContext.php
+++ tests/acceptance/features/bootstrap/LoggingContext.php
@@ -113,7 +113,7 @@ class LoggingContext implements Context {
 							$message
 						);
 					} elseif ($comparingMode === 'matching') {
-						Assert::assertRegExp(
+						Assert::assertMatchesRegularExpression(
 							$expectedLogEntry[$attribute], $logEntry[$attribute],
 							$message
 						);
