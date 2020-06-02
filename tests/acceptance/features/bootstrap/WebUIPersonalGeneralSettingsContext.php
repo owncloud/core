002b9e906e7b7a86cf220e2b6ec9b9e0b86ff3c9
Phil Davis <phil@jankaritech.com>
2020-06-02 13:54:59 +0545

    Add new step 'the user changes the password to the current password'

--- tests/acceptance/features/bootstrap/WebUIPersonalGeneralSettingsContext.php
+++ tests/acceptance/features/bootstrap/WebUIPersonalGeneralSettingsContext.php
@@ -125,6 +125,20 @@ class WebUIPersonalGeneralSettingsContext extends RawMinkContext implements Cont
 	}
 
 	/**
+	 * @When the user changes the password to the current password using the webUI
+	 *
+	 * @return void
+	 * @throws \Exception
+	 */
+	public function theUserChangesThePasswordToCurrentPasswordUsingTheWebUI() {
+		$username = $this->featureContext->getCurrentUser();
+		$currentPassword = \trim($this->featureContext->getUserPassword($username));
+		$this->personalGeneralSettingsPage->changePassword(
+			$currentPassword, $currentPassword, $this->getSession()
+		);
+	}
+
+	/**
 	 * @When the user changes the password to :newPassword entering the wrong current password :wrongPassword using the webUI
 	 * @Given the user has changed the password to :newPassword entering the wrong current password :wrongPassword using the webUI
 	 *
