d6fe828fc561984923397431693c983aa27e8e17
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2020-06-10 13:28:46 +0200

    Include a migration to disable the enterprise_key app if present

---
+++ core/Migrations/Version20200610110817.php
@@ -0,0 +1,22 @@
+<?php
+namespace OC\Migrations;
+
+use OCP\Migration\ISimpleMigration;
+use OCP\Migration\IOutput;
+
+/**
+ * Disable enterprise_key app during the upgrade to 10.5.0
+ */
+class Version20200610110817 implements ISimpleMigration {
+
+	/**
+	 * @param IOutput $out
+	 */
+	public function run(IOutput $out) {
+		$appManager = \OC::$server->getAppManager();
+		if ($appManager->isInstalled('enterprise_key')) {
+			$appManager->disableApp('enterprise_key');
+			$out->warning('The enterprise_key app is deprecated and obsolete. It has been disabled.');
+		}
+	}
+}
