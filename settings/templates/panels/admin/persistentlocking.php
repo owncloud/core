d2d568785c8e8f2d901f2baa26626d728e4cd80f
Phil Davis <phil@jankaritech.com>
2020-07-17 16:10:36 +0545

    Add admin setttings checkbox to 'Enable manual file locking on the web UI'

--- settings/templates/panels/admin/persistentlocking.php
+++ settings/templates/panels/admin/persistentlocking.php
@@ -17,4 +17,10 @@ script('settings', 'panels/persistentlocking');
 		min="0"
 		value="<?php p($_['maximumTimeout'])?>" />
 	<br/>
+	<input type="checkbox" name="enable_lock_file_action" id="manualFileLockOnWebUIEnabled" class="checkbox"
+		   value="1" <?php if ($_['manualFileLockOnWebUIEnabled'] === 'yes') {
+	print_unescaped('checked="checked"');
+} ?> />
+	<label for="manualFileLockOnWebUIEnabled"><?php p($l->t('Enable manual file locking on the web UI'));?></label>
+	<br/>
 </div>
