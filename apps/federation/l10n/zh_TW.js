8661488fa1c328a9936d9f3704fcd11b62705836
Thomas Müller <thomas.mueller@tmit.eu>
2020-10-11 00:15:51 +0000

    [tx] updated from transifex

--- apps/federation/l10n/zh_TW.js
+++ apps/federation/l10n/zh_TW.js
@@ -6,6 +6,8 @@ OC.L10N.register(
     "No ownCloud server found" : "找不到ownCloud伺服器",
     "Could not add server" : "無法添加伺服器",
     "Federation" : "聯邦",
+    "ownCloud Federation allows you to connect with other trusted ownClouds to exchange the user directory. For example this will be used to auto-complete external users for federated sharing." : "ownCloud Federation允許您與其他可信的ownCloud連接與交換使用者資料夾。 範例，這將用於自動完成外部用戶的聯合共享。",
+    "Add server automatically once a federated share was created successfully" : "成功創建聯合分享後，自動地添加伺服器",
     "Trusted ownCloud Servers" : "可信的 ownCloud伺服器",
     "+ Add ownCloud server" : "+添加ownCloud伺服器",
     "ownCloud Server" : "ownCloud伺服器",
