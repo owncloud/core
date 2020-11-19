e076d9383e18e3ac4c9b262def4869745a902695
Thomas Müller <thomas.mueller@tmit.eu>
2020-11-19 01:35:28 +0000

    [tx] updated from transifex

--- apps/dav/l10n/hu_HU.js
+++ apps/dav/l10n/hu_HU.js
@@ -2,6 +2,7 @@ OC.L10N.register(
     "dav",
     {
     "Contact birthdays" : "Az ismerősök születésnapjai",
+    "User unknown" : "Felhasználó ismeretlen",
     "User disabled" : "Felhasználó letiltva",
     "Personal" : "Személyes",
     "Contacts" : "Névjegyek",
