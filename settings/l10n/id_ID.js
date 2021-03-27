16d04954125a87ecba7a1029290d332e97ec2c78
Jan Ackermann <jackermann@owncloud.com>
2021-03-27 01:21:06 +0000

    [tx] updated from transifex

---
+++ settings/l10n/id_ID.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "settings",
+    {
+    "Invalid email address" : "Alamat surel tidak benar",
+    "Create" : "Buat",
+    "Delete" : "Hapus",
+    "Cancel" : "Batal",
+    "Email" : "Email",
+    "Password" : "Kata Sandi",
+    "Username" : "Nama pengguna"
+},
+"nplurals=1; plural=0;");
