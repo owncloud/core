78e43b50fae57b3a1edaea38859234d1a976d18b
JanAckermann <jackermann@owncloud.com>
2021-07-26 12:39:11 +0200

    Add create cancel button to newFileMenu

--- apps/files/js/newfilemenu.js
+++ apps/files/js/newfilemenu.js
@@ -28,6 +28,10 @@
 		'<form class="filenameform" autocapitalize="none">' +
 		'<label class="hidden-visually" for="{{cid}}-input-{{fileType}}">{{fileName}}</label>' +
 		'<input id="{{cid}}-input-{{fileType}}" type="text" value="{{fileName}}" autocomplete="off">' +
+		'<div class="action-menu">' +
+		'<button type="button" class="cancel"> {{cancelLabel}} </button>' +
+		'<button type="submit" class="create primary"> {{createLabel}} </button>' +
+		'</div>' +
 		'</form>';
 
 	/**
@@ -41,7 +45,7 @@
 		className: 'newFileMenu popovermenu bubble hidden open menu',
 
 		events: {
-			'click .menuitem': '_onClickAction'
+			'click .menuitem': '_onClickAction',
 		},
 
 		/**
@@ -90,6 +94,11 @@
 		 */
 		_onClickAction: function(event) {
 			var $target = $(event.target);
+
+			if($target.hasClass('cancel') || $target.hasClass('create')){
+				return;
+			}
+
 			if (!$target.hasClass('menuitem')) {
 				$target = $target.closest('.menuitem');
 			}
@@ -115,7 +124,7 @@
 			}
 
 			if ($target.find('form').length) {
-				$target.find('input').focus();
+				$target.find('input[type=text]').focus();
 				return;
 			}
 
@@ -130,14 +139,16 @@
 			var $form = $(OCA.Files.NewFileMenu._TEMPLATE_FORM({
 				fileName: newName,
 				cid: this.cid,
-				fileType: fileType
+				fileType: fileType,
+				cancelLabel: t('files', 'Cancel'),
+				createLabel: t('files', 'Create')
 			}));
 
 			//this.trigger('actionPerformed', action);
 			$target.append($form);
 
 			// here comes the OLD code
-			var $input = $form.find('input');
+			var $input = $form.find('input[type=text]');
 
 			var lastPos;
 			var checkInput = function () {
@@ -160,8 +171,19 @@
 				return false;
 			};
 
+			var closeForm = function (){
+				$form.remove();
+				$target.find('.displayname').removeClass('hidden');
+			};
+
+
 			// verify filename on typing
-			$input.keyup(function() {
+			$input.keyup(function(e) {
+				if(e.key === 'Escape'){
+					closeForm();
+					return;
+				}
+
 				if (checkInput()) {
 					$input.tooltip('hide');
 					$input.removeClass('error');
@@ -175,6 +197,8 @@
 				lastPos = newName.length;
 			}
 			$input.selectRange(0, lastPos);
+
+			$form.find('.cancel').click(closeForm);
 
 			$form.submit(function(event) {
 				event.stopPropagation();
