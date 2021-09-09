96853ece655c6174062f4f52bab1402681545200
Jannik Stehle <jannik.stehle@gmail.com>
2021-09-09 10:29:54 +0200

    Clearify the behavior of the public link permission checks

--- apps/files_sharing/lib/Controller/Share20OcsController.php
+++ apps/files_sharing/lib/Controller/Share20OcsController.php
@@ -393,12 +393,12 @@ class Share20OcsController extends OCSController {
 		}
 
 		$shareType = (int)$this->request->getParam('shareType', '-1');
-		$defaultPermissionsUsed = false;
+		$noPermissionFromRequest = false;
 
 		// Parse permissions (if available)
 		$permissions = $this->getPermissionsFromRequest();
 		if ($permissions === null) {
-			$defaultPermissionsUsed = true;
+			$noPermissionFromRequest = true;
 			if ($shareType !== Share::SHARE_TYPE_LINK) {
 				$permissions = $this->config->getAppValue('core', 'shareapi_default_permissions', Constants::PERMISSION_ALL);
 			} else {
@@ -508,8 +508,10 @@ class Share20OcsController extends OCSController {
 			}
 
 			// don't allow "create"-permission if public upload is not allowed.
-			// we only need this check if we're not dealing with the default permissions.
-			if (!$defaultPermissionsUsed && !$publicUploadAllowed && $permissions & Constants::PERMISSION_CREATE) {
+			// we only need this check if permissions were passed via the request, otherwise
+			// it is already being handled.
+			$includesCreatePermission = $permissions & Constants::PERMISSION_CREATE;
+			if (!$noPermissionFromRequest && !$publicUploadAllowed && $includesCreatePermission) {
 				$share->getNode()->unlock(ILockingProvider::LOCK_SHARED);
 				return new Result(null, 403, $this->l->t('Public upload disabled by the administrator'));
 			}
@@ -898,9 +900,8 @@ class Share20OcsController extends OCSController {
 			}
 
 			// create (upload)
-			if (
-				$newPermissions & Constants::PERMISSION_CREATE
-			) {
+			$includesCreatePermission = $newPermissions & Constants::PERMISSION_CREATE;
+			if ($includesCreatePermission) {
 				if (!$this->shareManager->shareApiLinkAllowPublicUpload()) {
 					$share->getNode()->unlock(ILockingProvider::LOCK_SHARED);
 					return new Result(null, 403, $this->l->t('Public upload disabled by the administrator'));
