1f26c1c15faa626c550867162a41895cdaa602f7
JanAckermann <jackermann@owncloud.com>
2021-09-14 10:16:07 +0200

    Move delete button in file list next to download button

--- apps/files/templates/list.php
+++ apps/files/templates/list.php
@@ -52,6 +52,10 @@
 							<span class="icon icon-download"></span>
 							<span><?php p($l->t('Download'))?></span>
 						</a>
+						<a href="" class="delete-selected">
+							<span class="icon icon-delete"></span>
+							<span><?php p($l->t('Delete'))?></span>
+						</a>
 					</span>
 				</div>
 			</th>
@@ -60,10 +64,6 @@
 			</th>
 			<th id="headerDate" class="hidden column-mtime">
 				<a id="modified" class="columntitle" data-sort="mtime"><span><?php p($l->t('Modified')); ?></span><span class="sort-indicator"></span></a>
-					<span class="selectedActions"><a href="" class="delete-selected">
-						<span><?php p($l->t('Delete'))?></span>
-						<span class="icon icon-delete"></span>
-					</a></span>
 			</th>
 		</tr>
 	</thead>
