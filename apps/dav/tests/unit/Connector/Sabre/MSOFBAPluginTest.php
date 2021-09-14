bf57db09e0a148b35b07dd9106992b04dad44ec6
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2021-09-14 12:26:26 +0200

    fix: login redirect url has to hold the msofba-success route which was missing due to the earlier change (#39218)

--- apps/dav/tests/unit/Connector/Sabre/MSOFBAPluginTest.php
+++ apps/dav/tests/unit/Connector/Sabre/MSOFBAPluginTest.php
@@ -2,7 +2,6 @@
 namespace OCA\DAV\Tests\unit\Connector\Sabre;
 
 use OCA\DAV\Connector\Sabre\MSOFBAPlugin;
-use OCP\IURLGenerator;
 use OCP\IUserSession;
 use PHPUnit\Framework\MockObject\MockObject;
 use Sabre\DAV\Server;
@@ -25,10 +24,6 @@ class MSOFBAPluginTest extends TestCase {
 	 * @var IUserSession | MockObject
 	 */
 	private $userSession;
-	/**
-	 * @var IURLGenerator|MockObject
-	 */
-	private $urlGenerator;
 
 	public function setUp(): void {
 		parent::setUp();
@@ -41,25 +36,13 @@ class MSOFBAPluginTest extends TestCase {
 		$this->server->httpRequest->setMethod('OPTIONS');
 
 		$this->userSession = $this->createMock(IUserSession::class);
-		$this->urlGenerator = $this->createMock(IURLGenerator::class);
 
-		$this->plugin = new MSOFBAPlugin($this->userSession, $this->urlGenerator);
+		$this->plugin = new MSOFBAPlugin($this->userSession, \OC::$server->getURLGenerator());
 	}
 
 	public function testMSOfficeNotAuthorized(): void {
 		# sapi to be called
 		$this->server->sapi->expects(self::once())->method('sendResponse')->with($this->server->httpResponse);
-
-		# some url need to be generated
-		$this->urlGenerator->expects(self::once())->method('linkToRoute')->willReturn('/success');
-		$this->urlGenerator->expects(self::exactly(2))->method('linkToRouteAbsolute')->willReturnCallback(static function ($routeName) {
-			if ($routeName === 'dav.msofba.success') {
-				return 'https://example.net/success';
-			}
-			if ($routeName === 'core.login.showLoginForm') {
-				return 'https://example.net/login';
-			}
-		});
 
 		$this->server->httpRequest->setAbsoluteUrl('https://currentdomain.tld:8080/owncloud/remote.php/dav/files/user1/target/path');
 		$this->server->httpRequest->setUrl('/owncloud/remote.php/dav/files/user1/target/path');
@@ -69,17 +52,13 @@ class MSOFBAPluginTest extends TestCase {
 		$this->server->start();
 
 		self::assertEquals(403, $this->server->httpResponse->getStatus());
-		self::assertEquals('https://example.net/login', $this->server->httpResponse->getHeader('X-FORMS_BASED_AUTH_REQUIRED'));
+		self::assertStringEndsWith('index.php/login?redirect_url=/index.php/apps/dav/msofba-success', $this->server->httpResponse->getHeader('X-FORMS_BASED_AUTH_REQUIRED'));
 	}
 
 	public function testMSOfficeAuthorized(): void {
 		$this->userSession->expects(self::once())->method('isLoggedIn')->willReturn(true);
 		# sapi to be called
 		$this->server->sapi->expects(self::once())->method('sendResponse')->with($this->server->httpResponse);
-
-		# some url need to be generated
-		$this->urlGenerator->expects(self::never())->method('linkToRoute');
-		$this->urlGenerator->expects(self::never())->method('linkToRouteAbsolute');
 
 		$this->server->httpRequest->setAbsoluteUrl('https://currentdomain.tld:8080/owncloud/remote.php/dav/files/user1/target/path');
 		$this->server->httpRequest->setUrl('/owncloud/remote.php/dav/files/user1/target/path');
@@ -96,10 +75,6 @@ class MSOFBAPluginTest extends TestCase {
 		$this->userSession->expects(self::never())->method('isLoggedIn');
 		# sapi to be called
 		$this->server->sapi->expects(self::once())->method('sendResponse')->with($this->server->httpResponse);
-
-		# some url need to be generated
-		$this->urlGenerator->expects(self::never())->method('linkToRoute');
-		$this->urlGenerator->expects(self::never())->method('linkToRouteAbsolute');
 
 		$this->server->httpRequest->setAbsoluteUrl('https://currentdomain.tld:8080/owncloud/remote.php/dav/files/user1/target/path');
 		$this->server->httpRequest->setUrl('/owncloud/remote.php/dav/files/user1/target/path');
