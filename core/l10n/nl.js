14b9b784e94b2a4756f6c9df08fbc696ff8815e9
Lukas Hirt <lhirt@owncloud.com>
2021-09-15 01:01:37 +0000

    [tx] updated from transifex

--- core/l10n/nl.js
+++ core/l10n/nl.js
@@ -257,7 +257,7 @@ OC.L10N.register(
     "You can click here to return to %s." : "Klik hier om terug te keren naar %s.",
     "Hey there,\n\njust letting you know that %s shared %s with you.\nView it: %s\n\n" : "Hallo,\n\n%s deelt %s met u.\nBekijk hier: %s\n\n",
     "The share will expire on %s." : "De share vervalt op %s.",
-    "Cheers!" : "Tot ziens!",
+    "Cheers!" : "Met vriendelijke groet",
     "Internal Server Error" : "Interne serverfout",
     "The server encountered an internal error and was unable to complete your request." : "De server ontdekte een interne fout en kon uw aanvraag niet verder uitvoeren.",
     "Please contact the server administrator if this error reappears multiple times and include the technical details below in your report." : "Neem contact op met de serverbeheerder als deze fout meerdere keren opnieuw optreedt en neem de onderstaande technische details op in uw melding.",
