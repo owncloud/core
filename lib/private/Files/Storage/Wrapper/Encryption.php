1fea2de22f0936dbc9f2b6929cf84d28edd2f907
Ilja Neumann <ineumann@owncloud.com>
2021-12-07 14:50:46 +0100

    Simplify öast commit

--- lib/private/Files/Storage/Wrapper/Encryption.php
+++ lib/private/Files/Storage/Wrapper/Encryption.php
@@ -1009,17 +1009,8 @@ class Encryption extends Wrapper {
 
 		// if the header doesn't contain a encryption module we check if it is a
 		// legacy file. If true, we add the default encryption module
-		if (!isset($result[Util::HEADER_ENCRYPTION_MODULE_KEY])) {
-			if (!empty($result)) {
-				$result[Util::HEADER_ENCRYPTION_MODULE_KEY] = 'OC_DEFAULT_MODULE';
-			} elseif ($exists) {
-				// if the header was empty we have to check first if it is a encrypted file at all
-				// We would do query to filecache only if we know that entry in filecache exists
-				$info = $this->getCache()->get($path);
-				if (isset($info['encrypted']) && $info['encrypted'] === true) {
-					$result[Util::HEADER_ENCRYPTION_MODULE_KEY] = 'OC_DEFAULT_MODULE';
-				}
-			}
+		if (!isset($result[Util::HEADER_ENCRYPTION_MODULE_KEY]) && $exists) {
+			$result[Util::HEADER_ENCRYPTION_MODULE_KEY] = 'OC_DEFAULT_MODULE';
 		}
 
 		return $result;
