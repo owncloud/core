922e6e6db24e4e75857fc9b1263b631ed170ae61
Swikriti Tripathi <swikriti808@gmail.com>
2021-12-14 10:52:46 +0545

    try with 3 sec pause

--- tests/acceptance/features/lib/OwncloudPage.php
+++ tests/acceptance/features/lib/OwncloudPage.php
@@ -272,7 +272,7 @@ class OwncloudPage extends Page {
 			$ocDialog = $this->getPage("OwncloudPageElement\\OCDialog");
 			$ocDialog->setElement($element);
 			$ocDialogs[] = $ocDialog;
-			sleep(5);
+			sleep(3);
 		}
 		return $ocDialogs;
 	}
