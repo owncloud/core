1ad3c611ed7df1e37b6c621f419fe8ab9835abd7
mmattel <martin.mattel@diemattels.at>
2022-01-30 13:00:35 +0100

    Add wnd.listen.reconnectAfterTime config sample

--- config/config.apps.sample.php
+++ config/config.apps.sample.php
@@ -428,6 +428,8 @@ $CONFIG = [
  *
  * Possible keys: `wnd.groupmembership.checkUserFirst` BOOL
  *
+ * Possible keys: `wnd.connector.opts.timeout` INTEGER
+ *
  * *Note* With WND 2.1.0, key `wnd.storage.testForHiddenMount` is obsolete and has been removed completely.
  */
 
@@ -538,6 +540,12 @@ $CONFIG = [
  * recommended to enable this option only if there are a high number of ACLs targeting users.
  */
 'wnd.groupmembership.checkUserFirst' => false,
+
+/**
+ * The timeout (in ms) for all the operations against the backend.
+ * The same timeout will be applied for all the connections.
+ */
+'wnd.connector.opts.timeout' => 20000,  // 20 seconds
 
 /**
  * App: Workflow / Tagging
