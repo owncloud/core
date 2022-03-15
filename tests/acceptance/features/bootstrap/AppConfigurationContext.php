f07e5b132b499875cd2da066268ef3bde74d24b9
Phil Davis <phil@jankaritech.com>
2022-03-16 05:19:23 +0545

    Provide PseudoAdminForRevaTest

--- tests/acceptance/features/bootstrap/AppConfigurationContext.php
+++ tests/acceptance/features/bootstrap/AppConfigurationContext.php
@@ -182,12 +182,32 @@ class AppConfigurationContext implements Context {
 	}
 
 	/**
+	 * @return string
+	 */
+	public function getAdminUsernameForCapabilitiesCheck():string {
+		if (\TestHelpers\OcisHelper::isTestingOnReva()) {
+			// When testing on reva we don't have a user called "admin" to use
+			// to access the capabilities. So create an ordinary user on-the-fly
+			// with a default password. That user should be able to get a
+			// capabilities response that the test can process.
+			$adminUsername = "PseudoAdminForRevaTest";
+			$createdUsers = $this->featureContext->getCreatedUsers();
+			if (!\array_key_exists($adminUsername, $createdUsers)) {
+				$this->featureContext->createUser($adminUsername);
+			}
+		} else {
+			$adminUsername = $this->featureContext->getAdminUsername();
+		}
+		return $adminUsername;
+	}
+
+	/**
 	 * @When the administrator retrieves the capabilities using the capabilities API
 	 *
 	 * @return void
 	 */
 	public function theAdministratorGetsCapabilities():void {
-		$this->userGetsCapabilities($this->featureContext->getAdminUsername());
+		$this->userGetsCapabilities($this->getAdminUsernameForCapabilitiesCheck());
 	}
 
 	/**
@@ -197,7 +217,7 @@ class AppConfigurationContext implements Context {
 	 * @throws Exception
 	 */
 	public function theAdministratorGetsCapabilitiesCheckResponse():void {
-		$this->userGetsCapabilitiesCheckResponse($this->featureContext->getAdminUsername());
+		$this->userGetsCapabilitiesCheckResponse($this->getAdminUsernameForCapabilitiesCheck());
 	}
 
 	/**
