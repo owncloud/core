42f032157a7b61d832ae3dd8012babaf69ea277f
ownClouders <devops@owncloud.com>
2022-04-05 00:54:01 +0000

    [tx] updated from transifex

--- apps/dav/l10n/fr.js
+++ apps/dav/l10n/fr.js
@@ -10,4 +10,4 @@ OC.L10N.register(
     "Remote Address: %s" : "Adresse distante : %s",
     "Request ID: %s" : "ID de la demande : %s"
 },
-"nplurals=2; plural=(n > 1);");
+"nplurals=3; plural=(n==0 || n==1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
