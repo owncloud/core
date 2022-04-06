e6856641c6d789cae14afdfcbcdd46fc00549fce
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2022-04-06 10:20:22 +0200

    Add mouse tracking if session_keepalive is false

--- core/js/js.js
+++ core/js/js.js
@@ -1570,10 +1570,80 @@ function initCore() {
 		heartBeat();
 	}
 
+	/**
+	 * Check mouse movement to send a heartbeat if the mouse moved
+	 */
+	function initMouseTrack() {
+		var interval = 120;  // 2 minutes
+		if (oc_config.session_lifetime) {
+			interval = Math.floor(oc_config.session_lifetime / 2);
+		}
+		interval = Math.min(Math.max(interval, 60), 3600);  // ensure interval is in [60, 3600]
+
+		var extendableHeartbeat = null;
+		var extendableMouseMoved = false;
+		var mouseMoved = false;
+		var heartbeatTimestamp = 0;
+
+		// If the mouse has moved within this interval, set a timeout to send
+		// a heartbeat request. If the mouse moves again in the next interval,
+		// the timeout will be extended. Note that the heartbeat request might
+		// be delayed indefinitely.
+		setInterval(function() {
+			if (extendableMouseMoved) {
+				clearTimeout(extendableHeartbeat);
+				extendableHeartbeat = setTimeout(function() {
+					if (!extendableMouseMoved) {
+						// if the mouse has moved, either this timeout has been cleared and
+						// a new one is being created, or a new timeout will be created soon
+						// (if this timeout is executed before the setInterval function)
+						var currentTimestamp = Date.now();
+						if (currentTimestamp > heartbeatTimestamp + (30 * 1000)) {
+							// if a heartbeat has been sent recently, don't send a new one
+							console.log('I1 sending heartbeat');
+							$.post(OC.generateUrl('/heartbeat'));
+							heartbeatTimestamp = currentTimestamp;
+						} else {
+							console.log('I1 skipping heartbeat');
+						}
+					} else {
+						console.log('I1 delaying hearbeat because mouse moved.');
+					}
+				}, 30 * 1000);
+			}
+			extendableMouseMoved = false;
+		}, 30 * 1000);
+
+		// If the mouse has been moved within this interval, send a heartbeat
+		// immediately. This will prevent the session to expire.
+		setInterval(function() {
+			if (mouseMoved) {
+				var currentTimestamp = Date.now();
+				if (currentTimestamp > heartbeatTimestamp + (interval * 1000)) {
+					// if a heartbeat has been sent recently, don't send a new one
+					console.log('I2 sending heartbeat');
+					$.post(OC.generateUrl('/heartbeat'));
+					heartbeatTimestamp = currentTimestamp;
+				} else {
+					console.log('I2 skipping heartbeat');
+				}
+			}
+			mouseMoved = false;  // no need to wait for the response
+		}, interval * 1000);
+
+		// set a couple of variables to indicate that the mouse has moved
+		$(window).on('mousemove', function() {
+			mouseMoved = true;
+			extendableMouseMoved = true;
+		});
+	}
+
 	// session heartbeat (defaults to enabled)
 	if (typeof(oc_config.session_keepalive) === 'undefined' || !!oc_config.session_keepalive) {
 
 		initSessionHeartBeat();
+	} else {
+		initMouseTrack();
 	}
 
 	OC.registerMenu($('#expand'), $('#expanddiv'));
