d38920a8eeff410abb7ed5b81db0aaedd721fec4
Kiran Parajuli <kiranparajuli589@gmail.com>
2022-04-12 14:18:29 +0545

    Only load ocis helpers if PATH_TO_OCIS is set to a valid directory
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>

--- tests/acceptance/features/bootstrap/bootstrap.php
+++ tests/acceptance/features/bootstrap/bootstrap.php
@@ -21,19 +21,18 @@
  */
 
 $pathToOcis = \getenv("PATH_TO_OCIS");
-if (!$pathToOcis) {
-	$pathToOcis = "../../../../../ocis";
-}
 
 $classLoader = new \Composer\Autoload\ClassLoader();
 $classLoader->addPsr4("Page\\", __DIR__ . "/../lib", true);
 $classLoader->addPsr4("TestHelpers\\", __DIR__ . "/../../../TestHelpers", true);
-$classLoader->addPsr4("TestHelpers\\", $pathToOcis . "/tests/TestHelpers", true);
-$classLoader->addPsr4(
-	"",
-	$pathToOcis . "/tests/acceptance/features/bootstrap",
-	true
-);
+if ($pathToOcis && \is_dir($pathToOcis)) {
+	$classLoader->addPsr4("TestHelpers\\", $pathToOcis . "/tests/TestHelpers", true);
+	$classLoader->addPsr4(
+		"",
+		$pathToOcis . "/tests/acceptance/features/bootstrap",
+		true
+	);
+}
 $classLoader->register();
 
 // Sleep for 10 milliseconds
