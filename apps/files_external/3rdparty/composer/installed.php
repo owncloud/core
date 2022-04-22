ec0bbc2c5bc97db23e4996daba5b0e98e481d9ac
Phil Davis <phil@jankaritech.com>
2022-04-22 10:37:06 +0545

    Do composer update with latest composer 2.3.5 in 3rdparty dir

--- apps/files_external/3rdparty/composer/installed.php
+++ apps/files_external/3rdparty/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '9fd80f4cb3b7abc036b59d97987803d4beb46298',
         'name' => 'files_external/3rdparty',
         'dev' => true,
     ),
     'versions' => array(
         'files_external/3rdparty' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '9fd80f4cb3b7abc036b59d97987803d4beb46298',
             'dev_requirement' => false,
         ),
         'firebase/php-jwt' => array(
