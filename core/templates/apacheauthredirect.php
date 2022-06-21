ebbd6efc2328690455aafb04bfc3c92ee15b7469
Piotr Mrowczynski <pmrowcz@gmail.com>
2022-06-21 22:48:18 +0200

    complete apache auth flow with redirect page

---
+++ core/templates/apacheauthredirect.php
@@ -0,0 +1,10 @@
+<?php
+
+script('core', [
+	'apacheauthredirect'
+]);
+?>
+
+<span class="msg">
+	<?php p($l->t('The application was authorized successfully. You can now close this window.'));?>
+</span>
