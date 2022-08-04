63af5a48d6df924ff6228735ba614068f569f953
Phil Davis <phil@jankaritech.com>
2022-08-04 15:01:21 +0545

    Adjust acceptance test scenarios for read-write public link shares of files

--- tests/acceptance/features/bootstrap/Sharing.php
+++ tests/acceptance/features/bootstrap/Sharing.php
@@ -123,6 +123,10 @@ trait Sharing {
 		'attributes', 'permissions', 'share_with', 'share_with_displayname', 'share_with_additional_info'
 	];
 
+	/*
+	 * Contains information about the public links that have been created with the webUI.
+	 * Each entry in the array has a "name", "url" and "path".
+	 */
 	private $createdPublicLinks = [];
 
 	/**
@@ -130,6 +134,41 @@ trait Sharing {
 	 */
 	public function getCreatedPublicLinks():array {
 		return $this->createdPublicLinks;
+	}
+
+	/**
+	 * The end (last) entry will itself be an array with keys "name", "url" and "path"
+	 *
+	 * @return array
+	 */
+	public function getLastCreatedPublicLink():array {
+		return \end($this->createdPublicLinks);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLastCreatedPublicLinkUrl():string {
+		$lastCreatedLink = $this->getLastCreatedPublicLink();
+		return $lastCreatedLink["url"];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLastCreatedPublicLinkPath():string {
+		$lastCreatedLink = $this->getLastCreatedPublicLink();
+		return $lastCreatedLink["path"];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLastCreatedPublicLinkToken():string {
+		$lastCreatedLinkUrl = $this->getLastCreatedPublicLinkUrl();
+		// The token is the last part of the URL, delimited by "/"
+		$urlParts = \explode("/", $lastCreatedLinkUrl);
+		return \end($urlParts);
 	}
 
 	/**
@@ -1104,11 +1143,12 @@ trait Sharing {
 	/**
 	 * @param string $name
 	 * @param string $url
+	 * @param string $path
 	 *
 	 * @return void
 	 */
-	public function addToListOfCreatedPublicLinks(string $name, string $url):void {
-		$this->createdPublicLinks[] = ["name" => $name, "url" => $url];
+	public function addToListOfCreatedPublicLinks(string $name, string $url, string $path = ""):void {
+		$this->createdPublicLinks[] = ["name" => $name, "url" => $url, "path" => $path];
 	}
 
 	/**
@@ -1183,7 +1223,7 @@ trait Sharing {
 				if (isset($this->lastPublicShareData->data)) {
 					$linkName = (string) $this->lastPublicShareData->data[0]->name;
 					$linkUrl = (string) $this->lastPublicShareData->data[0]->url;
-					$this->addToListOfCreatedPublicLinks($linkName, $linkUrl);
+					$this->addToListOfCreatedPublicLinks($linkName, $linkUrl, $path);
 				}
 			} else {
 				$shareData = $this->getResponseXml(null, __METHOD__);
@@ -3688,14 +3728,39 @@ trait Sharing {
 	}
 
 	/**
+	 * The tests can create public link shares with the API or with the webUI.
+	 * If lastPublicShareData is null, then there have not been any created with the API,
+	 * so look for details of a public link share created with the webUI.
+	 *
 	 * @return string authorization token
 	 */
 	public function getLastPublicShareToken():string {
-		if (\count($this->lastPublicShareData->data->element) > 0) {
-			return (string)$this->lastPublicShareData->data[0]->token;
-		}
+		if ($this->lastPublicShareData === null) {
+			return $this->getLastCreatedPublicLinkToken();
+		} else {
+			if (\count($this->lastPublicShareData->data->element) > 0) {
+				return (string)$this->lastPublicShareData->data[0]->token;
+			}
 
-		return (string)$this->lastPublicShareData->data->token;
+			return (string)$this->lastPublicShareData->data->token;
+		}
+	}
+
+	/**
+	 * @return string path of file that was shared (relevant when a single file has been shared)
+	 */
+	public function getLastPublicSharePath():string {
+		if ($this->lastPublicShareData === null) {
+			// There have not been any public links created with the API
+			// so get the path of the last public link created with the webUI
+			return $this->getLastCreatedPublicLinkPath();
+		} else {
+			if (\count($this->lastPublicShareData->data->element) > 0) {
+				return (string)$this->lastPublicShareData->data[0]->path;
+			}
+
+			return (string)$this->lastPublicShareData->data->path;
+		}
 	}
 
 	/**
