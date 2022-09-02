23f9633e67300213dd76a4f88f8b8ec40d092e81
Piotr Mrowczynski <pmrowcz@gmail.com>
2022-09-02 11:57:29 +0200

    adjustments for public link file readwrite continued

--- apps/files_sharing/lib/Controller/Share20OcsController.php
+++ apps/files_sharing/lib/Controller/Share20OcsController.php
@@ -532,6 +532,7 @@ class Share20OcsController extends OCSController {
 				);
 			} elseif ($permissions === Constants::PERMISSION_CREATE ||
 				$permissions === (Constants::PERMISSION_READ | Constants::PERMISSION_CREATE | Constants::PERMISSION_UPDATE | Constants::PERMISSION_DELETE) ||
+				$permissions === (Constants::PERMISSION_READ | Constants::PERMISSION_UPDATE) ||
 				$permissions === (Constants::PERMISSION_READ | Constants::PERMISSION_CREATE)) {
 				$share->setPermissions($permissions);
 			} else {
