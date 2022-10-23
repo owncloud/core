032f166f0595f293d6690d097d9019d13ba1a427
ownClouders <devops@owncloud.com>
2022-10-23 00:48:10 +0000

    [tx] updated from transifex

--- apps/dav/l10n/bg_BG.js
+++ apps/dav/l10n/bg_BG.js
@@ -1,13 +1,13 @@
 OC.L10N.register(
     "dav",
     {
-    "Contact birthdays" : "Рождени дни на контакти",
+    "Contact birthdays" : "Рождени дни на контактите",
     "User unknown" : "Потребителят е неизвестен",
     "User disabled" : "Потребителят е деактивиран",
     "Personal" : "Лично",
     "Contacts" : "Контакти",
     "Technical details" : "Технически подробности",
     "Remote Address: %s" : "Отдалечен адрес: %s",
-    "Request ID: %s" : "ID на заявка: %s"
+    "Request ID: %s" : "ID на заявката: %s"
 },
 "nplurals=2; plural=(n != 1);");
--- apps/federation/l10n/bg_BG.js
+++ apps/federation/l10n/bg_BG.js
@@ -1,16 +1,16 @@
 OC.L10N.register(
     "federation",
     {
-    "Server added to the list of trusted ownClouds" : "Сървърат добавен в листа със сигурни ownClouds",
-    "Server is already in the list of trusted servers." : "Сървъра вече е добавен в листа с сигурни сървъри",
+    "Server added to the list of trusted ownClouds" : "Сървърът е добавен в списъка с доверени облаци ownClouds",
+    "Server is already in the list of trusted servers." : "Сървърът вече е в списъка с доверени сървъри.",
     "No ownCloud server found" : "Не е намерен ownCloud сървър",
     "Could not add server" : "Неуспешно добавяне на сървър",
     "Federation" : "Федерация",
-    "ownCloud Federation allows you to connect with other trusted ownClouds to exchange the user directory. For example this will be used to auto-complete external users for federated sharing." : "ownCloud федеративността разрешава да се свързвате с други доверени ownCloud-ове за обмянвата на потребителската папка. На пример това ще бъде използвано за автоматично допълване на външни потребители за федеративно споделяне.",
+    "ownCloud Federation allows you to connect with other trusted ownClouds to exchange the user directory. For example this will be used to auto-complete external users for federated sharing." : "ownCloud Federation ви позволява да се свързвате с други доверени ownClouds, за да обменяте потребителската директория. Например това ще се използва за автоматично добавяне на външни потребители за федеративно споделяне.",
     "Add server automatically once a federated share was created successfully" : "Автоматично добавяне на сървъра, когато федеративното споделяне е успешно създадено.",
-    "Trusted ownCloud Servers" : "Сигурни ownCloud сървъри",
+    "Trusted ownCloud Servers" : "Доверени ownCloud сървъри",
     "+ Add ownCloud server" : "+ Добавяне ownCloud сървър",
     "ownCloud Server" : "ownCloud сървър",
-    "Add" : "Добави"
+    "Add" : "Добавяне"
 },
 "nplurals=2; plural=(n != 1);");
