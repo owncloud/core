ad410b2d35913b2443cdaecb86c707cc190da9c6
Piotr Mrówczyński <pmrowcz@gmail.com>
2022-11-28 10:38:47 +0100

    Update apacheauthredirect.php

--- core/templates/apacheauthredirect.php
+++ core/templates/apacheauthredirect.php
@@ -8,5 +8,5 @@ script('core', [
 <?php print_unescaped('<input type="hidden" id="redirect_url" name="redirect_url" value="' . \OCP\Util::sanitizeHTML($_['redirect_url']) . '">'); ?>
 
 <p class="info">
-	<?php p($l->t('The application was authorized successfully. You should now get redirected to the requested page, otherwise you can close this window.'));?>
+	<?php p($l->t('The application was authorized successfully. You will now get redirected to the requested page, otherwise you can close this window.'));?>
 </p>
