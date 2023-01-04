06c801e25745c2fedd9d8b67eb6b453db3607048
Jürgen Weigert <jnweiger@gmail.com>
2023-01-04 14:33:16 -0800

    improve wording with untrusted domain (#40502)
    This avoids a misunderstanding and thus fixes https://github.com/owncloud/core/issues/35800

--- core/templates/untrustedDomain.php
+++ core/templates/untrustedDomain.php
@@ -2,7 +2,7 @@
 
 <ul class="error-wide">
 	<li class='error'>
-		<?php p($l->t('You are accessing the server from an untrusted domain.')); ?><br>
+		<?php p($l->t('You are accessing the server through an untrusted domain.')); ?><br>
 
 		<p class='hint'>
 			<?php p($l->t('Please contact your administrator. If you are an administrator of this instance, configure the "trusted_domains" setting in config/config.php. ')) ?>
