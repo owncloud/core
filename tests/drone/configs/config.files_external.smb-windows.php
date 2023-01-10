2d668015ffac6d56d2ec6ec2cd2711e4beba18f9
Phil Davis <phil@jankaritech.com>
2023-01-10 15:47:29 +0545

    Use env vars for values in config.files_external.smb-windows.php

--- tests/drone/configs/config.files_external.smb-windows.php
+++ tests/drone/configs/config.files_external.smb-windows.php
@@ -1,10 +1,39 @@
 <?php
+$envVarsSet = true;
+$smbHost = \getenv('SMB_WINDOWS_HOST');
+if ($smbHost === false) {
+	$envVarsSet = false;
+	echo __FILE__ . " SMB_WINDOWS_HOST has not been set";
+}
+$smbUserName = \getenv('SMB_WINDOWS_USERNAME');
+if ($smbUserName === false) {
+	$envVarsSet = false;
+	echo __FILE__ . " SMB_WINDOWS_USERNAME has not been set";
+}
+$smbUserPassword = \getenv('SMB_WINDOWS_PWD');
+if ($smbUserPassword === false) {
+	$envVarsSet = false;
+	echo __FILE__ . " SMB_WINDOWS_PWD has not been set";
+}
+$smbShareName = \getenv('SMB_WINDOWS_SHARE_NAME');
+if ($smbShareName === false) {
+	$envVarsSet = false;
+	echo __FILE__ . " SMB_WINDOWS_SHARE_NAME has not been set";
+}
+$smbDomain = \getenv('SMB_WINDOWS_DOMAIN');
+if ($smbDomain === false) {
+	$envVarsSet = false;
+	echo __FILE__ . " SMB_WINDOWS_DOMAIN has not been set";
+}
+if ($envVarsSet === false) {
+	echo __FILE__ . " the above environment variables must be set when running the SMB WIndows PHP unit tests";
+}
 return [
-	'run'=>true,
-	'host'=>'fsweb.test.owncloud.com',
-	'user'=>'test100',
-	'password'=>'Password123!',
-	'root'=>'',
-	'share'=>'test100',
-	'domain'=>'WORKGROUP',
+	'run' => true,
+	'host' => $smbHost,
+	'user' => $smbUserName,
+	'password' => $smbUserPassword,
+	'root' => '',
+	'share' => $smbShareName,
+	'domain' => $smbDomain,
 ];
