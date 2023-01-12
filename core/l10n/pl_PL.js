9fbacf2cb6aeb2b00bfe60300f5c975b2daacac9
ownClouders <devops@owncloud.com>
2023-01-12 17:38:12 +0000

    [tx] updated from transifex

---
+++ core/l10n/pl_PL.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "core",
+    {
+    "Settings" : "Ustawienia",
+    "Cancel" : "Anuluj",
+    "Error" : "Błąd",
+    "Edit" : "Edycja",
+    "Name" : "Nazwa",
+    "Save" : "Zapisz",
+    "Delete" : "Usuń",
+    "Personal" : "Osobiste",
+    "Help" : "Pomoc",
+    "Technical details" : "Szczegóły techniczne",
+    "Remote Address: %s" : "Adres zdalny: %s",
+    "Request ID: %s" : "ID żądania: %s"
+},
+"nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);");
