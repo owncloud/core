567d57fd4296caf1fe3c6514b40de3262668dd6a
ownClouders <devops@owncloud.com>
2023-02-24 00:51:52 +0000

    [tx] updated from transifex

--- apps/files_versions/l10n/de.js
+++ apps/files_versions/l10n/de.js
@@ -2,8 +2,13 @@ OC.L10N.register(
     "files_versions",
     {
     "Versions" : "Versionen",
+    "File {file} has been reverted and marked as new current version" : "Die Datei {file} wurde in eine älteren Fassung umgewandelt und als aktuelle Version markiert.",
     "Failed to revert {file} to revision {timestamp}." : "Konnte {file} der Revision {timestamp} nicht rückgängig machen.",
+    "Failed to publish version" : "Die Version konnte nicht veröffentlicht werden.",
     "_%n byte_::_%n bytes_" : ["%n Byte","%n Bytes"],
-    "Restore" : "Wiederherstellen"
+    "Restore" : "Wiederherstellen",
+    "persistent" : "persistent",
+    "Publish version" : "Version veröffentlichen",
+    "current" : "aktuell"
 },
 "nplurals=2; plural=(n != 1);");
