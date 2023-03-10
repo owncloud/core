b77c7da7a8b9e0026287a158a6ec51fe2e9ad041
navid-shokri <123634558+navid-shokri@users.noreply.github.com>
2023-03-10 09:48:10 +0000

    add federated group logic to find share by token handle shareNotFound Exception to continue the execution

--- lib/private/Share20/Manager.php
+++ lib/private/Share20/Manager.php
@@ -1517,8 +1517,22 @@ class Manager implements IManager {
 
 		// If it is not a link share try to fetch a federated share by token
 		if ($share === null) {
-			$provider = $this->factory->getProviderForType(\OCP\Share::SHARE_TYPE_REMOTE);
-			$share = $provider->getShareByToken($token);
+			try{
+				$provider = $this->factory->getProviderForType(\OCP\Share::SHARE_TYPE_REMOTE);
+				$share = $provider->getShareByToken($token);
+			}
+			catch(ShareNotFound $ex){
+				$this->logger->error(
+					"shared file not found by token: $token for federated user share, try to check federated group share.",
+					['app' => __CLASS__]
+				);
+			}
+		}
+
+		if($share === null){
+			$provider = $this->factory->getProviderForType(\OCP\Share::SHARE_TYPE_REMOTE_GROUP);
+			if ($provider !== null)
+				$share = $provider->getShareByToken($token);
 		}
 
 		if (self::shareHasExpired($share)) {
