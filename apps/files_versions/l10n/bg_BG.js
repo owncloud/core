47fb099cf9270224ef51af417b2144dbd835b1c8
ownClouders <devops@owncloud.com>
2023-03-14 00:54:27 +0000

    [tx] updated from transifex

--- apps/files_versions/l10n/bg_BG.js
+++ apps/files_versions/l10n/bg_BG.js
@@ -2,8 +2,13 @@ OC.L10N.register(
     "files_versions",
     {
     "Versions" : "Версии",
+    "File {file} has been reverted and marked as new current version" : "Файлът {file} е върнат и маркиран като нова актуална версия",
     "Failed to revert {file} to revision {timestamp}." : "Неуспешно връщане на {file} към версия {timestamp}.",
+    "Failed to publish version" : "Неуспешно публикуване на версия",
     "_%n byte_::_%n bytes_" : ["%n байт","%n байта"],
-    "Restore" : "Възстановяване"
+    "Restore" : "Възстановяване",
+    "persistent" : "постоянна",
+    "Publish version" : "Публикуване на версия",
+    "current" : "текуща"
 },
 "nplurals=2; plural=(n != 1);");
