62e210d8ae9710c4f702f08f1359205c4a100688
Juergen Weigert <jnweiger@gmail.com>
2023-04-06 16:29:01 +0200

    bump rc.3 including improved 32-bit fixes.

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 // We only can count up. The 4. digit is only for the internal patchlevel to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patchlevel
 // when updating major/minor version number.
-$OC_Version = [10, 12, 1, 1];
+$OC_Version = [10, 12, 1, 2];
 
 // The human-readable string
-$OC_VersionString = '10.12.1 RC2';
+$OC_VersionString = '10.12.1 RC3';
 
 $OC_VersionCanBeUpgradedFrom = [[8, 2, 11],[9, 0, 9],[9, 1]];
 
