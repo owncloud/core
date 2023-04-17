bd5f8b8f79a3bd9c6cc470b4ccc2cde342cb98a7
Phil Davis <phil@jankaritech.com>
2023-04-17 15:37:51 +0545

    Echo some text to describe what happened in litmus tests

--- tests/litmus/run.sh
+++ tests/litmus/run.sh
@@ -6,6 +6,7 @@
 # and it looks for the string "1 failed".
 # This is a workaround for CI so that we can continue to run the litmus test set
 # even though we know that one of the test cases will fail.
+# See PR https://github.com/owncloud/core/pull/40662
 TEST_LOG_FILE=$(mktemp)
 
 /usr/local/bin/litmus-wrapper | tee ${TEST_LOG_FILE}
@@ -16,11 +17,14 @@ if [[ ${RESULT_SUMMARY} =~ 1.failed ]]
 then
 	# There was exactly 1 test that failed. This is what we expect
 	# This is what we expect, so exit with code 0 "success"
+	echo "Exactly 1 test failed, as expected"
 	FINAL_EXIT_STATUS=0
 else
 	# We did not find the text "1 failed"
 	# So maybe more than 1 test failed, or maybe they all passed (unexpectedly)
 	# Exit with code 1 to indicate that something is unexpected.
+	echo "Exactly 1 test was expected to fail but something different happened"
+	echo "Exiting with error status"
 	FINAL_EXIT_STATUS=1
 fi
 
