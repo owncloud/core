2f935eb07a297e53f9ae4ddc91b5dc9b7c139657
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2023-05-17 15:07:56 +0200

    Add commands to handle the trusted servers from command line

---
+++ apps/federation/lib/Command/TrustedServerRemove.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @copyright Copyright (c) 2023, ownCloud GmbH
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+namespace OCA\Federation\Command;
+
+use OCA\Federation\TrustedServers;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TrustedServerRemove extends Command {
+	/** @var TrustedServers */
+	private $trustedServers;
+
+	/**
+	 * @param TrustedServers $trustedServers
+	 */
+	public function __construct(TrustedServers $trustedServers) {
+		parent::__construct();
+		$this->trustedServers = $trustedServers;
+	}
+
+	protected function configure() {
+		$this
+			->setName('federation:trusted-servers:remove')
+			->setDescription('List the trusted servers')
+			->addArgument(
+				'id',
+				InputArgument::REQUIRED,
+				'The id of the server. Check with occ federation:trusted-servers:list'
+			);
+	}
+
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$id = (int)$input->getArgument('id');
+		$this->trustedServers->removeServer($id);
+		return 0;
+	}
+}
