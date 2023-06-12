bcc7e5cc30620dda30f4cad9e12934a6fab4e1a9
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2023-06-12 11:09:02 +0200

    Typos in the template

--- settings/templates/panels/admin/enforce2fa.php
+++ settings/templates/panels/admin/enforce2fa.php
@@ -9,8 +9,8 @@ script('settings', 'panels/enforce2fa');
 	<h3><?php p($l->t('Enforce two-factor authentication')); ?></h3>
 	<p><?php p($l->t('Before enforcing the two-factor authentication, check the following requirements:')); ?></p>
 	<ul>
-		<li><?php p($l->t('At least a two-factor authentication app is installed and enabled in ownCloud.')); ?></li>
-		<li><?php p($l->t('The users can setup at least a two-factor app from the challenge page. Some apps might not be prepared for this')); ?></li>
+		<li><?php p($l->t('At least one two-factor authentication app is installed and enabled in ownCloud.')); ?></li>
+		<li><?php p($l->t('The users can setup at least one two-factor app from the challenge page. Some apps might not be prepared for this')); ?></li>
 	</ul>
 	<p><?php p($l->t('The "twofactor_totp" app fulfills those requirements, and might be used as a fallback so the users can enter their accounts in order to configure other two-factor authentication apps')); ?></p>
 	<br/>
@@ -22,12 +22,12 @@ script('settings', 'panels/enforce2fa');
 	</p>
 	<br/>
 	<p>
-		<?php p($l->t('Exclude the following groups of enforcing the two-factor authentication')); ?>
+		<?php p($l->t('Exclude the following groups from enforcing two-factor authentication')); ?>
 		<br />
 		<input name="enforce_2fa_excluded_groups" type="hidden" id="enforce_2fa_excluded_groups" value="<?php p($_['enforce2faExcludedGroups']) ?>" style="width: 400px">
 		<em>
 			<br />
-			<?php p($l->t('Users in these groups can use two-factor authentication on their own')); ?>
+			<?php p($l->t('Note: Users in these groups can use two-factor authentication on their own')); ?>
 		</em>
 	</p>
 </div>
