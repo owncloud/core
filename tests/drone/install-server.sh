76dc3c3d6a531e894fe9232b8b338eeff2375ce1
Phil Davis <phil@jankaritech.com>
2023-08-11 14:31:39 +0545

    display pgsql users

--- tests/drone/install-server.sh
+++ tests/drone/install-server.sh
@@ -92,6 +92,7 @@ case "${DB_TYPE}" in
     DB=pgsql
     # PGPASSWORD="owncloud" psql -h postgres -U postgres -c "CREATE ROLE owncloud LOGIN;"
     PGPASSWORD="owncloud" psql -h postgres -U owncloud -c "GRANT ALL ON SCHEMA public TO owncloud;"
+    PGPASSWORD="owncloud" psql -h postgres -U owncloud -c "\\du"
     ;;
   oracle)
     wait-for-it -t 600 oracle:1521
