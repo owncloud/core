940c0dc8e9ab759848c1732a6af5903cd6f493b2
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2023-09-25 13:14:50 +0200

    Add unit tests

--- settings/Controller/AppConfigController.php
+++ settings/Controller/AppConfigController.php
@@ -23,7 +23,6 @@ use OCP\IAppConfig;
 use OCP\IRequest;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
-use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Http\JSONResponse;
 
 /**
