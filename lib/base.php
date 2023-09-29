e335e99efe30f56223cc2b8953344ea5b20bfbaa
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2023-09-29 11:36:43 +0200

    feat: move php version check to base.php

--- lib/base.php
+++ lib/base.php
@@ -5,14 +5,14 @@ $eol = '<br>';
 if (\defined('OC_CONSOLE')) {
 	$eol = PHP_EOL;
 }
-if (PHP_VERSION_ID < 70400) {
-	echo 'This version of ownCloud requires at least PHP 7.4.0'.$eol;
+if (PHP_VERSION_ID < 80200) {
+	echo 'This version of ownCloud requires at least PHP 8.2.0'.$eol;
 	echo 'You are currently running PHP ' . PHP_VERSION . '. Please update your PHP version.'.$eol;
 	exit(1);
 }
 
-if (PHP_VERSION_ID >= 80000) {
-	echo 'This version of ownCloud is not compatible with PHP 8.0' . $eol;
+if (PHP_VERSION_ID >= 80300) {
+	echo 'This version of ownCloud is not compatible with PHP 8.3' . $eol;
 	echo 'You are currently running PHP ' . PHP_VERSION . '.' . $eol;
 	exit(1);
 }
