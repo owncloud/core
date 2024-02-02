cc004fdc1334af7834808b7ca7a7ec60124f7365
ownClouders <devops@owncloud.com>
2024-02-02 01:20:47 +0000

    [tx] updated from transifex

--- apps/files_versions/l10n/sq.js
+++ apps/files_versions/l10n/sq.js
@@ -3,7 +3,7 @@ OC.L10N.register(
     {
     "Versions" : "Versione",
     "File {file} has been reverted and marked as new current version" : "Kartela {file} është prapakthyer dhe i është vënë shenjë si versioni i ri i tanishëm",
-    "Failed to revert {file} to revision {timestamp}." : "Dështoi në rikthimin e {file} te rishikimi {timestamp}.",
+    "Failed to revert {file} to revision {timestamp}." : "S’u arrit të rikthehet {file} te rishikimi {timestamp}.",
     "Failed to publish version" : "S’u arrit të publikohet version",
     "_%n byte_::_%n bytes_" : ["%n bajt","%n bajte"],
     "Restore" : "Riktheje",
