88713c79b6dd05d7f770d98b7f2e2a967ff611d8
ownClouders <devops@owncloud.com>
2024-02-29 01:04:48 +0000

    [tx] updated from transifex

--- apps/files_external/l10n/he.js
+++ apps/files_external/l10n/he.js
@@ -115,4 +115,4 @@ OC.L10N.register(
     "Allow users to mount the following external storage" : "מאפשר למשתמשים לחבר אחסון חיצוני הבא",
     "Allow sharing on user-mounted external storages" : "הפעלת שיתוף של זיכרונות התקן-משתמש חיצוניים"
 },
-"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
+"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: 2;");
