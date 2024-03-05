8e896dfbb6c2663393a5ed8df087ade3f08cfa10
ownClouders <devops@owncloud.com>
2024-03-05 00:49:56 +0000

    [tx] updated from transifex

--- apps/files_sharing/l10n/he.js
+++ apps/files_sharing/l10n/he.js
@@ -130,4 +130,4 @@ OC.L10N.register(
     "Group Sharing Blacklist" : "רשימה שחורה לשיתוף קבוצות",
     "Exclude groups from receiving shares" : "מניעת קבוצות מלקבל שיתופים"
 },
-"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
+"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: 2;");
--- apps/files_versions/l10n/he.js
+++ apps/files_versions/l10n/he.js
@@ -3,7 +3,7 @@ OC.L10N.register(
     {
     "Versions" : "גרסאות",
     "Failed to revert {file} to revision {timestamp}." : "נכשל אחזור {file} לגרסה {timestamp}.",
-    "_%n byte_::_%n bytes_" : ["%n ביט","%n ביטים","%n ביטים","%n ביטים"],
+    "_%n byte_::_%n bytes_" : ["%n ביט","%n ביטים","%n ביטים"],
     "Restore" : "שחזור"
 },
-"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
+"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: 2;");
