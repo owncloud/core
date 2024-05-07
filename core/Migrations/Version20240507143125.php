1b5c5fa8aac57f841417d9f550f09af3aaf05990
Ilja Neumann <ineumann@owncloud.com>
2024-05-07 18:50:38 +0200

    fix: apply migration only for pg and sqlite

--- core/Migrations/Version20240507143125.php
+++ core/Migrations/Version20240507143125.php
@@ -1,12 +1,19 @@
 <?php
 namespace OC\Migrations;
 
+use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
+use Doctrine\DBAL\Platforms\SqlitePlatform;
 use OCP\IDBConnection;
 use OCP\Migration\ISqlMigration;
 
 class Version20240507143125 implements ISqlMigration {
 	public function sql(IDBConnection $connection) {
-		return ["CREATE UNIQUE INDEX child_share_unique ON oc_share (share_with, parent) 
+		$dbPlatform = $connection->getDatabasePlatform();
+		if ($dbPlatform instanceof PostgreSqlPlatform || $dbPlatform instanceof SqlitePlatform) {
+			return ["CREATE UNIQUE INDEX child_share_unique ON oc_share (share_with, parent) 
     		WHERE parent IS NOT NULL AND share_type = 2"];
+		}
+
+		return [];
 	}
 }
