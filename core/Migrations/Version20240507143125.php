9a29ebb1baa95301a7730dd528cce0728e800533
Ilja Neumann <ineumann@owncloud.com>
2024-05-08 10:49:31 +0200

    fix: add equivalent index for mysql

--- core/Migrations/Version20240507143125.php
+++ core/Migrations/Version20240507143125.php
@@ -1,6 +1,7 @@
 <?php
 namespace OC\Migrations;
 
+use Doctrine\DBAL\Platforms\MySqlPlatform;
 use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
 use Doctrine\DBAL\Platforms\SqlitePlatform;
 use OCP\IDBConnection;
@@ -12,6 +13,13 @@ class Version20240507143125 implements ISqlMigration {
 		if ($dbPlatform instanceof PostgreSqlPlatform || $dbPlatform instanceof SqlitePlatform) {
 			return ["CREATE UNIQUE INDEX child_share_unique ON oc_share (share_with, parent) 
     		WHERE parent IS NOT NULL AND share_type = 2"];
+		}
+
+		if ($dbPlatform instanceof MySqlPlatform) {
+			// This should be preceise enough as mysql ignores nulls in unique indexes
+			return [
+				"CREATE UNIQUE INDEX child_share_unique ON oc_share (item_source, share_with, share_type, parent);"
+			];
 		}
 
 		return [];
