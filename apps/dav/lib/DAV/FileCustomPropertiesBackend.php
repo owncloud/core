550e3ea5e5bd1339cc816733ec4b4749cf207dd1
Phil Davis <phil@jankaritech.com>
2024-06-01 14:16:06 +0545

    chore: use owncloud/coding-standard 5.1

--- apps/dav/lib/DAV/FileCustomPropertiesBackend.php
+++ apps/dav/lib/DAV/FileCustomPropertiesBackend.php
@@ -40,12 +40,12 @@ use Sabre\DAV\INode;
 class FileCustomPropertiesBackend extends AbstractCustomPropertiesBackend {
 	public const SELECT_BY_ID_STMT = 'SELECT * FROM `*PREFIX*properties` WHERE `fileid` = ?';
 	public const INSERT_BY_ID_STMT = 'INSERT INTO `*PREFIX*properties`'
-	. ' (`fileid`,`propertyname`,`propertyvalue`, `propertytype`) VALUES(?,?,?,?)';
+		. ' (`fileid`,`propertyname`,`propertyvalue`, `propertytype`) VALUES(?,?,?,?)';
 	public const UPDATE_BY_ID_AND_NAME_STMT = 'UPDATE `*PREFIX*properties`'
-	. ' SET `propertyvalue` = ?, `propertytype` = ? WHERE `fileid` = ? AND `propertyname` = ?';
+		. ' SET `propertyvalue` = ?, `propertytype` = ? WHERE `fileid` = ? AND `propertyname` = ?';
 	public const DELETE_BY_ID_STMT = 'DELETE FROM `*PREFIX*properties` WHERE `fileid` = ?';
 	public const DELETE_BY_ID_AND_NAME_STMT = 'DELETE FROM `*PREFIX*properties`'
-	. ' WHERE `fileid` = ? AND `propertyname` = ?';
+		. ' WHERE `fileid` = ? AND `propertyname` = ?';
 
 	/**
 	 * @var CappedMemoryCache
