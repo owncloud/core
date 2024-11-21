ca7a471f05f028a2f8d3f637ee07da136f6f2d47
ownClouders <devops@owncloud.com>
2024-11-21 00:29:52 +0000

    [tx] updated from transifex

--- apps/files_versions/l10n/gl.js
+++ apps/files_versions/l10n/gl.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Versions" : "Versións",
     "Failed to revert {file} to revision {timestamp}." : "Non foi posíbel reverter {file} á revisión {timestamp}.",
     "_%n byte_::_%n bytes_" : ["%n byte","%n bytes"],
-    "Restore" : "Restabelecer"
+    "Restore" : "Restabelecer",
+    "Publish version" : "Publicar versión"
 },
 "nplurals=2; plural=(n != 1);");
