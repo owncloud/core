6cc5344460845c7746c9f716c0ba1bbf2e876a7e
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2025-07-02 11:48:13 +0200

    fix: web updater is now disabled by default + discourage users to use the web updater

--- apps/updatenotification/templates/admin.php
+++ apps/updatenotification/templates/admin.php
@@ -21,10 +21,6 @@
 
 	<?php if ($isNewVersionAvailable === true): ?>
 		<strong><?php p($l->t('A new version is available: %s', [$newVersionString])); ?></strong>
-		<input type="button" id="oca_updatenotification_button" value="<?php p($l->t('Open updater')) ?>">
-		<?php if ($changeLogUrl): ?>
-			<a target="_blank" rel="noreferrer" class="icon-info svg" title="<?php p($l->t('Show changelog')); ?>" href="<?php print_unescaped($changeLogUrl) ?>"></a>
-		<?php endif; ?>
 	<?php else: ?>
 		<strong><?php print_unescaped($l->t('Your version is up to date.')); ?></strong>
 		<span class="icon-info svg" title="<?php p($l->t('Checked on %s', [$lastCheckedDate])) ?>"></span>
