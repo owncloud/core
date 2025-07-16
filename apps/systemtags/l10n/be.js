20d79b84d43e3f8b831fa5377f88ee2ced5b00d5
ownClouders <devops@owncloud.com>
2025-07-16 00:49:06 +0000

    [tx] updated from transifex

--- apps/systemtags/l10n/be.js
+++ apps/systemtags/l10n/be.js
@@ -1,9 +1,9 @@
 OC.L10N.register(
-    "files_trashbin",
+    "systemtags",
     {
-    "Delete" : "Выдаліць",
-    "Error" : "Памылка",
-    "Select all" : "Выбраць усе",
-    "Name" : "Назва"
+    "No files in here" : "Тут няма файлаў",
+    "Name" : "Назва",
+    "Size" : "Памер",
+    "Modified" : "Зменены"
 },
 "nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);");
