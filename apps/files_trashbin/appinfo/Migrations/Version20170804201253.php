f7a497dac7a78107a978430fe9041f8d6f5102bf
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2026-02-18 11:41:23 +0100

    fix: convert Type:: to Types::

--- apps/files_trashbin/appinfo/Migrations/Version20170804201253.php
+++ apps/files_trashbin/appinfo/Migrations/Version20170804201253.php
@@ -4,6 +4,7 @@ namespace OCA\Files_Trashbin\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
 use OCP\Migration\ISchemaMigration;
 
 /** Updates some fields to bigint if required */
@@ -15,8 +16,8 @@ class Version20170804201253 implements ISchemaMigration {
 			$table = $schema->getTable("{$prefix}files_trash");
 
 			$idColumn = $table->getColumn('auto_id');
-			if ($idColumn && $idColumn->getType()->getName() !== Type::BIGINT) {
-				$idColumn->setType(Type::getType(Type::BIGINT));
+			if ($idColumn && $idColumn->getType()->getName() !== Types::BIGINT) {
+				$idColumn->setType(Type::getType(Types::BIGINT));
 				$idColumn->setOptions(['length' => 20]);
 			}
 		}
