d5060f995ccc70669bcd088849737821e7cdd21e
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2026-02-19 11:23:06 +0100

    feat: 11.0.0-prealpha

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 // We only can count up. The 4. digit is only for the internal patch-level to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch-level
 // when updating major/minor version number.
-$OC_Version = [10, 16, 1, 0];
+$OC_Version = [11, 0, 0, 0];
 
 // The human-readable string
-$OC_VersionString = '10.16.1';
+$OC_VersionString = '11.0.0-prealpha';
 
 $OC_VersionCanBeUpgradedFrom = [[8, 2, 11],[9, 0, 9],[9, 1]];
 
