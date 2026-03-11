ce3f006b0d8968ce081252a5a93d046460aec7b3
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2026-03-11 15:15:11 +0100

    fix: remove unused code from \OC\Archive classes - fixes deprecated usage of ZipArchive::open()

--- lib/private/Archive/Archive.php
+++ lib/private/Archive/Archive.php
@@ -45,9 +45,8 @@ abstract class Archive {
 			case 'application/zip':
 				return new ZIP($path);
 			case 'application/gzip':
-			case 'application/x-gzip':
-				return new TAR($path);
 			case 'application/x-bzip2':
+			case 'application/x-gzip':
 				return new TAR($path);
 		}
 	}
@@ -56,107 +55,11 @@ abstract class Archive {
 	 * @param $source
 	 */
 	abstract public function __construct($source);
-	/**
-	 * add an empty folder to the archive
-	 * @param string $path
-	 * @return bool
-	 */
-	abstract public function addFolder($path);
-	/**
-	 * add a file to the archive
-	 * @param string $path
-	 * @param string $source either a local file or string data
-	 * @return bool
-	 */
-	abstract public function addFile($path, $source='');
-	/**
-	 * rename a file or folder in the archive
-	 * @param string $source
-	 * @param string $dest
-	 * @return bool
-	 */
-	abstract public function rename($source, $dest);
-	/**
-	 * get the uncompressed size of a file in the archive
-	 * @param string $path
-	 * @return int
-	 */
-	abstract public function filesize($path);
-	/**
-	 * get the last modified time of a file in the archive
-	 * @param string $path
-	 * @return int
-	 */
-	abstract public function mtime($path);
-	/**
-	 * get the files in a folder
-	 * @param string $path
-	 * @return array
-	 */
-	abstract public function getFolder($path);
-	/**
-	 * get all files in the archive
-	 * @return array
-	 */
-	abstract public function getFiles();
-	/**
-	 * get the content of a file
-	 * @param string $path
-	 * @return string
-	 */
-	abstract public function getFile($path);
-	/**
-	 * extract a single file from the archive
-	 * @param string $path
-	 * @param string $dest
-	 * @return bool
-	 */
-	abstract public function extractFile($path, $dest);
+
 	/**
 	 * extract the archive
 	 * @param string $dest
 	 * @return bool
 	 */
 	abstract public function extract($dest);
-	/**
-	 * check if a file or folder exists in the archive
-	 * @param string $path
-	 * @return bool
-	 */
-	abstract public function fileExists($path);
-	/**
-	 * remove a file or folder from the archive
-	 * @param string $path
-	 * @return bool
-	 */
-	abstract public function remove($path);
-	/**
-	 * get a file handler
-	 * @param string $path
-	 * @param string $mode
-	 * @return resource
-	 */
-	abstract public function getStream($path, $mode);
-	/**
-	 * add a folder and all its content
-	 * @param string $path
-	 * @param string $source
-	 * @return boolean|null
-	 */
-	public function addRecursive($path, $source) {
-		$dh = \opendir($source);
-		if (\is_resource($dh)) {
-			$this->addFolder($path);
-			while (($file = \readdir($dh)) !== false) {
-				if ($file=='.' or $file=='..') {
-					continue;
-				}
-				if (\is_dir($source.'/'.$file)) {
-					$this->addRecursive($path.'/'.$file, $source.'/'.$file);
-				} else {
-					$this->addFile($path.'/'.$file, $source.'/'.$file);
-				}
-			}
-		}
-	}
 }
